b6dfd74af95246a5f4f290d0e93cb1276978de67
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-10-30 22:00:42 -0700

    updated

--- steve_dev/ReasoningTool.py
+++ steve_dev/ReasoningTool.py
@@ -1,23 +1,27 @@
 from Orangeboard import Orangeboard
 from ReasoningToolNode import ReasoningToolNode
 from QueryOMIM import QueryOMIM
-import mygene
+from QueryMyGene import QueryMyGene
+from QueryPC2 import QueryPC2
 
-mg = mygene.MyGeneInfo()
 query_omim_obj = QueryOMIM()
-
+query_mygene_obj = QueryMyGene()
+query_pc2 = QueryPC2()
 
 genetic_condition_mim_id = 603903  # sickle-cell anemia
 target_disease_disont_id = 12365
 
-master_rel_is_directed = {"genetic_cond_affects": True}
+master_rel_is_directed = {"genetic_cond_affects": True,
+                          "is_member_of": True}
                         
-master_rel_ids_in_orangeboard = {"genetic_cond_affects": dict()}
+master_rel_ids_in_orangeboard = {"genetic_cond_affects": dict(),
+                                 "is_member_of": dict()}
 
-master_node_ids_in_orangeboard = {"mim":     dict(),
-                                  "disont":  dict(),
-                                  "uniprot": dict(),
-                                  "gene":    dict()}
+master_node_ids_in_orangeboard = {"mim":      dict(),
+                                  "disont":   dict(),
+                                  "uniprot":  dict(),
+                                  "gene":     dict(),
+                                  "reactome": dict()}
 
 
 def add_rel(orangeboard, rel_type, source_node_uuid, target_node_uuid):
@@ -51,14 +55,6 @@ def get_rel_if_in_orangeboard(source_node_uuid, target_node_uuid, rel_type):
                 rel_uuid = rel_type_map[rel_key]
     return rel_uuid
 
-def convert_gene_symbol_to_uniprot_id(gene_symbol):
-    res = mg.query('symbol:' + gene_symbol, species='human',
-                   fields='uniprot')
-    uniprot_id = None
-    if len(res) > 0:
-        uniprot_id = set([hit["uniprot"]["Swiss-Prot"] for hit in res["hits"]])
-    return uniprot_id
-
 def add_rel_if_not_in_orangeboard(orangeboard, source_node_uuid, target_node_uuid, rel_type):
     rel_uuid = get_rel_if_in_orangeboard(source_node_uuid, target_node_uuid, rel_type)
     if None == rel_uuid:
@@ -71,12 +67,20 @@ def add_node_if_not_in_orangeboard(orangeboard, biotype, node_name):
         node_uuid = master_node_ids_in_orangeboard[biotype][node_name]
     return node_uuid
 
+def expand_uniprot(orangeboard, node):
+    uniprot_id_str = node.get_bioname()
+    pathways_set = query_pc2.uniprot_id_to_reactome_pathways(uniprot_id_str)
+    source_node_uuid = node.get_uuid()
+    for pathway_id in pathways_set:
+        target_node_uuid = add_node_if_not_in_orangeboard(orangeboard, "reactome", pathway_id)
+        add_rel_if_not_in_orangeboard(orangeboard, source_node_uuid, target_node_uuid, "is_member_of")
+        
 def expand_mim(orangeboard, node):
     res_dict = query_omim_obj.disease_mim_to_gene_symbols_and_uniprot_ids(int(node.get_bioname()))
     uniprot_ids = res_dict["uniprot_ids"]
     gene_symbols = res_dict["gene_symbols"]
     for gene_symbol in gene_symbols:
-        uniprot_id = convert_gene_symbol_to_uniprot_id(gene_symbol)
+        uniprot_id = query_mygene_obj.convert_gene_symbol_to_uniprot_id(gene_symbol)
         if None != uniprot_id:
             uniprot_ids = uniprot_ids.union(uniprot_id)
     source_node_uuid = node.get_uuid()
@@ -103,6 +107,11 @@ add_node(ob, "mim", genetic_condition_mim_id)
 ## add the initial target disease into the Orangeboard, as a "disease ontology" node
 add_node(ob, "disont", target_disease_disont_id)
 
+    
+for node in ob.get_all_nodes():
+    node.__class__  = ReasoningToolNode
+    if not node.is_expanded():
+        expand(ob, node)
     
 for node in ob.get_all_nodes():
     node.__class__  = ReasoningToolNode
