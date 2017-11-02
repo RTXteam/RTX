c8d5da0dc47b3dc53a3eb5ce78ac1f5017ed61c4
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-11-01 21:55:21 -0700

    lots of code changes

--- steve_dev/QueryPC2.py
+++ steve_dev/QueryPC2.py
@@ -1,16 +1,32 @@
 import requests
 import sys
 
-
 class QueryPC2:
     API_BASE_URL = 'http://www.pathwaycommons.org/pc2'
 
     @staticmethod
     def send_query_get(handler, url_suffix):
-        res = requests.get(QueryPC2.API_BASE_URL + "/" + handler + "?" + url_suffix)
+        url_str = QueryPC2.API_BASE_URL + "/" + handler + "?" + url_suffix
+        print(url_str)
+        res = requests.get(url_str)
         assert 200 == res.status_code
         return res
 
+    @staticmethod
+    def pathway_to_uniprot_ids(pathway_reactome_id):
+        query_str = "uri=http://identifiers.org/reactome/" + pathway_reactome_id + "&format=TXT"
+        res = QueryPC2.send_query_get("get", query_str)
+        start_capturing = False
+        res_text = res.text
+        res_set = set()
+        for line_str in res_text.splitlines():
+            if start_capturing:
+                res_set.add(line_str.split("\t")[3].split(":")[1])
+            if line_str.split("\t")[0] == "PARTICIPANT":
+                start_capturing = True
+        return res_set
+
+    @staticmethod
     def uniprot_id_to_reactome_pathways(uniprot_id):
         res = QueryPC2.send_query_get("search.json", "q=" + uniprot_id + "&type=pathway")
         res_dict = res.json()
@@ -20,10 +36,9 @@ class QueryPC2:
         return set(pathway_list)
 
     @staticmethod
-    def __test():
-        qp = QueryPC2()
-        print(qp.uniprot_id_to_reactome_pathways("P68871"))
-
+    def test():
+        print(QueryPC2.uniprot_id_to_reactome_pathways("P68871"))
+        print(QueryPC2.pathway_to_uniprot_ids("R-HSA-168249"))
 
 if "--test" in set(sys.argv):
-    QueryPC2.__test()
+    QueryPC2.test()
