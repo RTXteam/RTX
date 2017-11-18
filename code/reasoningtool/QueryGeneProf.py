7cffcdb2a93f9ef3bac6d0d31a40327d19476171
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-11-18 07:57:01 -0800

    GeneProf down; set up timeout and add code to fix #41

--- code/reasoningtool/QueryGeneProf.py
+++ code/reasoningtool/QueryGeneProf.py
@@ -1,47 +1,53 @@
 import requests
-import functools
-import CachedMethods
-
+import sys
 
 class QueryGeneProf:
     API_BASE_URL = 'http://www.geneprof.org/GeneProf/api'
+    TIMEOUT_SEC = 1
 
     @staticmethod
     def send_query_get(handler, url_suffix):
         url_str = QueryGeneProf.API_BASE_URL + "/" + handler + "/" + url_suffix
 #        print(url_str)
-        res = requests.get(url_str)
-        assert 200 == res.status_code
+        try:
+            res = requests.get(url_str, timeout=QueryGeneProf.TIMEOUT_SEC)
+        except requests.exceptions.Timeout:
+            print(url_str, sys.stderr)
+            print("Timeout in QueryGeneProf for URL: " + url_str, file=sys.stderr)
+            return None
+        status_code = res.status_code
+        if status_code != 200:
+            print("HTTP response status code: " + str(status_code) + " for URL:\n" + url_str, file=sys.stderr)
+            res = None
         return res
 
     @staticmethod
-    @CachedMethods.register
     def gene_symbol_to_geneprof_ids(gene_symbol):
         handler = 'gene.info/gp.id'
         url_suffix = 'human/C_NAME/' + gene_symbol + '.json'
-        res = QueryGeneProf.send_query_get(handler, url_suffix).json()
-        if len(res) > 0:
-            ret_set = set(res['ids'])
-        else:
-            ret_set = {}
+        res = QueryGeneProf.send_query_get(handler, url_suffix)
+        ret_set = {}
+        if res is not None:
+            res_json = res.json()
+            if res_json is not None and len(res_json) > 0:
+                ret_set = set(res_json['ids'])
         return ret_set
 
     @staticmethod
-    @CachedMethods.register
     def geneprof_id_to_transcription_factor_gene_symbols(geneprof_id):
         handler = 'gene.info/regulation/binary/by.target'
         url_suffix = 'human/' + str(geneprof_id) + '.json?with-sample-info=true'
         res = QueryGeneProf.send_query_get(handler, url_suffix).json()
-        sample_info_dict_list = [expt['sample'] for expt in res['values'] if expt['is_target']]
         tf_genes = set()
-        for sample_info in sample_info_dict_list:
-            gene_symbol = sample_info.get("Gene", None)
-            if gene_symbol is not None:
-                tf_genes.add(gene_symbol)
+        if res is not None:
+            sample_info_dict_list = [expt['sample'] for expt in res['values'] if expt['is_target']]
+            for sample_info in sample_info_dict_list:
+                gene_symbol = sample_info.get("Gene", None)
+                if gene_symbol is not None:
+                    tf_genes.add(gene_symbol)
         return tf_genes
 
     @staticmethod
-    @CachedMethods.register
     def gene_symbol_to_transcription_factor_gene_symbols(gene_symbol):
         geneprof_ids_set = QueryGeneProf.gene_symbol_to_geneprof_ids(gene_symbol)
         tf_gene_symbols = set()
@@ -53,7 +59,7 @@ class QueryGeneProf:
         
 if __name__ == '__main__':
     QueryGeneProf.gene_symbol_to_geneprof_ids('xyzzy')
-    hmox1_geneprof_id = QueryGeneProf.gene_symbol_to_geneprof_ids('HMOX1')
-    print(next(iter(hmox1_geneprof_id)))
-    print(QueryGeneProf.gene_symbol_to_transcription_factor_gene_symbols(str(next(iter(hmox1_geneprof_id)))))
     print(QueryGeneProf.gene_symbol_to_transcription_factor_gene_symbols('HMOX1'))
+    print(QueryGeneProf.gene_symbol_to_geneprof_ids('HMOX1'))
+#    print(next(iter(hmox1_geneprof_id)))
+#    print(QueryGeneProf.gene_symbol_to_transcription_factor_gene_symbols(str(next(iter(hmox1_geneprof_id)))))
