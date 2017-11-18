f76cafa5c564a2ab3c9337258157a46bbb641042
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-11-18 07:57:10 -0800

    code cleanup

--- code/reasoningtool/QueryReactome.py
+++ code/reasoningtool/QueryReactome.py
@@ -1,6 +1,5 @@
 import requests
-import CachedMethods
-
+import sys
 
 class QueryReactome:
 
@@ -12,31 +11,30 @@ class QueryReactome:
 #        print(url_str)
         res = requests.get(url_str, headers={'accept': 'application/json'})
         status_code = res.status_code
-        assert status_code in [200, 404]
-        if status_code == 404:
+        if status_code != 200:
+            print("HTTP response status code: " + str(status_code) + " for URL:\n" + url_str, file=sys.stderr)
             res = None
         return res
 
     @staticmethod
-    @CachedMethods.register
     def query_uniprot_id_to_interacting_uniprot_ids(uniprot_id):
         res = QueryReactome.send_query_get("interactors/static/molecule", uniprot_id + "/details").json()
         res_uniprot_ids = dict()
-        res_entities = res.get('entities', None)
-        if res_entities is not None:
-            for res_entity in res_entities:
-                res_entity_interactors = res_entity.get('interactors', None)
-                if res_entity_interactors is not None:
-                    for res_entity_interactor in res_entity_interactors:
-                        int_uniprot_id = res_entity_interactor.get('acc', None)
-                        if int_uniprot_id is not None:
-                            if 'CHEBI:' not in int_uniprot_id:
-                                int_alias = res_entity_interactor.get('alias', '')
-                                res_uniprot_ids[int_uniprot_id] = int_alias
+        if res is not None:
+            res_entities = res.get('entities', None)
+            if res_entities is not None:
+                for res_entity in res_entities:
+                    res_entity_interactors = res_entity.get('interactors', None)
+                    if res_entity_interactors is not None:
+                        for res_entity_interactor in res_entity_interactors:
+                            int_uniprot_id = res_entity_interactor.get('acc', None)
+                            if int_uniprot_id is not None:
+                                if 'CHEBI:' not in int_uniprot_id:
+                                    int_alias = res_entity_interactor.get('alias', '')
+                                    res_uniprot_ids[int_uniprot_id] = int_alias
         return res_uniprot_ids
         
     @staticmethod
-    @CachedMethods.register
     def query_uniprot_to_reactome_entity_id(uniprot_id):
         res = QueryReactome.send_query_get("data/complexes/UniProt", uniprot_id)
         if res is not None:
@@ -51,17 +49,16 @@ class QueryReactome:
         return ret_ids
 
     @staticmethod
-    @CachedMethods.register
     def query_uniprot_to_reactome_entity_id_desc(uniprot_id):
-        res_json = QueryReactome.send_query_get("data/complexes/UniProt", uniprot_id).json()
-        if type(res_json)==list:
-            ret_ids = dict([[res_entry["stId"], res_entry["displayName"]] for res_entry in res_json])
-        else:
-            ret_ids = dict()
+        res = QueryReactome.send_query_get("data/complexes/UniProt", uniprot_id)
+        ret_ids = dict()
+        if res is not None:
+            res_json = res.json()
+            if type(res_json)==list:
+                ret_ids = dict([[res_entry["stId"], res_entry["displayName"]] for res_entry in res_json])
         return ret_ids
     
     @staticmethod
-    @CachedMethods.register
     def query_reactome_entity_id_to_reactome_pathway_ids(reactome_entity_id):
         res = QueryReactome.send_query_get("data/pathways/low/diagram/entity", reactome_entity_id + "/allForms?species=9606")
         if res is not None:
@@ -73,7 +70,6 @@ class QueryReactome:
         return ret_set
 
     @staticmethod
-    @CachedMethods.register
     def query_reactome_entity_id_to_reactome_pathway_ids_desc(reactome_entity_id):
         res = QueryReactome.send_query_get("data/pathways/low/diagram/entity", reactome_entity_id + "/allForms?species=9606")
         if res is not None:
@@ -84,7 +80,6 @@ class QueryReactome:
         return reactome_ids_dict
     
     @staticmethod
-    @CachedMethods.register
     def query_uniprot_id_to_reactome_pathway_ids(uniprot_id):
         reactome_entity_ids = QueryReactome.query_uniprot_to_reactome_entity_id(uniprot_id)
         res_set = set()
@@ -95,7 +90,6 @@ class QueryReactome:
         return res_set
 
     @staticmethod
-    @CachedMethods.register
     def query_uniprot_id_to_reactome_pathway_ids_desc(uniprot_id):
         reactome_entity_ids = QueryReactome.query_uniprot_to_reactome_entity_id(uniprot_id)
         res_dict = dict()
@@ -106,35 +100,33 @@ class QueryReactome:
         return res_dict
     
     @staticmethod
-    @CachedMethods.register
     def query_reactome_pathway_id_to_uniprot_ids(reactome_pathway_id):
-        res_json = QueryReactome.send_query_get("data/participants", reactome_pathway_id).json()
-        participant_ids_list = [refEntity["displayName"] for peDbEntry in res_json for refEntity in peDbEntry["refEntities"]]
-        uniprot_ids_list = [id.split(" ")[0].split(":")[1] for id in participant_ids_list if "UniProt" in id]
-        return set(uniprot_ids_list)
+        res = QueryReactome.send_query_get("data/participants", reactome_pathway_id)
+        uniprot_ids_set = set()
+        if res is not None:
+            res_json = res.json()
+            participant_ids_list = [refEntity["displayName"] for peDbEntry in res_json for refEntity in peDbEntry["refEntities"]]
+            uniprot_ids_list = [id.split(" ")[0].split(":")[1] for id in participant_ids_list if "UniProt" in id]
+            uniprot_ids_set = set(uniprot_ids_list)
+        return uniprot_ids_set
 
     @staticmethod
-    @CachedMethods.register
     def query_reactome_pathway_id_to_uniprot_ids_desc(reactome_pathway_id):
-        res_json = QueryReactome.send_query_get("data/participants", reactome_pathway_id).json()
-#        print(res_json)
-        participant_ids_list = [refEntity["displayName"] for peDbEntry in res_json for refEntity in peDbEntry["refEntities"]]
-        uniprot_ids_list = [[id.split(" ")[0].split(":")[1], id.split(" ")[1]] for id in participant_ids_list if "UniProt" in id]
-        return dict(uniprot_ids_list)
-    
-    # @staticmethod
-    # def uniprot_to_reactome_entity_id(uniprot_id):
-    #     handler = 'data/complexes/UniProt'
-    #     url_suffix = uniprot_id
-    #     res = QueryReactome.send_query_get(handler, url_suffix)
-    #     return res.json()
-
-        
+        res = QueryReactome.send_query_get("data/participants", reactome_pathway_id)
+        ret_dict = dict()
+        if res is not None:
+            res_json = res.json()
+            #        print(res_json)
+            participant_ids_list = [refEntity["displayName"] for peDbEntry in res_json for refEntity in peDbEntry["refEntities"]]
+            uniprot_ids_list = [[id.split(" ")[0].split(":")[1], id.split(" ")[1]] for id in participant_ids_list if "UniProt" in id]
+            ret_dict = dict(uniprot_ids_list)
+        return ret_dict
+           
 if __name__ == '__main__':
-    #        print(QueryReactome.query_uniprot_to_reactome_entity_id("P68871"))
-    #        print(QueryReactome.query_uniprot_to_reactome_entity_id("O75521-2"))
-    #        print(QueryReactome.query_reactome_entity_id_to_reactome_pathway_ids_desc("R-HSA-2230989"))
+    print(QueryReactome.query_uniprot_to_reactome_entity_id("P68871"))
+    print(QueryReactome.query_uniprot_to_reactome_entity_id("O75521-2"))
+    print(QueryReactome.query_reactome_entity_id_to_reactome_pathway_ids_desc("R-HSA-2230989"))
     print(QueryReactome.query_uniprot_id_to_interacting_uniprot_ids("Q13501"))
-    #        print(QueryReactome.query_uniprot_id_to_reactome_pathway_ids_desc("P68871"))
-    #        print(QueryReactome.query_reactome_pathway_id_to_uniprot_ids_desc("R-HSA-5423646"))
+    print(QueryReactome.query_uniprot_id_to_reactome_pathway_ids_desc("P68871"))
+    print(QueryReactome.query_reactome_pathway_id_to_uniprot_ids_desc("R-HSA-5423646"))
         
