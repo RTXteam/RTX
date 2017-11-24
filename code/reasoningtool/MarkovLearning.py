e5242c767981d8532815259f3da2bbd750225e33
dkoslicki <david.koslicki@math.oregonstate.edu>
2017-11-24 14:37:23 -0800

    tweak the bayesian/laplace priors since we incorporated more training data

--- code/reasoningtool/MarkovLearning.py
+++ code/reasoningtool/MarkovLearning.py
@@ -91,7 +91,7 @@ def train(state_space, quad_to_matrix_index, obs_dict, type='ML'):
 			if row_sums[index] > 0:
 				trans_mat[index, :] /= row_sums[index]
 	elif type == 'L':
-		pseudo_count = 0.01
+		pseudo_count = 0.001
 		trans_mat += pseudo_count  # add a psedo-count
 		row_sums = trans_mat.sum(axis=1)
 		for index in range(len(row_sums)):
@@ -173,7 +173,8 @@ def test():
 	state_space, quad_to_matrix_index = initialize_Markov_chain(connection, config)
 	trained = train(state_space, quad_to_matrix_index, paths_dict, type='L')
 	# This can get messed up if you change the priors
-	assert np.abs(path_probability(trained, quad_to_matrix_index, paths_dict[omim][1][0]) - 0.271387803655) < .001
+	#print(path_probability(trained, quad_to_matrix_index, paths_dict[omim][1][0]))
+	assert np.abs(path_probability(trained, quad_to_matrix_index, paths_dict[omim][1][0]) - 0.851746) < .01
 	trained = train(state_space, quad_to_matrix_index, paths_dict, type='ML')
 	# This should always == 1
 	assert path_probability(trained, quad_to_matrix_index, paths_dict[omim][1][0]) == 1.0
