0205dba511168b05c55454b47792c2da194d96c4
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-11-25 11:37:07 -0800

    #75

--- code/reasoningtool/BuildMasterKG.py
+++ code/reasoningtool/BuildMasterKG.py
@@ -68,16 +68,17 @@ q1_diseases_dict = {'DOID:11476':   'osteoporosis',
                     'DOID:10923':   'sickle cell anemia',
                     'DOID:2055':    'post-traumatic stress disorder'}
 
-q2_mesh_to_diseases_look_aside_dict = {'MESH:D000855': 'DOID:8689',
-                                       'MESH:D016174': 'DOID:1883',
-                                       'MESH:D001007': 'DOID:2030',
-                                       'MESH:D056912': 'DOID:8670',
-                                       'MESH:D007024': 'DOID:10556',
-                                       'MESH:D010259': 'DOID:10938',
-                                       'MESH:D009169': 'DOID:399',
-                                       'MESH:D008414': 'DOID:10690',
-                                       'MESH:D014549': 'DOID:724',
-                                       'MESH:D016411': 'DOID:0050749'}
+q2_mesh_to_conditions_look_aside_dict = {'MESH:D000855': 'DOID:8689',
+                                         'MESH:D016174': 'DOID:1883',
+                                         'MESH:D001007': 'DOID:2030',
+                                         'MESH:D056912': 'DOID:8670',
+                                         'MESH:D007024': 'DOID:10556',
+                                         'MESH:D010259': 'DOID:10938',
+                                         'MESH:D009169': 'DOID:399',
+                                         'MESH:D008414': 'DOID:10690',
+                                         'MESH:D014549': 'DOID:724',
+                                         'MESH:D016411': 'DOID:0050749',
+                                         'MESH:D011247': 'HP:0002686'}
 
 ob.set_dict_reltype_dirs(master_rel_is_directed)
 ob.neo4j_set_url()
@@ -123,8 +124,8 @@ def convert_mesh_entrez_uid_to_curie_form(mesh_entrez_uid):
 
 human_phenont_name_id_dict = ParsePhenont.get_name_id_dict('../../hpo/hp.obo')
 
-def get_disont_ids_for_mesh_term(mesh_term):
-    ret_disont_ids = []
+def get_curie_ont_ids_for_mesh_term(mesh_term):
+    ret_curie_ids = []
     mesh_uids = QueryNCBIeUtils.get_mesh_uids_for_mesh_term(mesh_term)
     if len(mesh_uids) > 0:
         for mesh_uid in mesh_uids:
@@ -133,16 +134,20 @@ def get_disont_ids_for_mesh_term(mesh_term):
                 mesh_id_curie = convert_mesh_entrez_uid_to_curie_form(mesh_uid_int)
                 disont_ids = QuerySciGraph.get_disont_ids_for_mesh_id(mesh_id_curie)
                 if len(disont_ids) > 0:
-                    ret_disont_ids += disont_ids
+                    ret_curie_ids += disont_ids
                 else:
-                    lal_disont_id = q2_mesh_to_diseases_look_aside_dict.get(mesh_id_curie, None)
-                    if lal_disont_id is not None:
-                        ret_disont_ids += [lal_disont_id]
+                    
+                    lal_curie_id = q2_mesh_to_conditions_look_aside_dict.get(mesh_id_curie, None)
+                    if lal_curie_id is not None:
+                        ret_curie_ids += [lal_curie_id]
             else:
                 print('Got MeSH UID less than ' + str(MESH_ENTREZ_UID_BASE) + ': ' + mesh_uid + \
                       '; for MeSH term: ' + mesh_term, file=sys.stderr)
-
-    return ret_disont_ids
+    if len(ret_curie_ids)==0:
+        human_phenont_id = human_phenont_name_id_dict.get(mesh_term, None)
+        if human_phenont_id is not None:
+            ret_curie_ids.append(human_phenont_id)
+    return ret_curie_ids
     
 def seed_and_expand_kg_q2():
     
@@ -153,26 +158,34 @@ def seed_and_expand_kg_q2():
     
     first_row = True
     mesh_terms_set = set()
+    mesh_term_to_curie_ids_dict = dict()
+    curie_ids_for_df = []
     for index, row in drug_dis_df.iterrows():
         mesh_term = row['Condition']
         if mesh_term not in mesh_terms_set:
+            mesh_term_to_curie_ids_dict[mesh_term] = None
             mesh_terms_set.add(mesh_term)
-            disont_ids = get_disont_ids_for_mesh_term(mesh_term)
-            if len(disont_ids) > 0:
-                assert type(disont_ids)==list
-                for disont_id in disont_ids:
-                    disont_desc = QueryDisont.query_disont_to_label(disont_id)
-                    ob.add_node('disont_disease', disont_id, desc=disont_desc, seed_node_bool=first_row)
-                    first_row = False
-            else:
-                human_phenont_id = human_phenont_name_id_dict.get(mesh_term, None)
-                if human_phenont_id is not None:
-                    ob.add_node('phenont_phenotype', human_phenont_id, desc=mesh_term, seed_node_bool=first_row)
-                    first_row = False
-                else:
-                    print('Unable to get Disease Ontology ID or Human Phenotype Ontology ID for MeSH term: ' + mesh_term, file=sys.stdout)                        
-                    
+            curie_ids = get_curie_ont_ids_for_mesh_term(mesh_term)
+            if len(curie_ids) > 0:
+                assert type(curie_ids)==list
+                for curie_id in curie_ids:
+                    if 'DOID:' in curie_id:
+                        disont_desc = QueryDisont.query_disont_to_label(curie_id)
+                        ob.add_node('disont_disease', curie_id, desc=disont_desc, seed_node_bool=first_row)
+                        mesh_term_to_curie_ids_dict[mesh_term] = curie_id
+                        first_row = False
+                    else:
+                        if 'HP:' in curie_id:
+                            ob.add_node('phenont_phenotype', curie_id, desc=mesh_term, seed_node_bool=first_row)
+                            mesh_term_to_curie_ids_dict[mesh_term] = curie_id
+                            first_row = False
+                        else:
+                            assert False ## should never get here
+        curie_ids_for_df.append(mesh_term_to_curie_ids_dict[mesh_term])
+    drug_dis_df['CURIE_ID'] = pandas.Series(curie_ids_for_df, index=drug_dis_df.index)
+    drug_dis_df.to_csv('../../q2/q2-drugandcondition-list-mapped.txt', sep='\t')
 
+        
     ## triple-expand the knowledge graph
     bne.expand_all_nodes()
     bne.expand_all_nodes()
