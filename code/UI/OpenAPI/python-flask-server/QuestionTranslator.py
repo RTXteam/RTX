816db4ed8912cef107bbe88a55e81fc61b0b2024
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2017-11-27 01:26:05 +0000

    made the translate function a little more robust in the face of errant spaces and question marks and oter strange characters

--- code/UI/OpenAPI/python-flask-server/QuestionTranslator.py
+++ code/UI/OpenAPI/python-flask-server/QuestionTranslator.py
@@ -9,33 +9,45 @@ class QuestionTranslator:
 
     #print(question)
     print("Trying to translate question '"+text+"'")
+    originalText = text
+    text = re.sub("\?","", text)
+    text = re.sub("^\s+","", text)
+    text = re.sub("\s+$","", text)
+    text = text.lower()
 
-    match = re.match( "(Which|What) genetic conditions (might )*offer protection against (.+)[\?]*", text, re.I )
+    match = re.match( "[\;\|\>\<]", text )
+    if match:
+        query = [ { "knownQueryTypeId": "", "message": "Illegal characters in the question '"+text+"'", "restatedQuestion": "", "originalQuestion": originalText } ]
+        return(query)
+
+    match = re.match( "^(Which|What) genetic conditions (might )*offer protection against (.+)$", text, re.I )
     if match:
       term = match.group(3)
-      query = [ { "knownQueryTypeId": "Q1", "terms": [ term ], "restatedQuestion": "Which genetic conditions may offer protection against "+term+"?", "originalQuestion": text } ]
+      query = [ { "knownQueryTypeId": "Q1", "terms": [ term ], "restatedQuestion": "Which genetic conditions may offer protection against "+term+"?", "originalQuestion": originalText } ]
+      print(query)
       return(query)
 
-    match = re.match( "what is the clinical outcome pathway of (.+) for treatment of (.+)[\?]*", text, re.I )
+    match = re.match( "what is the clinical outcome pathway of (.+) for treatment of (.+)", text, re.I )
     if match:
       term1 = match.group(1)
       term2 = match.group(2)
-      query = [ { "knownQueryTypeId": "Q2", "terms": [ term1,term2 ], "restatedQuestion": "What is the clinical outcome pathway of "+term1+" for treatment of "+term2+"?", "originalQuestion": text } ]
+      query = [ { "knownQueryTypeId": "Q2", "terms": [ term1,term2 ], "restatedQuestion": "What is the clinical outcome pathway of "+term1+" for treatment of "+term2+"?", "originalQuestion": originalText } ]
+      print(query)
       return(query)
 
-    match = re.match( "(Which|What) protein(s)? does (.+) target[\?]*", text, re.I )
+    match = re.match( "(Which|What) protein(s)? does (.+) target", text, re.I )
     if match:
       term = match.group(3)
-      query = [ { "knownQueryTypeId": "Q3", "terms": [ term ], "restatedQuestion": "Which proteins does "+term+" target?", "originalQuestion": text } ]
+      query = [ { "knownQueryTypeId": "Q3", "terms": [ term ], "restatedQuestion": "Which proteins does "+term+" target?", "originalQuestion": originalText } ]
       return(query)
 
-    match = re.match( "what is (.+)[\?]*", text, re.I )
+    match = re.match( "what is (.+)", text, re.I )
     if match:
       term = match.group(1)
-      query = [ { "knownQueryTypeId": "Q0", "terms": [ term ], "restatedQuestion": "What is "+term+"?", "originalQuestion": text } ]
+      query = [ { "knownQueryTypeId": "Q0", "terms": [ term ], "restatedQuestion": "What is "+term+"?", "originalQuestion": originalText } ]
       return(query)
 
-    query = [ { "knownQueryTypeId": "", "message": "I do not understand the question '"+text+"'", "restatedQuestion": "", "originalQuestion": text } ]
+    query = [ { "knownQueryTypeId": "", "message": "I do not understand the question '"+text+"'", "restatedQuestion": "", "originalQuestion": originalText } ]
     return(query)
 
 
