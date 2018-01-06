a843cbe66f11dff537273692b3f07e7761b1f1c9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-01-05 16:09:00 -0800

    #128

--- code/docker/run_container_lysine.ncats.io.sh
+++ code/docker/run_container_lysine.ncats.io.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker run -it --expose 80 --expose 7473 --expose 7687 --name NCATS3 \
-           --network host --mount type=bind,source=/mnt/data,target=/mnt/data \
-           ncats3:version5 bash
+sudo docker run -td --expose 80 --expose 7473 --expose 7687 --name NCATS3 \
+                   --network host --mount type=bind,source=/mnt/data,target=/mnt/data \
+                   ncats3:version5
