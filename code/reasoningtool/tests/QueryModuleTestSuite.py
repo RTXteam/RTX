5b09454e2bb52a155333bffc7326ddd10689d562
Yao Yao <yaoy.cs@gmail.com>
2018-01-11 04:35:39 +0800

    added test cases to the test suite

--- code/reasoningtool/tests/QueryModuleTestSuite.py
+++ code/reasoningtool/tests/QueryModuleTestSuite.py
@@ -5,7 +5,15 @@ from tests.QueryDisGeNetTests import QueryDisGeNetTestCase
 from tests.QueryDisontTests import QueryDisontTestCase
 from tests.QueryGeneProfTests import QueryGeneProfTestCase
 from tests.QueryMiRBaseTests import QueryMiRBaseTestCase
-
+from tests.QueryMiRGateTests import QueryMiRGateTestCase
+from tests.QueryMyGeneTests import QueryMyGeneTestCase
+from tests.QueryNCBIeUtilsTests import QueryNCBIeUtilsTestCase
+from tests.QueryOMIMTests import QueryOMIMTestCase
+from tests.QueryPC2Tests import QueryPC2TestCase
+from tests.QueryPharosTests import QueryPharosTestCase
+from tests.QueryReactomeTests import QueryReactomeTestCase
+from tests.QuerySciGraphTests import QuerySciGraphTestCase
+from tests.QueryUniprotTests import QueryUniprotTestCase
 
 
 class QueryModuleTestSuite(unittest.TestSuite):
@@ -17,10 +25,28 @@ class QueryModuleTestSuite(unittest.TestSuite):
         suite.addTest(QueryDisGeNetTestCase())
         suite.addTest(QueryDisontTestCase())
         suite.addTest(QueryGeneProfTestCase())
+
         suite.addTest(QueryMiRBaseTestCase())
+        suite.addTest(QueryMiRGateTestCase())
+        suite.addTest(QueryMyGeneTestCase())
+        suite.addTest(QueryNCBIeUtilsTestCase())
+        suite.addTest(QueryOMIMTestCase())
+
+        suite.addTest(QueryPC2TestCase())
+        suite.addTest(QueryPharosTestCase())
+        suite.addTest(QueryReactomeTestCase())
+        suite.addTest(QuerySciGraphTestCase())
+        suite.addTest(QueryUniprotTestCase())
 
         self.run(suite)
 
 
 if __name__ == '__main__':
+    """
+    Run this module outside `tests` folder.
+    
+        John@Workstation ~/Documents/Git-repo/NCATS/code/reasoningtool (master)
+        $ python -m unittest tests/QueryModuleTestSuite.py
+
+    """
     unittest.main()
