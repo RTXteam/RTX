e38579524d9ae2f6a0fc2372df6d1b0a5723a3fe
dkoslicki <dmkoslicki@gmail.com>
2018-03-06 15:01:05 -0800

    add probabilites to output format

--- code/reasoningtool/QuestionsSuite.py
+++ code/reasoningtool/QuestionsSuite.py
@@ -11,22 +11,23 @@ def one_hop_relationship_type(source_name, target_label, relationship_type):
 		results_list.append(
 			{'type': 'node',
 	 		'name': target,
-	 		'desc': RU.get_node_property(target, "description", node_label=target_label)})
+	 		'desc': RU.get_node_property(target, "description", node_label=target_label),
+			 'prob': 1})  # All these are known to be true
 	return results_list
 
 #########################################################
 # Tests
 def test_one_hop_relationship_type():
 	res = one_hop_relationship_type("carbetocin", "uniprot_protein", "targets")
-	assert res == [{'desc': 'OXTR', 'name': 'P30559', 'type': 'node'}]
+	assert res == [{'desc': 'OXTR', 'name': 'P30559', 'type': 'node','prob': 1}]
 	res = one_hop_relationship_type("OMIM:263200", "uniprot_protein", "disease_affects")
-	known_res = [{'desc': 'PKHD1', 'name': 'P08F94', 'type': 'node'}, {'desc': 'DZIP1L', 'name': 'Q8IYY4', 'type': 'node'}]
+	known_res = [{'desc': 'PKHD1', 'name': 'P08F94', 'type': 'node','prob': 1}, {'desc': 'DZIP1L', 'name': 'Q8IYY4', 'type': 'node','prob': 1}]
 	for item in res:
 		assert item in known_res
 	for item in known_res:
 		assert item in res
 	res = one_hop_relationship_type("OMIM:263200", "ncbigene_microrna", "gene_assoc_with")
-	assert res == [{'desc': 'MIR1225', 'name': 'NCBIGene:100188847', 'type': 'node'}]
+	assert res == [{'desc': 'MIR1225', 'name': 'NCBIGene:100188847', 'type': 'node','prob': 1}]
 
 
 def test_suite():
