7f033d10f39288c021d7977b516af841ddc2c5e1
dkoslicki <dmkoslicki@gmail.com>
2018-03-09 15:59:00 -0800

    Experimental NLP-light question handler

---
+++ code/reasoningtool/QuestionAnswering/QuestionTranslator.py
@@ -0,0 +1,31 @@
+# An attempt at using rudimentary NLP for question translation
+import WordnetDistance as wd
+
+Q0_corpus = [
+	"What is an",
+	"What is a",
+
+]
+
+Q1_corpus = [
+
+]
+
+Q2_corpus = [
+	"what is the clinical outcome pathway of for the treatment",
+	"what is the clinical outcome pathway for the treatment of with ",
+	"what is the COP for the treatment of "
+]
+
+Q4_corpus = [
+		"What proteins are the target of",
+		"what proteins are targeted by",
+		"what proteins are in the pathway",
+		"what are the phenotypes of the disease",
+		"What are the symptoms of the disease",
+		"what micrornas control the expression of",
+		"what proteins are expressed in",
+		"what are the genes associated with",
+		"what are the drugs that target",
+]
+
