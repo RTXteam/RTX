1e8a796c84e353fd8692800e86338cec37c54706
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-03-26 16:54:43 -0700

    #36

--- code/reasoningtool/BioNetExpander.py
+++ code/reasoningtool/BioNetExpander.py
@@ -62,7 +62,7 @@ class BioNetExpander:
                 node_desc = ';'.join(list(gene_names))
                 target_node = self.orangeboard.add_node('uniprot_protein', target_uniprot_id, desc=node_desc)
                 self.orangeboard.add_rel('targets', 'ChEMBL', node, target_node, prob=probability)
-        targets = QueryPharos.query_drug_name_to_targets(drug_name)
+        targets = QueryPharos.query_drug_name_to_targets(drug_desc)
         if targets is not None:
             for target in targets:
                 uniprot_id = QueryPharos.query_target_uniprot_accession(str(target["id"]))
