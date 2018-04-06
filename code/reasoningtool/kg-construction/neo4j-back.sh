7ba53f37003d9ca3bcf093b44fe6d9d93c24efcd
DeqingQu <qu.deqing@gmail.com>
2018-04-05 20:25:40 -0700

    add set -e to script

--- code/reasoningtool/kg-construction/neo4j-back.sh
+++ code/reasoningtool/kg-construction/neo4j-back.sh
@@ -1,8 +1,16 @@
+#!/bin/sh
+
+### BEGIN INTRO
 #   This script is to dump the Neo4j database and transfer the backup file to http://rtxkgdump.saramsey.org/
 #   If you want to run the script, you can log in to the 'rtxsteve.saramesy.org' instance, then log in to the 'kgdump' docker container
 #   As the root user of 'kgdump' docker container, you can run the script in any folder using 'sh neo4j-back.sh'
 #   The backup files will be stored in the /var/www/html folder of the 'rtxkgdump.saramsey.org' instance.
 #   The backup files can be accessed in http://rtxkgdump.saramsey.org/
+### END INTRO
+
+# Author: Deqing Qu
+
+set -e
 
 file=`date '+%m%d%y-%H%M%S'`
 
