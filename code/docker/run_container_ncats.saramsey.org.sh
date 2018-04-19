b09b1323b7bcac75f91329c61431425575e736b0
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-04-18 21:19:36 -0700

    reorg

--- code/docker/run_container_ncats.saramsey.org.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker run -td --expose 80 --expose 7473 --expose 7687 --expose 7474 --name NCATS \
-                    --network host --mount type=bind,source=/mnt/data,target=/mnt/data \
-                    ncats3:version4
-
