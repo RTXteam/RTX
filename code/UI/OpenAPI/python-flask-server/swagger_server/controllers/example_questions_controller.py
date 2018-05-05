295187a43187bbf2895ec40458c7df6cf553dd03
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-05 04:55:35 +0000

    new controller for listing example questions

---
+++ code/UI/OpenAPI/python-flask-server/swagger_server/controllers/example_questions_controller.py
@@ -0,0 +1,15 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def example_questions():  # noqa: E501
+    """Request a list of example questions that RTX can answer
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
