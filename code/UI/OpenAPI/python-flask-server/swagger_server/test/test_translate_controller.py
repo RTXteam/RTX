db9a3a04c377f32149a84335158de97f0c23ee78
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-07 17:56:33 +0000

    updates to autogen tests

--- code/UI/OpenAPI/python-flask-server/swagger_server/test/test_translate_controller.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/test/test_translate_controller.py
@@ -2,28 +2,30 @@
 
 from __future__ import absolute_import
 
-from swagger_server.models.query import Query
-from swagger_server.models.question import Question
-from . import BaseTestCase
-from six import BytesIO
 from flask import json
+from six import BytesIO
+
+from swagger_server.models.query import Query  # noqa: E501
+from swagger_server.models.question import Question  # noqa: E501
+from swagger_server.test import BaseTestCase
 
 
 class TestTranslateController(BaseTestCase):
-    """ TranslateController integration test stubs """
+    """TranslateController integration test stubs"""
 
     def test_translate(self):
-        """
-        Test case for translate
+        """Test case for translate
 
         Translate natural language question into a standardized query
         """
         body = Question()
-        response = self.client.open('/api/rtx/v1/translate',
-                                    method='POST',
-                                    data=json.dumps(body),
-                                    content_type='application/json')
-        self.assert200(response, "Response body is : " + response.data.decode('utf-8'))
+        response = self.client.open(
+            '/api/rtx/v1/translate',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
