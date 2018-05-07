7f5f43e4127243c1ae559a3335b8f3c750e901b8
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-07 17:49:42 +0000

    new feedback controller

---
+++ code/UI/OpenAPI/python-flask-server/swagger_server/controllers/feedback_controller.py
@@ -0,0 +1,37 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def get_feedback_all():  # noqa: E501
+    """Request a list of all feedback provided thus far
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def get_feedback_expertise_levels():  # noqa: E501
+    """Request a list of allowable expertise levels
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def get_feedback_ratings():  # noqa: E501
+    """Request a list of allowable ratings
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
