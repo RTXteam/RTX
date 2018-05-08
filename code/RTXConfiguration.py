6bd0c36193a1e2bf7532a7d6da39af0d19b2c329
dkoslicki <dmkoslicki@gmail.com>
2018-05-08 16:19:31 -0700

    Update with production and KG2 connection information. See #119

--- code/RTXConfiguration.py
+++ code/RTXConfiguration.py
@@ -7,24 +7,40 @@ import datetime
 
 class RTXConfiguration:
 
-  #### Constructor
-  def __init__(self):
-    self.version = "RTX 0.4.1"
+	#### Constructor
+	def __init__(self):
+		self.version = "RTX 0.4.1"
+
+		# This is the flag/property to switch between the two containers
+		self.live = "Production"
+		#self.live = "KG2"
+
+		if self.live == "Production":
+			self.bolt = "bolt://rtx.ncats.io:7687"
+			self.database = "rtx.ncats.io:7474/db/data"
+
+		if self.live == "KG2":
+			self.bolt = "bolt://rtx.ncats.io:7787"
+			self.database = "rtx.ncats.io:7574/db/data"
 
 
-  #### Define attribute version
-  @property
-  def version(self) -> str:
-    return self._version
 
-  @version.setter
-  def version(self, version: str):
-    self._version = version
+	#### Define attribute version
+	@property
+	def version(self) -> str:
+		return self._version
+
+	@version.setter
+	def version(self, version: str):
+		self._version = version
 
 
 def main():
-  rtxConfig = RTXConfiguration()
-  print("RTX Version string: " + rtxConfig.version)
+	rtxConfig = RTXConfiguration()
+	print("RTX Version string: " + rtxConfig.version)
+	print("live version: %s" % rtxConfig.live)
+	print("bolt protocol: %s" % rtxConfig.bolt)
+	print("database: %s" % rtxConfig.database)
 
 
 if __name__ == "__main__": main()
