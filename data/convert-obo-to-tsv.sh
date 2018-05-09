2ddd6052a17218b1763f078d9a87ee657fb0e0db
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-05-09 10:58:42 -0700

    #48

--- data/convert-obo-to-tsv.sh
+++ data/convert-obo-to-tsv.sh
@@ -2,9 +2,12 @@
 
 # to be blamed for this script:  Stephen Ramsey
 
-rep -v 'alt_id\:' go.obo | \
+egrep -v 'def\:|synonym\:|alt_id\:|is_a\:|comment\:' go.obo | \
     egrep 'namespace\:|id\:|name\:' | \
     /usr/local/bin/gtr --delete "\n" | \
-    /usr/local/bin/sed 's/name\:/\t/g' | \
-    /usr/local/bin/sed 's/id\:/\n/g' | \
-    /usr/local/bin/sed 's/namespace\:/\t/g' > go.tsv
+    /usr/local/bin/sed 's/name\: /\t/g' | \
+    /usr/local/bin/sed 's/id\: /\n/g' | \
+    /usr/local/bin/sed 's/namespace\: /\t/g' | \
+    grep 'GO:' | \
+    awk -F"\t" '{print $3 "\t" $1 "\t" $2 "\tgeneric" }' > go.tsv
+
