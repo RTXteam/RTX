88cd509a5c8c4b2076adb2188815d6ce6e6c9cb7
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-11 18:10:30 +0000

    new models and controllers from the new yaml

---
+++ code/UI/OpenAPI/python-flask-server/swagger_server/controllers/result_controller.py
@@ -0,0 +1,58 @@
+import connexion
+import six
+
+from swagger_server.models.response import Response  # noqa: E501
+from swagger_server.models.result import Result  # noqa: E501
+from swagger_server.models.result_feedback import ResultFeedback  # noqa: E501
+from swagger_server import util
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../Feedback/")
+from RTXFeedback import RTXFeedback
+
+
+def get_result(result_id):  # noqa: E501
+    """Request stored result
+
+     # noqa: E501
+
+    :param result_id: Integer identifier of the result to return
+    :type result_id: int
+
+    :rtype: Result
+    """
+    return 'do some magic!'
+
+
+def get_result_feedback(result_id):  # noqa: E501
+    """Request stored feedback for this result
+
+     # noqa: E501
+
+    :param result_id: Integer identifier of the result to return
+    :type result_id: int
+
+    :rtype: ResultFeedback
+    """
+    rtxFeedback = RTXFeedback()
+    return rtxFeedback.getResultFeedback(response_id, result_id)
+
+
+def post_result_feedback(result_id):  # noqa: E501
+    """Store feedback for a particular result
+
+     # noqa: E501
+
+    :param result_id: Integer identifier of the result to return
+    :type result_id: int
+
+    :rtype: Response
+    """
+    if connexion.request.is_json:
+        ratingData = connexion.request.get_json()  # noqa: E501
+        rtxFeedback = RTXFeedback()
+        response = rtxFeedback.addNewResultRating(result_id, ratingData)
+        return(response)
+    return( { "status": 502, "title": "body content not JSON", "detail": "Required body content is not JSON", "type": "about:blank" }, 502 )
+
