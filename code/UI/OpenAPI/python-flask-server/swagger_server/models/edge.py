6770a69ff14b78190ff96cc5b5fb9155d1abfd23
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-11 18:07:46 +0000

    regenerated from new yaml file

--- code/UI/OpenAPI/python-flask-server/swagger_server/models/edge.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/models/edge.py
@@ -6,7 +6,7 @@ from datetime import date, datetime  # noqa: F401
 from typing import List, Dict  # noqa: F401
 
 from swagger_server.models.base_model_ import Model
-from swagger_server.models.origin import Origin  # noqa: F401,E501
+from swagger_server.models.edge_attribute import EdgeAttribute  # noqa: F401,E501
 from swagger_server import util
 
 
@@ -16,41 +16,76 @@ class Edge(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, type: str=None, source_id: str=None, target_id: str=None, confidence: float=None, origin_list: List[Origin]=None):  # noqa: E501
+    def __init__(self, type: str=None, relation: str=None, source_id: str=None, target_id: str=None, is_defined_by: str=None, provided_by: str=None, confidence: float=None, publications: str=None, evidence_type: str=None, qualifiers: str=None, negated: bool=None, attribute_list: List[EdgeAttribute]=None):  # noqa: E501
         """Edge - a model defined in Swagger
 
         :param type: The type of this Edge.  # noqa: E501
         :type type: str
+        :param relation: The relation of this Edge.  # noqa: E501
+        :type relation: str
         :param source_id: The source_id of this Edge.  # noqa: E501
         :type source_id: str
         :param target_id: The target_id of this Edge.  # noqa: E501
         :type target_id: str
+        :param is_defined_by: The is_defined_by of this Edge.  # noqa: E501
+        :type is_defined_by: str
+        :param provided_by: The provided_by of this Edge.  # noqa: E501
+        :type provided_by: str
         :param confidence: The confidence of this Edge.  # noqa: E501
         :type confidence: float
-        :param origin_list: The origin_list of this Edge.  # noqa: E501
-        :type origin_list: List[Origin]
+        :param publications: The publications of this Edge.  # noqa: E501
+        :type publications: str
+        :param evidence_type: The evidence_type of this Edge.  # noqa: E501
+        :type evidence_type: str
+        :param qualifiers: The qualifiers of this Edge.  # noqa: E501
+        :type qualifiers: str
+        :param negated: The negated of this Edge.  # noqa: E501
+        :type negated: bool
+        :param attribute_list: The attribute_list of this Edge.  # noqa: E501
+        :type attribute_list: List[EdgeAttribute]
         """
         self.swagger_types = {
             'type': str,
+            'relation': str,
             'source_id': str,
             'target_id': str,
+            'is_defined_by': str,
+            'provided_by': str,
             'confidence': float,
-            'origin_list': List[Origin]
+            'publications': str,
+            'evidence_type': str,
+            'qualifiers': str,
+            'negated': bool,
+            'attribute_list': List[EdgeAttribute]
         }
 
         self.attribute_map = {
             'type': 'type',
+            'relation': 'relation',
             'source_id': 'source_id',
             'target_id': 'target_id',
+            'is_defined_by': 'is_defined_by',
+            'provided_by': 'provided_by',
             'confidence': 'confidence',
-            'origin_list': 'origin_list'
+            'publications': 'publications',
+            'evidence_type': 'evidence_type',
+            'qualifiers': 'qualifiers',
+            'negated': 'negated',
+            'attribute_list': 'attribute_list'
         }
 
         self._type = type
+        self._relation = relation
         self._source_id = source_id
         self._target_id = target_id
+        self._is_defined_by = is_defined_by
+        self._provided_by = provided_by
         self._confidence = confidence
-        self._origin_list = origin_list
+        self._publications = publications
+        self._evidence_type = evidence_type
+        self._qualifiers = qualifiers
+        self._negated = negated
+        self._attribute_list = attribute_list
 
     @classmethod
     def from_dict(cls, dikt) -> 'Edge':
@@ -67,7 +102,7 @@ class Edge(Model):
     def type(self) -> str:
         """Gets the type of this Edge.
 
-        Entity type of this response  # noqa: E501
+        Higher-level relationship type of this edge  # noqa: E501
 
         :return: The type of this Edge.
         :rtype: str
@@ -78,13 +113,36 @@ class Edge(Model):
     def type(self, type: str):
         """Sets the type of this Edge.
 
-        Entity type of this response  # noqa: E501
+        Higher-level relationship type of this edge  # noqa: E501
 
         :param type: The type of this Edge.
         :type type: str
         """
 
         self._type = type
+
+    @property
+    def relation(self) -> str:
+        """Gets the relation of this Edge.
+
+        Lower-level relationship type of this edge  # noqa: E501
+
+        :return: The relation of this Edge.
+        :rtype: str
+        """
+        return self._relation
+
+    @relation.setter
+    def relation(self, relation: str):
+        """Sets the relation of this Edge.
+
+        Lower-level relationship type of this edge  # noqa: E501
+
+        :param relation: The relation of this Edge.
+        :type relation: str
+        """
+
+        self._relation = relation
 
     @property
     def source_id(self) -> str:
@@ -133,6 +191,52 @@ class Edge(Model):
         self._target_id = target_id
 
     @property
+    def is_defined_by(self) -> str:
+        """Gets the is_defined_by of this Edge.
+
+        A CURIE/URI for the translator group that made the KG  # noqa: E501
+
+        :return: The is_defined_by of this Edge.
+        :rtype: str
+        """
+        return self._is_defined_by
+
+    @is_defined_by.setter
+    def is_defined_by(self, is_defined_by: str):
+        """Sets the is_defined_by of this Edge.
+
+        A CURIE/URI for the translator group that made the KG  # noqa: E501
+
+        :param is_defined_by: The is_defined_by of this Edge.
+        :type is_defined_by: str
+        """
+
+        self._is_defined_by = is_defined_by
+
+    @property
+    def provided_by(self) -> str:
+        """Gets the provided_by of this Edge.
+
+        A CURIE/URI for the knowledge source that defined this edge  # noqa: E501
+
+        :return: The provided_by of this Edge.
+        :rtype: str
+        """
+        return self._provided_by
+
+    @provided_by.setter
+    def provided_by(self, provided_by: str):
+        """Sets the provided_by of this Edge.
+
+        A CURIE/URI for the knowledge source that defined this edge  # noqa: E501
+
+        :param provided_by: The provided_by of this Edge.
+        :type provided_by: str
+        """
+
+        self._provided_by = provided_by
+
+    @property
     def confidence(self) -> float:
         """Gets the confidence of this Edge.
 
@@ -156,24 +260,116 @@ class Edge(Model):
         self._confidence = confidence
 
     @property
-    def origin_list(self) -> List[Origin]:
-        """Gets the origin_list of this Edge.
+    def publications(self) -> str:
+        """Gets the publications of this Edge.
 
-        A list of origins (sources) for this edge and attributes from those origins  # noqa: E501
+        A CURIE/URI for publications associated with this edge  # noqa: E501
 
-        :return: The origin_list of this Edge.
-        :rtype: List[Origin]
+        :return: The publications of this Edge.
+        :rtype: str
         """
-        return self._origin_list
+        return self._publications
 
-    @origin_list.setter
-    def origin_list(self, origin_list: List[Origin]):
-        """Sets the origin_list of this Edge.
+    @publications.setter
+    def publications(self, publications: str):
+        """Sets the publications of this Edge.
 
-        A list of origins (sources) for this edge and attributes from those origins  # noqa: E501
+        A CURIE/URI for publications associated with this edge  # noqa: E501
 
-        :param origin_list: The origin_list of this Edge.
-        :type origin_list: List[Origin]
+        :param publications: The publications of this Edge.
+        :type publications: str
         """
 
-        self._origin_list = origin_list
+        self._publications = publications
+
+    @property
+    def evidence_type(self) -> str:
+        """Gets the evidence_type of this Edge.
+
+        A CURIE/URI for class of evidence supporting the statement made in an edge - typically a class from the ECO ontology  # noqa: E501
+
+        :return: The evidence_type of this Edge.
+        :rtype: str
+        """
+        return self._evidence_type
+
+    @evidence_type.setter
+    def evidence_type(self, evidence_type: str):
+        """Sets the evidence_type of this Edge.
+
+        A CURIE/URI for class of evidence supporting the statement made in an edge - typically a class from the ECO ontology  # noqa: E501
+
+        :param evidence_type: The evidence_type of this Edge.
+        :type evidence_type: str
+        """
+
+        self._evidence_type = evidence_type
+
+    @property
+    def qualifiers(self) -> str:
+        """Gets the qualifiers of this Edge.
+
+        Terms representing qualifiers that modify or qualify the meaning of the statement made in an edge  # noqa: E501
+
+        :return: The qualifiers of this Edge.
+        :rtype: str
+        """
+        return self._qualifiers
+
+    @qualifiers.setter
+    def qualifiers(self, qualifiers: str):
+        """Sets the qualifiers of this Edge.
+
+        Terms representing qualifiers that modify or qualify the meaning of the statement made in an edge  # noqa: E501
+
+        :param qualifiers: The qualifiers of this Edge.
+        :type qualifiers: str
+        """
+
+        self._qualifiers = qualifiers
+
+    @property
+    def negated(self) -> bool:
+        """Gets the negated of this Edge.
+
+        Boolean that if set to true, indicates the edge statement is negated i.e. is not true  # noqa: E501
+
+        :return: The negated of this Edge.
+        :rtype: bool
+        """
+        return self._negated
+
+    @negated.setter
+    def negated(self, negated: bool):
+        """Sets the negated of this Edge.
+
+        Boolean that if set to true, indicates the edge statement is negated i.e. is not true  # noqa: E501
+
+        :param negated: The negated of this Edge.
+        :type negated: bool
+        """
+
+        self._negated = negated
+
+    @property
+    def attribute_list(self) -> List[EdgeAttribute]:
+        """Gets the attribute_list of this Edge.
+
+        A list of additional attributes for this edge  # noqa: E501
+
+        :return: The attribute_list of this Edge.
+        :rtype: List[EdgeAttribute]
+        """
+        return self._attribute_list
+
+    @attribute_list.setter
+    def attribute_list(self, attribute_list: List[EdgeAttribute]):
+        """Sets the attribute_list of this Edge.
+
+        A list of additional attributes for this edge  # noqa: E501
+
+        :param attribute_list: The attribute_list of this Edge.
+        :type attribute_list: List[EdgeAttribute]
+        """
+
+        self._attribute_list = attribute_list
