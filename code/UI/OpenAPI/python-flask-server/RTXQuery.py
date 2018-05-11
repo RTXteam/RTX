bd1a85f233a475c18dbfd8ff876708418556f35f
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-11 23:28:07 +0000

    fix config path

--- code/UI/OpenAPI/python-flask-server/RTXQuery.py
+++ code/UI/OpenAPI/python-flask-server/RTXQuery.py
@@ -12,7 +12,7 @@ import json
 import datetime
 import ast
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../")
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../")
 from RTXConfiguration import RTXConfiguration
 
 from QueryMeSH import QueryMeSH
