0a130b936436d7295e533aaa5fc86eee0bc5c9de
Eric Deutsch <edeutsch@users.noreply.github.com>
2018-05-14 20:36:56 -0700

    finished class to provide list of example questions

--- code/reasoningtool/QuestionAnswering/QuestionExamples.py
+++ code/reasoningtool/QuestionAnswering/QuestionExamples.py
@@ -8,24 +8,32 @@ class QuestionExamples:
 	def __init__(self):
 		self.read_reference_file()
 
+	#### Define attribute questions
+	@property
+	def questions(self) -> str:
+		return self._questions
 
-	def read_reference_file(self)
+	@questions.setter
+	def questions(self, questions: list):
+		self._questions = questions
+
+	def read_reference_file(self):
 		with open(os.path.join(os.path.dirname(__file__), 'QuestionExamples.tsv'), 'r') as fid:
 			questions = []
 			for line in fid.readlines():
 				if line[0] == "#":
 					pass
 				else:
-					questions.append(line)
+					columns = line.split("\t")
+					if columns[1] == "true":
+						question = { "query_type_id": columns[0], "question_text": columns[2] }
+					questions.append(question)
 			self._questions = questions
 
 
-	def get_questions(self)
-		pass
-
 def main():
 	questions = QuestionExamples()
-
+	print(questions.questions)
 
 def tests():
 	questions = QuestionExamples()
