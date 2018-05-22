09cddaa39fe9cc86ca83e51a40c17a900c1ac86e
Eric Deutsch <edeutsch@systemsbiology.org>
2018-05-22 02:43:50 +0000

    fix PATH prior to import

--- code/UI/OpenAPI/python-flask-server/swagger_server/controllers/example_questions_controller.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/controllers/example_questions_controller.py
@@ -5,7 +5,7 @@ from swagger_server import util
 
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../reasoningtool/QuestionAnswering/")
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../../reasoningtool/QuestionAnswering/")
 from QuestionExamples import QuestionExamples
 
 
