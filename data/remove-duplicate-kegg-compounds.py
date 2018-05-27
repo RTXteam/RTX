38ab17d0eba208246d78fc43ea8ceefe1326dc50
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-05-27 11:42:27 -0700

    #212

--- data/remove-duplicate-kegg-compounds.py
+++ data/remove-duplicate-kegg-compounds.py
@@ -9,6 +9,7 @@ def get_kegg_for_chembl(chembl_id_str):
     url = 'http://mychem.info/v1/chem/' + chembl_id_str
     res = requests.get(url)
     res_kegg = None
+    res_brand_names = None
     if res is not None:
         status_code = res.status_code
         if status_code != 200:
@@ -21,8 +22,9 @@ def get_kegg_for_chembl(chembl_id_str):
                 if res_chebi is not None:
                     res_kegg = res_chebi.get("kegg_compound_database_links", None)
                     if res_kegg is not None:
-                        ret_str = res_kegg
-    return res_kegg
+                        res_brand_names = res_chebi.get("brand_names", None)
+
+    return [res_kegg, res_brand_names]
 
 
 def do_conversion():
@@ -31,34 +33,31 @@ def do_conversion():
                                      names=['type', 'rtx_name', 'term', 'purpose'],
                                      header=0,
                                      dtype={'rtx_name': str})
-    first_row = True
 
     chemical_substance_terms = set()
-    kegg_ids = set()
-    
+    kegg_ids = dict()
+
     for index, row in seed_node_data.iterrows():
         entity_type = row['type']
         term = row['term']
         id = row['rtx_name']
         if entity_type == 'chemical_substance':
             chemical_substance_terms.add(term)
-            kegg_id = get_kegg_for_chembl(id)
+            kegg_id, brand_names = get_kegg_for_chembl(id)
             if kegg_id is not None:
                 if (type(kegg_id) == str):
-                    kegg_ids.add(kegg_id)
+                    kegg_ids[kegg_id] = brand_names
                 else:
                     if (type(kegg_id) == list):
-                        kegg_ids.update(kegg_id)
+                        for single_id in kegg_id:
+                            kegg_ids[single_id] = brand_names
         else:
             if entity_type == 'metabolite':
-                if term.lower() in chemical_substance_terms:
+                # need to check if the ID maps
+                assert id.startswith('KEGG:')
+                kegg_compound_id = id.split('KEGG:')[1]
+                if kegg_compound_id in kegg_ids and kegg_ids[kegg_compound_id] is not None:
                     continue
-                else:
-                    # need to check if the ID maps
-                    assert id.startswith('KEGG:')
-                    kegg_compound_id = id.split('KEGG:')[1]
-                    if kegg_compound_id in kegg_ids:
-                        continue
         print(entity_type + "\t" + id + "\t" + term + "\t" + row['purpose'])
 
 do_conversion()
