2f39fe4ccfdf2b7b3d43faabce8a31e3ff6d17a2
DeqingQu <qu.deqing@gmail.com>
2018-06-19 12:09:30 -0700

    #245 get_cuis_from_string_precision

--- code/reasoningtool/SemMedDB/QueryUMLSApi.py
+++ code/reasoningtool/SemMedDB/QueryUMLSApi.py
@@ -15,80 +15,97 @@ import time
 from io import StringIO
 import re
 import pandas
-#import CachedMethods
-#import requests_cache
+# import CachedMethods
+import requests_cache
 import lxml.html as lh
 from lxml.html import fromstring
 
 
 class QueryUMLS:
-	TIMEOUT_SEC = 120
-	API_BASE_URL = 'https://uts-ws.nlm.nih.gov/rest'
-	Ticket_URL = 'https://utslogin.nlm.nih.gov'
-	auth_endpoint = "/cas/v1/api-key"
-	api_key = '98b04ff2-e69d-4177-889b-d5fbcf8a9114'
+    TIMEOUT_SEC = 120
+    API_BASE_URL = 'https://uts-ws.nlm.nih.gov/rest'
+    Ticket_URL = 'https://utslogin.nlm.nih.gov'
+    auth_endpoint = "/cas/v1/api-key"
+    api_key = '98b04ff2-e69d-4177-889b-d5fbcf8a9114'
 
+    @staticmethod
+    def get_ticket_gen():
+        # params = {'username': self.username,'password': self.password}
+        params = {'apikey': QueryUMLS.api_key}
+        h = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "User-Agent": "python"}
+        r = requests.post(QueryUMLS.Ticket_URL + QueryUMLS.auth_endpoint, data=params, headers=h)
+        response = fromstring(r.text)
+        ## extract the entire URL needed from the HTML form (action attribute) returned - looks similar to https://utslogin.nlm.nih.gov/cas/v1/tickets/TGT-36471-aYqNLN2rFIJPXKzxwdTNC5ZT7z3B3cTAKfSc5ndHQcUxeaDOLN-cas
+        ## we make a POST call to this URL in the getst method
+        tgt = response.xpath('//form/@action')[0]
+        return tgt
 
-	@staticmethod
-	def get_ticket_gen():
-		#params = {'username': self.username,'password': self.password}
-		params = {'apikey': QueryUMLS.api_key}
-		h = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "User-Agent":"python" }
-		r = requests.post(QueryUMLS.Ticket_URL+QueryUMLS.auth_endpoint,data=params,headers=h)
-		response = fromstring(r.text)
-		## extract the entire URL needed from the HTML form (action attribute) returned - looks similar to https://utslogin.nlm.nih.gov/cas/v1/tickets/TGT-36471-aYqNLN2rFIJPXKzxwdTNC5ZT7z3B3cTAKfSc5ndHQcUxeaDOLN-cas
-		## we make a POST call to this URL in the getst method
-		tgt = response.xpath('//form/@action')[0]
-		return tgt
+    @staticmethod
+    def get_single_ticket(tgt):
+        params = {'service': "http://umlsks.nlm.nih.gov"}
+        h = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "User-Agent": "python"}
+        r = requests.post(tgt, data=params, headers=h)
+        st = r.text
+        return st
 
-	@staticmethod
-	def get_single_ticket(tgt):
-		params = {'service': "http://umlsks.nlm.nih.gov"}
-		h = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "User-Agent":"python" }
-		r = requests.post(tgt,data=params,headers=h)
-		st = r.text
-		return st
+    @staticmethod
+    def send_query_get(handler, st):
+        url_str = QueryUMLS.API_BASE_URL + '/' + handler + '&ticket=' + st
+        try:
+            res = requests.get(url_str, headers={'accept': 'application/json'}, timeout=QueryUMLS.TIMEOUT_SEC)
+        except requests.exceptions.Timeout:
+            print('HTTP timeout in QueryNCBIeUtils.py; URL: ' + url_str, file=sys.stderr)
+            time.sleep(1)  ## take a timeout because NCBI rate-limits connections
+            return None
+        except requests.exceptions.ConnectionError:
+            print('HTTP connection error in QueryNCBIeUtils.py; URL: ' + url_str, file=sys.stderr)
+            time.sleep(1)  ## take a timeout because NCBI rate-limits connections
+            return None
+        status_code = res.status_code
+        if status_code != 200:
+            print('HTTP response status code: ' + str(status_code) + ' for URL:\n' + url_str, file=sys.stderr)
+            res = None
+        return res
 
-	@staticmethod
-	def send_query_get(handler, st):
-		url_str = QueryUMLS.API_BASE_URL + '/' + handler + '&ticket=' + st
-		try:
-			res = requests.get(url_str, headers={'accept': 'application/json'}, timeout=QueryUMLS.TIMEOUT_SEC)
-		except requests.exceptions.Timeout:
-			print('HTTP timeout in QueryNCBIeUtils.py; URL: ' + url_str, file=sys.stderr)
-			time.sleep(1)  ## take a timeout because NCBI rate-limits connections
-			return None
-		except requests.exceptions.ConnectionError:
-			print('HTTP connection error in QueryNCBIeUtils.py; URL: ' + url_str, file=sys.stderr)
-			time.sleep(1)  ## take a timeout because NCBI rate-limits connections
-			return None
-		status_code = res.status_code
-		if status_code != 200:
-			print('HTTP response status code: ' + str(status_code) + ' for URL:\n' + url_str, file=sys.stderr)
-			res = None
-		return res
+    @staticmethod
+    def get_synonyms_from_string(string, tgt):
+        st = QueryUMLS.get_single_ticket(tgt)
+        query = '/search/current?string=' + string + '&pageSize=10000'
+        r = QueryUMLS.send_query_get(query, st)
+        if r is not None:
+            return [a['name'] for a in r.json()['result']['results']]
+        else:
+            return None
 
-	@staticmethod
-	def get_synonyms_from_string(string,tgt):
-		st = QueryUMLS.get_single_ticket(tgt)
-		query = '/search/current?string=' + string + '&pageSize=10000'
-		r=QueryUMLS.send_query_get(query,st)
-		if r is not None:
-			return [a['name'] for a in r.json()['result']['results']]
-		else:
-			return None
+    @staticmethod
+    def get_cuis_from_string(string, tgt):
+        st = QueryUMLS.get_single_ticket(tgt)
+        query = '/search/current?string=' + string + '&pageSize=10000'
+        r = QueryUMLS.send_query_get(query, st)
+        if r is not None:
+            return [a['ui'] for a in r.json()['result']['results']]
+        else:
+            return None
 
-	@staticmethod
-	def get_cuis_from_string(string,tgt):
-		st = QueryUMLS.get_single_ticket(tgt)
-		query = '/search/current?string=' + string + '&pageSize=10000'
-		r=QueryUMLS.send_query_get(query,st)
-		if r is not None:
-			return [a['ui'] for a in r.json()['result']['results']]
-		else:
-			return None
+    @staticmethod
+    def get_cuis_from_string_precision(string, tgt):
+        cuis = set()
+        st = QueryUMLS.get_single_ticket(tgt)
+        query = '/search/current?string=' + string + '&pageSize=10000'
+        r = QueryUMLS.send_query_get(query, st)
+        if r is not None:
+            res_json = r.json()
+            if 'result' in res_json.keys():
+                if 'results' in res_json['result'].keys():
+                    for res_obj in res_json['result']['results']:
+                        if 'name' in res_obj.keys() and 'ui' in res_obj.keys():
+                            if res_obj['name'].lower() == string.lower():
+                                print(res_obj)
+                                cuis.add(res_obj['ui'])
+        return cuis
 
-
-if __name__=='__main__':
-	tgt = QueryUMLS.get_ticket_gen()
-	print(QueryUMLS.get_cuis_from_string('log15',tgt))
+if __name__ == '__main__':
+    tgt = QueryUMLS.get_ticket_gen()
+    print(QueryUMLS.get_cuis_from_string('log15', tgt))
+    print(QueryUMLS.get_cuis_from_string('pain', tgt))
+    print(QueryUMLS.get_cuis_from_string_precision('pain', tgt))
