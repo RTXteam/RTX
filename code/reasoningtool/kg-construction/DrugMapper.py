bc29dd09cb7f0fbd4cb522353dc764422fb4b66e
DeqingQu <qu.deqing@gmail.com>
2018-06-19 16:26:25 -0700

    #245, redefine map_drug_to_ontology

--- code/reasoningtool/kg-construction/DrugMapper.py
+++ code/reasoningtool/kg-construction/DrugMapper.py
@@ -38,63 +38,82 @@ class DrugMapper:
         return hp_set
 
     @staticmethod
-    def map_drug_to_UMLS_with_indications(chembl_id):
+    def map_drug_to_UMLS(chembl_id):
         """
-        mapping between a drug and UMLS ids corresponding to indications
+        mapping between a drug and UMLS ids corresponding to indications and contraindications
 
         :param chembl_id: The CHEMBL ID for a drug
 
-        :return: A set of strings containing the found UMLS ids or empty set if none where found
+        :return: A dictionary with two fields ('indication' and 'contraindication'). Each field is a set of strings
+                containing the found UMLS ids or empty set if none were found
         """
-        umls_set = set()
+        indication_umls_set = set()
+        contraindication_umls_set = set()
         if not isinstance(chembl_id, str):
-            return umls_set
-        indications = QueryMyChem.get_drug_indications(chembl_id)
-        tgt = QueryUMLS.get_ticket_gen()
+            return {'indications': indication_umls_set, "contraindications": contraindication_umls_set}
+        drug_use = QueryMyChem.get_drug_use(chembl_id)
+        indications = drug_use['indications']
+        contraindications = drug_use['contraindications']
 
+        sm = SynonymMapper()
         for indication in indications:
-            if 'snomed_name' in indication.keys():
-                cuis = QueryUMLS.get_cuis_from_string_precision(indication['snomed_name'], tgt)
-                for cui in cuis:
-                    umls_set.add(cui)
-        return umls_set
+            if 'snomed_id' in indication.keys():
+                oxo_result = sm.get_all_from_oxo('SNOMEDCT:' + indication['snomed_id'], ['UMLS'])
+                if oxo_result is not None:
+                    indication_umls_set.add(oxo_result[0])
 
-        # print(umls_set)
-        # if len(umls_set) == 0:
-        #     return hp_set
-        # sm = SynonymMapper()
-        # for umls_id in umls_set:
-        #     hp_ids = sm.get_all_from_oxo(umls_id, 'HP')
-        #     if hp_ids != None:
-        #         for hp_id in hp_ids:
-        #             hp_set.add(hp_id)
-        # return hp_set
+        for contraindication in contraindications:
+            if 'snomed_id' in contraindication.keys():
+                oxo_result = sm.get_all_from_oxo('SNOMEDCT:' + contraindication['snomed_id'], ['UMLS'])
+                if oxo_result is not None:
+                    contraindication_umls_set.add(oxo_result[0])
+
+        return {'indications': indication_umls_set, "contraindications": contraindication_umls_set}
+
+        # tgt = QueryUMLS.get_ticket_gen()
+        # for indication in indications:
+        #     if 'snomed_name' in indication.keys():
+        #         cui = QueryUMLS.get_cui_from_string_precision(indication['snomed_name'], tgt)
+        #         if cui is None:
+        #             print(indication['snomed_name'])
+        #         else:
+        #             print(cui)
+        #             umls_set.add(cui)
+        # return umls_set
+
+
 
     @staticmethod
-    def map_drug_to_ontology_with_indications(chembl_id):
+    def map_drug_to_ontology(chembl_id):
         """
         mapping between a drug and Disease Ontology IDs and/or Human Phenotype Ontology IDs corresponding to indications
 
         :param chembl_id: The CHEMBL ID for a drug
 
-        :return: A set of strings containing the found hp ids or empty set if none where found
+        :return: A dictionary with two fields ('indication' and 'contraindication'). Each field is a set of strings
+                containing the found hp / omim / doid ids or empty set if none were found
         """
-        hp_onto_set = set()
-        disease_onto_set = set()
+        indication_onto_set = set()
+        contraindication_onto_set = set()
         if not isinstance(chembl_id, str):
-            return {"hp_onto": hp_onto_set, "disease_onto": disease_onto_set}
-        indications = QueryMyChem.get_drug_indications(chembl_id)
+            return {'indications': indication_onto_set, "contraindications": contraindication_onto_set}
+        drug_use = QueryMyChem.get_drug_use(chembl_id)
+        indications = drug_use['indications']
+        contraindications = drug_use['contraindications']
         sm = SynonymMapper()
         for indication in indications:
             if 'snomed_id' in indication.keys():
                 oxo_results = sm.get_all_from_oxo('SNOMEDCT:' + indication['snomed_id'], ['DOID', 'OMIM', 'HP'])
                 if oxo_results is not None:
                     for oxo_result in oxo_results:
-                        if oxo_result[:4] == "DOID" or oxo_result[:4] == "OMIM":
-                            disease_onto_set.add(oxo_result)
-                        if oxo_result[:2] == "HP":
-                            hp_onto_set.add(oxo_result)
-        return {"hp_onto": hp_onto_set, "disease_onto": disease_onto_set}
+                        indication_onto_set.add(oxo_result)
+        for contraindication in contraindications:
+            if 'snomed_id' in contraindication.keys():
+                oxo_results = sm.get_all_from_oxo('SNOMEDCT:' + contraindication['snomed_id'], ['DOID', 'OMIM', 'HP'])
+                if oxo_results is not None:
+                    for oxo_result in oxo_results:
+                        contraindication_onto_set.add(oxo_result)
+        return {'indications': indication_onto_set, "contraindications": contraindication_onto_set}
 
 
 if __name__ == '__main__':
@@ -106,8 +125,8 @@ if __name__ == '__main__':
     print(hp_set)
     print(len(hp_set))
 
-    umls_set = DrugMapper.map_drug_to_UMLS_with_indications("CHEMBL521")
+    umls_set = DrugMapper.map_drug_to_UMLS("CHEMBL521")
     print(umls_set)
 
-    onto_set = DrugMapper.map_drug_to_ontology_with_indications("CHEMBL521")
+    onto_set = DrugMapper.map_drug_to_ontology("CHEMBL521")
     print(onto_set)
