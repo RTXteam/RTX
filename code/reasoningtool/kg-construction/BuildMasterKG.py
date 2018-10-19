a18fccc2c2438305cee67f1ea46506d7dce0ce39
DeqingQu <qu.deqing@gmail.com>
2018-10-18 18:10:52 -0700

    remove header=0, merge master branch to issue269 branch

--- code/reasoningtool/kg-construction/BuildMasterKG.py
+++ code/reasoningtool/kg-construction/BuildMasterKG.py
@@ -67,10 +67,10 @@ def add_pc2_to_kg():
 
 
 def seed_nodes_from_master_tsv_file():
-    seed_node_data = pandas.read_csv('../../../data/seed_nodes_filtered.tsv',
+    seed_node_data = pandas.read_csv('../../../data/seed_nodes_filtered1.tsv',
                                      sep="\t",
                                      names=['type', 'rtx_name', 'term', 'purpose'],
-                                     header=0,
+                                     # header=1,
                                      dtype={'rtx_name': str})
     first_row = True
     for index, row in seed_node_data.iterrows():
