5bd14f3a7c0985e150216ff2c83a8e7dafc5226f
Eric Deutsch <edeutsch@systemsbiology.org>
2018-11-21 00:19:20 +0000

    partial update to new schema version 0.9.0

--- code/UI/OpenAPI/python-flask-server/swagger_server/controllers/query_controller.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/controllers/query_controller.py
@@ -1,24 +1,24 @@
 import connexion
 import six
 
+from swagger_server.models.message import Message  # noqa: E501
 from swagger_server.models.query import Query  # noqa: E501
-from swagger_server.models.response import Response  # noqa: E501
 from swagger_server import util
 from RTXQuery import RTXQuery
 
 
 def query(body):  # noqa: E501
-    """Query RTX using a predefined question type
+    """Query RTX via one of several inputs
 
      # noqa: E501
 
     :param body: Query information to be submitted
     :type body: dict | bytes
 
-    :rtype: Response
+    :rtype: Message
     """
     if connexion.request.is_json:
         query = connexion.request.get_json()
         rtxq = RTXQuery()
-        result = rtxq.query(query)
-    return result
+        message = rtxq.query(query)
+    return message
