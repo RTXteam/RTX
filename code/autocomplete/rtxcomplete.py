604dcaef5c347ed369a0fbb219456d502dd68a12
Eric Deutsch <edeutsch@systemsbiology.org>
2019-03-08 02:53:57 +0000

    add some addiitonal testing methods for addiitional word autocomplete functionality. not complete.

--- code/autocomplete/rtxcomplete.py
+++ code/autocomplete/rtxcomplete.py
@@ -135,6 +135,35 @@ def fuzzy(word,limit):
     rows = cursor.fetchall()
     return rows
 
+def fuzzy2(word,limit):
+    cursor.execute("SELECT str FROM disease WHERE str LIKE \"%s%%\" LIMIT %s" % (word,limit))
+    rows = cursor.fetchall()
+    values = list()
+    values_dict = dict()
+    for row in rows:
+       if row[0] not in values_dict:
+           values.append(row[0])
+           values_dict[row[0]] = 1
+           print(row[0])
+    n_values = len(values)
+
+    if n_values < limit:
+        cursor.execute("SELECT str FROM disease WHERE str LIKE \"%%%s%%\" LIMIT %s" % (word,limit-n_values))
+        rows = cursor.fetchall()
+        for row in rows:
+            if row[0] not in values_dict:
+                values.append(row[0])
+                values_dict[row[0]] = 1
+                print(row[0])
+
+    return(values)
+
+
+def get_tables():
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = cursor.fetchall()
+    return rows
+
 def autofuzzy(word,limit):
     cursor.execute("SELECT word FROM spell WHERE word MATCH \"%s*\" LIMIT %s" % (word,limit))                                                             
     rows = cursor.fetchall()
