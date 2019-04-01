cfbb09cb43d425e0192f2175036f94c8d480c80f
Eric Deutsch <edeutsch@systemsbiology.org>
2019-04-01 21:46:14 +0000

    update to latest code generator

--- code/UI/OpenAPI/python-flask-server/openapi_server/__main__.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/__main__.py
@@ -13,7 +13,9 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../Feedback")
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml', arguments={'title': 'OpenAPI for NCATS Biomedical Translator Reasoners'})
+    app.add_api('openapi.yaml',
+                arguments={'title': 'OpenAPI for NCATS Biomedical Translator Reasoners'},
+                pythonic_params=True)
     app.run(port=5001, threaded=True)
 
 
