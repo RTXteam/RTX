0db676e86bcf336ab23c863fb4d995e8c3f6da87
power10dan <lintzu@onid.oregonstate.edu>
2019-10-18 20:13:05 -0700

    Final Version Of The Testing Harness

--- code/google_dist_benchmark/bench_mark_decorator.py
+++ code/google_dist_benchmark/bench_mark_decorator.py
@@ -1,27 +1,51 @@
 import time
-from networkx import edges
-from matplotlib import pyplot as plt
+import cypher
 
+from networkx import edges,nodes
+from matplotlib import pyplot as plt
+from ReasoningUtilities import defaults
+from ReasoningUtilities import connection
+from ReasoningUtilities import get_graph
+
+"""
+	Name: bench_harness
+	input:
+		iterations:int
+	output:
+		function: test_google_benchmark
+	description:
+		This is the decorator function
+		used as our testing harness
+"""
 def bench_harness(iterations):
-	def bench_start(function):
-		file_name = './benchmark_res/%s_iterations.txt' % (str(iterations))
-		for iteration in range(iterations):
+	def bench_start(function, *args, **kwargs):
+		file_name = './benchmark_res/benchmark_res.txt'
+		f = open(file_name, 'a')
+		for i in range(240,iterations+1, 100):
 			run_time_res = []
-			ts = time.time()
 			"""
-				set limit to scale linearly with number
+				set limit in the neo4j parameter
+				to scale linearly with number
 				of iterations
 			"""
-			graph_weighted_dist = function(iteration)
+			query = 'match p=(s:disease{id:"DOID:12365"})-[*1..2]-() return p limit %i' %(i)
+			res = cypher.run(query, conn=connection, config=defaults)
+			graph_weighted_dist = get_graph(res, directed=True)
+			# bench mark out function and calculate our runtime
+			ts = time.time()
+			function(None,graph=graph_weighted_dist, *args, **kwargs)
 			te = time.time()
 			run_time = te-ts
-			# get number of edges
-			edges_for_graph = edges(graph_weighted_dist)
-			run_time_res.append((edges_for_graph, run_time))
-			# run matplotlib to plot the result
-			with open('./benchmark_res', 'a') as f:
-				for edge, run_time in run_time_res:
-					edge_runtime_data = "%i\t%i" % (edge, run_time)
-					f.write(edge_runtime_data)
-				f.close()
+			# get number of edges and nodes
+			edges_for_graph = graph_weighted_dist.edges()
+			nodes_for_graph = nodes(graph_weighted_dist)
+			num_edges = len(list(edges_for_graph))
+			num_nodes = len(nodes_for_graph)
+			# write the result into a text file
+			edge_runtime_data = "%i\t%f\n" % (num_edges, float(run_time))
+			f.write(edge_runtime_data)
+			output_msg = "Currently at iteration %i with %i edges and %i nodes" % (i, num_edges, num_nodes)
+			print(output_msg)
+		f.close()
+		return function
 	return bench_start
--- code/google_dist_benchmark/google_dist_bench.py
+++ code/google_dist_benchmark/google_dist_bench.py
@@ -3,27 +3,15 @@ sys.path.append('../reasoningtool/QuestionAnswering')
 
 from ReasoningUtilities import weight_graph_with_google_distance
 from RTXConfiguration import RTXConfiguration
-from neo4j.v1 import GraphDatabase, basic_auth
 from bench_mark_decorator import bench_harness
-from networkx import edges
 
 class GoogleDistBenchmark:
-	@bench_harness(iterations=100)
-	def test_google_benchmark(self, limit=1):
-		rtxConfig = RTXConfiguration()
-		driver = GraphDatabase.driver(rtxConfig.neo4j_bolt, \
-									auth=basic_auth(rtxConfig.neo4j_username, \
-									rtxConfig.neo4j_password))
-		session = driver.session()
-		print("limited:", limit)
-		query = 'match p=(s:disease{id:"DOID:14325"})-[*1..2]-() return p limit %i' %(limit)
-		print(query)
-		g = session.run(query)
-		# remember to count the number of
-		# edges!
-		weight_graph_with_google_distance(g)
-		print(g)
-		return g
+	@bench_harness(iterations=240)
+	def test_google_benchmark(self, graph=None, *args, **kwargs):
+		print("Testing google weighted distance function")
+		if not graph:
+			return
+		weight_graph_with_google_distance(graph)
 if __name__ == '__main__':
 	google_dist = GoogleDistBenchmark()
 	google_dist.test_google_benchmark()
