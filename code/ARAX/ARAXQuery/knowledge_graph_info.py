e712aea5fda7838799df1826b03265eb62344158
Eric Deutsch <edeutsch@systemsbiology.org>
2020-02-03 22:18:03 -0800

    Added ability to add query_graph mappings to the KG

--- code/ARAX/ARAXQuery/knowledge_graph_info.py
+++ code/ARAX/ARAXQuery/knowledge_graph_info.py
@@ -11,6 +11,10 @@ from response import Response
 from ARAX_messenger import ARAXMessenger
 from query_graph_info import QueryGraphInfo
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../UI/OpenAPI/python-flask-server/")
+from swagger_server.models.node_attribute import NodeAttribute
+from swagger_server.models.edge_attribute import EdgeAttribute
+
 
 class KnowledgeGraphInfo:
 
@@ -64,7 +68,7 @@ class KnowledgeGraphInfo:
             self.query_graph_id_node_status = 'no nodes have query_graph_ids'
         else:
             self.query_graph_id_node_status = 'only some nodes have query_graph_ids'
-        response.info("In the KnowledgeGraph, {self.query_graph_id_node_status}")
+        response.info(f"In the KnowledgeGraph, {self.query_graph_id_node_status}")
 
         #### Loop through edges computing some stats
         n_edges_with_query_graph_ids = 0
@@ -87,7 +91,103 @@ class KnowledgeGraphInfo:
             self.query_graph_id_edge_status = 'no edges have query_graph_ids'
         else:
             self.query_graph_id_edge_status = 'only some edges have query_graph_ids'
-        response.info("In the KnowledgeGraph, {self.query_graph_id_edge_status}")
+        response.info(f"In the KnowledgeGraph, {self.query_graph_id_edge_status}")
+
+        #### Return the response
+        return response
+
+
+    #### Top level decision maker for applying filters
+    def add_query_graph_tags(self, message, query_graph_info):
+
+        #### Define a default response
+        response = Response()
+        self.response = response
+        self.message = message
+        response.debug(f"Adding temporary QueryGraph ids to KnowledgeGraph")
+
+        #### Get shorter handles
+        knowedge_graph = message.knowledge_graph
+        nodes = knowedge_graph.nodes
+        edges = knowedge_graph.edges
+
+        #### Loop through nodes adding query_graph_ids
+        for node in nodes:
+
+            #### Check to see if there is already a query_graph_id attribute on the node
+            already_have_query_graph_id = False
+            if node.node_attributes is None:
+                node.node_attributes = []
+            else:
+                for attribute in node.node_attributes:
+                    if attribute.name == 'query_graph_id':
+                        already_have_query_graph_id = True
+                        break
+
+            #### If not, then determine what it should be and add it
+            if not already_have_query_graph_id:
+                id = node.id
+                types = node.type
+ 
+                #### Find a matching type in the QueryGraph for this node
+                if types is None:
+                    response.error(f"KnowledgeGraph node {id} does not have a type. This should never be", error_code="NodeMissingType")
+                    return response
+                n_found_types = 0
+                found_type = None
+                for node_type in types:
+                    if node_type in query_graph_info.node_type_map:
+                        n_found_types += 1
+                        found_type = node_type
+
+                #### If we did not find exactly one matching type, error out
+                if n_found_types == 0:
+                    response.error(f"Tried to find types '{types}' for KnowledgeGraph node {id} in query_graph_info, but did not find it", error_code="NodeTypeMissingInQueryGraph")
+                    return response
+                elif n_found_types > 1:
+                    response.error(f"Tried to find types '{types}' for KnowledgeGraph node {id} in query_graph_info, and found multiple matches. This is ambiguous", error_code="MultipleNodeTypesInQueryGraph")
+                    return response
+                query_graph_id = query_graph_info.node_type_map[found_type]
+
+                #### And add it
+                node_attribute = NodeAttribute()
+                node_attribute.name = 'query_graph_id'
+                node_attribute.value = query_graph_id
+                node.node_attributes.append(node_attribute)
+
+
+        #### Loop through the edges adding query_graph_ids
+        for edge in edges:
+            id = edge.id
+
+            #### Check to see if there is already a query_graph_id attribute on the edge
+            already_have_query_graph_id = False
+            if edge.edge_attributes is None:
+                edge.edge_attributes = []
+            else:
+                for attribute in edge.edge_attributes:
+                    if attribute.name == 'query_graph_id':
+                        already_have_query_graph_id = True
+                        break
+
+            #### If not, then add it
+            if not already_have_query_graph_id:
+
+                #### Determine what the query_graph_id should be for this edge
+                if edge.type is None:
+                    response.error(f"KnowledgeGraph edge {id} does not have a type. This should never be", error_code="EdgeMissingType")
+                    return response
+                if edge.type not in query_graph_info.edge_type_map:
+                    response.error(f"Tried to find type '{edge.type}' for KnowledgeGraph node {id} in query_graph_info, but did not find it", error_code="EdgeTypeMissingInQueryGraph")
+                    return response
+                query_graph_id = query_graph_info.edge_type_map[edge.type]
+
+            query_graph_id = query_graph_info.edge_type_map[edge.type]
+            edge_attribute = EdgeAttribute()
+            edge_attribute.name = 'query_graph_id'
+            edge_attribute.value = query_graph_id
+            edge.edge_attributes.append(edge_attribute)
+
 
         #### Return the response
         return response
@@ -144,8 +244,26 @@ def main():
         print(response.show(level=Response.DEBUG))
         return response
 
+    #### Try to add query_graph_ids to the KnowledgeGraph
+    result = knowledge_graph_info.add_query_graph_tags(message,query_graph_info)
+    response.merge(result)
+    if result.status != 'OK':
+        print(response.show(level=Response.DEBUG))
+        return response
+
+    #### Reassess some information about the KnowledgeGraph
+    result = knowledge_graph_info.check_for_query_graph_tags(message,query_graph_info)
+    response.merge(result)
+    if result.status != 'OK':
+        print(response.show(level=Response.DEBUG))
+        return response
+
+
+    print(response.show(level=Response.DEBUG))
+
     tmp = { 'query_graph_id_node_status': knowledge_graph_info.query_graph_id_node_status, 'query_graph_id_edge_status': knowledge_graph_info.query_graph_id_edge_status, 
         'n_nodes': knowledge_graph_info.n_nodes, 'n_edges': knowledge_graph_info.n_edges }
+    #print(json.dumps(ast.literal_eval(repr(message)),sort_keys=True,indent=2))
     print(json.dumps(ast.literal_eval(repr(tmp)),sort_keys=True,indent=2))
 
 
