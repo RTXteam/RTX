12e740f321b2f4487da26748781578a1531d704f
Eric Deutsch <edeutsch@systemsbiology.org>
2020-02-05 20:52:13 +0000

    new code gen

--- code/UI/OpenAPI/python-flask-server/swagger_server/models/knowledge_graph.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/models/knowledge_graph.py
@@ -6,8 +6,6 @@ from datetime import date, datetime  # noqa: F401
 from typing import List, Dict  # noqa: F401
 
 from swagger_server.models.base_model_ import Model
-from swagger_server.models.edge import Edge  # noqa: F401,E501
-from swagger_server.models.node import Node  # noqa: F401,E501
 from swagger_server import util
 
 
