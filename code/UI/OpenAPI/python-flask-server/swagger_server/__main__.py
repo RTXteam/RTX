ef840f26cc35c866ff7a9395cf10edb9f8651b8f
Eric Deutsch <edeutsch@systemsbiology.org>
2020-02-14 10:56:01 -0800

    Enabling CORS support for development. This might conflict with the NGINX on arax.rtx.ai and may need to be disabled on arax.rtx.ai. But this is added for testing on local machines

--- code/UI/OpenAPI/python-flask-server/swagger_server/__main__.py
+++ code/UI/OpenAPI/python-flask-server/swagger_server/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 
 from swagger_server import encoder
 
@@ -9,6 +10,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'RTX'})
+    CORS(app.app)
     app.run(port=5001, threaded=True)
 
 
