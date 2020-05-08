71f385ee6c09682fef6c592acd51e71aed427f69
Eric Deutsch <edeutsch@systemsbiology.org>
2020-05-08 23:03:59 +0000

    added feature to force intermediate nodes in an incoming query_graph to is_set=true. Unclear if this is the best way to handle this

--- code/ARAX/ARAXQuery/query_graph_info.py
+++ code/ARAX/ARAXQuery/query_graph_info.py
@@ -66,7 +66,7 @@ class QueryGraphInfo:
         self.node_type_map = {}
         for qnode in nodes:
             id = qnode.id
-            node_info[id] = { 'id': id, 'has_curie': False, 'type': qnode.type, 'has_type': False, 'is_set': False, 'n_edges': 0, 'is_connected': False, 'edges': [], 'edge_dict': {} }
+            node_info[id] = { 'id': id, 'node_object': qnode, 'has_curie': False, 'type': qnode.type, 'has_type': False, 'is_set': False, 'n_edges': 0, 'is_connected': False, 'edges': [], 'edge_dict': {} }
             if qnode.curie is not None: node_info[id]['has_curie'] = True
             if qnode.type is not None: node_info[id]['has_type'] = True
             #if qnode.is_set is not None: node_info[id]['is_set'] = True
@@ -219,14 +219,16 @@ class QueryGraphInfo:
             # Since queries with intermediate nodes that are not is_set=true tend to blow up, for now, make them is_set=true unless explicitly set to false
             if node_index > 0 and node_index < (self.n_nodes - 1 ):
                 if 'is_set' not in node or node['is_set'] is None:
-                    node['is_set'] = 'true'
+                    node['node_object'].is_set = True
                     response.warning(f"Setting unspecified is_set to true for {node['id']} because this will probably lead to a happier result")
-                elif node['is_set'].lower() == 'true':
+                elif node['is_set'] is True:
                     response.debug(f"Value for is_set is already true for {node['id']} so that's good")
-                elif node['is_set'].lower() == 'false':
-                    response.INFO(f"Value for is_set is set to false for intermediate node {node['id']}. This could lead to weird results. Consider setting it to true")
-                else:
-                    response.error(f"Unrecognized value is_set='{node['is_set']}' for {node['id']}. This should be true or false")
+                elif node['is_set'] is False:
+                    #response.info(f"Value for is_set is set to false for intermediate node {node['id']}. This could lead to weird results. Consider setting it to true")
+                    response.info(f"Value for is_set is false for intermediate node {node['id']}. Setting to true because this will probably lead to a happier result")
+                    node['node_object'].is_set = True
+                #else:
+                #    response.error(f"Unrecognized value is_set='{node['is_set']}' for {node['id']}. This should be true or false")
 
             node_index += 1
             if node_index < self.n_nodes:
