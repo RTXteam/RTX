6777f9624eb7768488e768c56e7aa4abdf2c5053
Chunyu Ma <machunyu4402@hotmail.com>
2020-06-14 23:49:30 -0400

    #758 update some scripts to run GraphSage

---
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/2_run_graphsage_unsupervised_train.sh
@@ -0,0 +1,42 @@
+## This bash script is to run graphsage
+
+## set graphsage folder
+graphsage_folder=~/work/Graphsage/GraphSAGE
+
+## set your working path
+work_path=~/work/RTX/code/reasoningtool/MLDrugRepurposing/Test_graphsage
+
+## set python path (Please use python 2.7 to run graphsage as graphsage was written by python2.7)
+ppath=~/anaconda3/envs/graphsage_p2.7env/bin/python
+
+## set model name
+model='graphsage_mean'
+## model option:
+#graphsage_mean -- GraphSage with mean-based aggregator
+#graphsage_seq -- GraphSage with LSTM-based aggregator
+#graphsage_maxpool -- GraphSage with max-pooling aggregator
+#graphsage_meanpool -- GraphSage with mean-pooling aggregator
+#gcn -- GraphSage with GCN-based aggregator
+#n2v -- an implementation of DeepWalk
+
+## set data input folder and training data prefix
+train_prefix=/home/cqm5886/work/RTX/code/reasoningtool/MLDrugRepurposing/Test_graphsage/graphsage_input/data #note: here 'data' is the training data prefix
+
+## other parameters
+model_size='big' #Can be big or small
+learning_rate=0.001 #test 0.01 and 0.001, 'initial learning rate'
+epochs=10 #test 5 and 10, 'number of epochs to train'
+samples_1=25 #suggest 15-25, based on the paper, bigger is better
+samples_2=25 #script only allows to set K=2, the same as samples_1
+dim_1=128 #Size of output dim (final is 2x this)
+dim_2=128
+max_total_steps=500 #Maximum total number of iterations
+validate_iter=5000 #how often to run a validation minibatch
+identity_dim=50 #Set to positive value to use identity embedding features of that dimension. Default 0
+
+
+## create a soft link to graphsage modules
+ln -s ${graphsage_folder}/graphsage ${work_path}
+
+## run graphsage unsupervised model
+time $ppath -m graphsage.unsupervised_train --train_prefix ${train_prefix} --model_size ${model_size} --learning_rate ${learning_rate} --epochs ${epochs} --samples_1 ${samples_1} --samples_2 ${samples_2} --dim_1 ${dim_1} --dim_2 ${dim_2} --model ${model} --max_total_steps ${max_total_steps} --validate_iter ${validate_iter} --identity_dim ${identity_dim}
---
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/README.txt
@@ -0,0 +1,9 @@
+To run GraphSage, I recommend to use conda to set up the environment first. Since GraphSage was written by python v2.7, we need to set up two python environment separately for python v2.7 and python v3.7. I prepared two conda environment files (eg. environment_p2.7.yml and environment_p3.7.yml) that I used to set up the conda environment for running GraphSage. You can use the following commands to set up the conda environment
+
+## NOTE: please install conda first before running the following commands
+## set up environment for python v2.7 
+conda create -n test -c conda-forge -f environment_p2.7.yml
+## set up environment for python v3.7
+conda create -n test -c conda-forge -f environment_p3.7.yml
+
+After setting up the conda environment, please follow the bash scripts (eg. 1_graphsage_make_data.sh, 2_run_graphsage_unsupervised_train.sh) to prepare the input files and run GraphSage. The prefix of these scripts is the order of running these scripts. So first run 1_graphsage_make_data.sh to prepare the input files (Note: please use conda environment with python v3.7 to run this script) and then run 2_run_graphsage_unsupervised_train.sh (Note: please use conda environment with python v2.7 to run this script).
