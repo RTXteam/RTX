5f70bbf04bad85ba21e9c1f10373567157e71763
Chunyu Ma <machunyu4402@hotmail.com>
2020-06-17 23:56:32 -0400

    #758 udpate scripts

--- code/ARAX/ARAXQuery/Overlay/GraphSage_train/2_run_graphsage_unsupervised_train.sh
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/2_run_graphsage_unsupervised_train.sh
@@ -4,10 +4,10 @@
 graphsage_folder=~/work/Graphsage/GraphSAGE
 
 ## set your working path
-work_path=~/work/RTX/code/reasoningtool/MLDrugRepurposing/Test_graphsage
+work_path=/home/cqm5886/work/RTX/code/reasoningtool/MLDrugRepurposing/Test_graphsage
 
 ## set python path (Please use python 2.7 to run graphsage as graphsage was written by python2.7)
-ppath=~/anaconda3/envs/graphsage_p2.7env/bin/python
+ppath=/home/cqm5886/anaconda3/envs/graphsage_p2.7env/bin/python
 
 ## set model name
 model='graphsage_mean'
@@ -33,10 +33,11 @@ dim_2=128
 max_total_steps=500 #Maximum total number of iterations
 validate_iter=5000 #how often to run a validation minibatch
 identity_dim=50 #Set to positive value to use identity embedding features of that dimension. Default 0
-
+batch_size=512 #minibatch size
+max_degree=96 #maximum node degree
 
 ## create a soft link to graphsage modules
 ln -s ${graphsage_folder}/graphsage ${work_path}
 
 ## run graphsage unsupervised model
-time $ppath -m graphsage.unsupervised_train --train_prefix ${train_prefix} --model_size ${model_size} --learning_rate ${learning_rate} --epochs ${epochs} --samples_1 ${samples_1} --samples_2 ${samples_2} --dim_1 ${dim_1} --dim_2 ${dim_2} --model ${model} --max_total_steps ${max_total_steps} --validate_iter ${validate_iter} --identity_dim ${identity_dim}
+time $ppath -m graphsage.unsupervised_train --train_prefix ${train_prefix} --model_size ${model_size} --learning_rate ${learning_rate} --epochs ${epochs} --samples_1 ${samples_1} --samples_2 ${samples_2} --dim_1 ${dim_1} --dim_2 ${dim_2} --model ${model} --max_total_steps ${max_total_steps} --validate_iter ${validate_iter} --identity_dim ${identity_dim} --batch_size ${batch_size} --max_degree ${max_degree}
