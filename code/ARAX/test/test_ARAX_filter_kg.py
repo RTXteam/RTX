5769319d5bdfeb893be42fad36749f72ed234cc5
finnagin <flan313@gmail.com>
2020-06-23 03:11:28 -0700

    #781

---
+++ code/ARAX/test/test_ARAX_filter_kg.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Usage:
+# run all: pytest -v test_ARAX_overlay.py
+# run just certain tests: pytest -v test_ARAX_filter_kg.py -k test_remove_by_property
+
+import sys
+import os
+import pytest
+from collections import Counter
+import copy
+import json
+import ast
+from typing import List, Union
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../ARAXQuery")
+from ARAX_query import ARAXQuery
+from response import Response
+
+PACKAGE_PARENT = '../../UI/OpenAPI/python-flask-server'
+sys.path.append(os.path.normpath(os.path.join(os.getcwd(), PACKAGE_PARENT)))
+from swagger_server.models.edge import Edge
+from swagger_server.models.node import Node
+from swagger_server.models.q_edge import QEdge
+from swagger_server.models.q_node import QNode
+from swagger_server.models.query_graph import QueryGraph
+from swagger_server.models.knowledge_graph import KnowledgeGraph
+from swagger_server.models.node_binding import NodeBinding
+from swagger_server.models.edge_binding import EdgeBinding
+from swagger_server.models.biolink_entity import BiolinkEntity
+from swagger_server.models.result import Result
+from swagger_server.models.message import Message
+
+
+def _do_arax_query(query: dict) -> List[Union[Response, Message]]:
+    araxq = ARAXQuery()
+    response = araxq.query(query)
+    if response.status != 'OK':
+        print(response.show(level=response.DEBUG))
+    return [response, araxq.message]
+
+
+
+
+if __name__ == "__main__":
+    pytest.main(['-v'])
