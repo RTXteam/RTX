e77a2e0519f952b50edcdbca75e4b2f5aaf30cc6
Chunyu Ma <machunyu4402@hotmail.com>
2020-07-26 12:38:36 -0400

    #875 udpate map_curie_to_OMOP.py for both using nodesynonymizer and hitting oxo API

--- code/ARAX/KnowledgeSources/COHD_local/scripts/map_curie_to_OMOP.py
+++ code/ARAX/KnowledgeSources/COHD_local/scripts/map_curie_to_OMOP.py
@@ -1,5 +1,9 @@
+"""This script is used to map the curie name to OMOP concept ids.
 
-#import python modules
+Author: Chunyu Ma
+"""
+
+# import python modules
 import pandas as pd
 import os
 import sys
@@ -11,18 +15,23 @@ from contextlib import redirect_stderr
 import re
 import requests
 
-#import internal modules
+# import internal modules
 pathlist = os.path.realpath(__file__).split(os.path.sep)
 RTXindex = pathlist.index("RTX")
 sys.path.append(os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code', 'ARAX', 'NodeSynonymizer']))
 from node_synonymizer import NodeSynonymizer
 
+
 class MapCurieToOMOP:
+    """This class is used to map the curie name to OMOP concept ids."""
 
     #### Constructor
     def __init__(self, kg="KG1"):
-        """
-        :param kg (str): the name of knowledge provider (optional) e.g. "KG1" or "KG2"
+        """Initialize the class instance.
+
+        Args:
+            kg (str, optional): the name of knowledge provider e.g. "KG1" or "KG2". Defaults to "KG1".
+
         """
         kg = kg.upper()
         self.kg = kg
@@ -32,9 +41,9 @@ class MapCurieToOMOP:
         ## set up the path of KGmetadata
         pre_path = os.path.sep.join([*pathlist[:(RTXindex + 1)], 'data', 'KGmetadata'])
 
-        if kg=="KG1":
+        if kg == "KG1":
             fpath = pre_path + "/NodeNamesDescriptions_KG1.tsv"
-        elif kg=="KG2":
+        elif kg == "KG2":
             fpath = pre_path + "/NodeNamesDescriptions_KG2.tsv"
         else:
             raise ValueError("The parameter 'kg' only accepts 'KG1' or 'KG2'")
@@ -47,22 +56,21 @@ class MapCurieToOMOP:
 
         self.kpdata_dict = dict()
         for row_index in range(self.kpdata.shape[0]):
-            if self.kpdata.loc[row_index,'curie'] not in self.kpdata_dict:
+            if self.kpdata.loc[row_index, 'curie'] not in self.kpdata_dict:
                 self.kpdata_dict[self.kpdata.loc[row_index, 'curie']] = {'name': {self.kpdata.loc[row_index, 'name']}, 'type': {self.kpdata.loc[row_index, 'type']}}
             else:
                 self.kpdata_dict[self.kpdata.loc[row_index, 'curie']]['name'].update([self.kpdata.loc[row_index, 'name']])
                 self.kpdata_dict[self.kpdata.loc[row_index, 'curie']]['type'].update([self.kpdata.loc[row_index, 'type']])
 
-
-
-
     def get_synonyms(self, curie_type):
-        """Retrieve the synonyms of nodes with certain type
+        """Get the synonyms of nodes with certain type.
 
-        :param curie_type (str or list): the type of curie nodes in specified knowledge provider (Required) e.g., "disease", "phenotypic_feature", ['disease', 'phenotypic_feature']
-        return: a dict containing synonym list, name and type for each node
+        Args:
+            curie_type (str or list): the type of curie nodes in specified knowledge provider (Required) e.g., "disease" or "phenotypic_feature" or ['disease', 'phenotypic_feature']
+
+        Returns:
+            dict: a dict containing synonym list, name and type for each node
         """
-
         ## check the input parameters
         if isinstance(curie_type, str):
             pass
@@ -93,54 +101,115 @@ class MapCurieToOMOP:
                 else:
                     pass
 
-            self.synonyms_dict  = dict()
+            self.synonyms_dict = dict()
             for curie in self.kpdata_dict:
-                if len(self.kpdata_dict[curie]['type'].intersection(set(curie_type)))>0:
+                if len(self.kpdata_dict[curie]['type'].intersection(set(curie_type))) > 0:
                     res = self.synonymizer.get_normalizer_results(curie, kg_name=self.kg)
                     synonym_list = list(set([row['identifier'] for row in res[curie]['equivalent_identifiers']]))
                     self.synonyms_dict[curie] = {'name': self.kpdata_dict[curie]['name'], 'type': self.kpdata_dict[curie]['type'], 'synonyms': synonym_list}
 
         self.get_synonyms_done = True
 
-    def _parallel_get_OMOP(self, synonym):
+    @staticmethod
+    def change_format(synonym):
+        """Change the format of synonym in order to match OMOP concept id.
 
+        Args:
+            synonym (str): a synonym of a curie name
+
+        Returns:
+            str: a new format of synonym of a curie name
+        """
         try:
             vocabulary_id, concept_code = synonym.split(':')
         except ValueError:
             vocabulary_id, concept_code = synonym.split(':')[1:]
-        if vocabulary_id not in ['ATC', 'CVX', 'HCPCS', 'ICD10', 'ICD-10', 'ICD10CM', 'ICD-9', 'MESH', 'NDFRT', 'RXNORM', 'SNOMEDCT', 'SNOMEDCT_VET', 'MEDDRA']:
-            return []
+
+        if vocabulary_id == "ICD-10":
+            synonym = synonym.replace('ICD-10', 'ICD10')
+        elif vocabulary_id == "ICD-9":
+            synonym = synonym.replace('ICD-9', 'ICD9CM')
+        elif vocabulary_id == "MESH":
+            synonym = synonym.replace('MESH', 'MeSH')
+        elif vocabulary_id == "RXNORM":
+            synonym = synonym.replace('RXNORM', 'RxNorm')
+        elif vocabulary_id == "SNOMEDCT":
+            synonym = synonym.replace('SNOMEDCT', 'SNOMED')
+        elif vocabulary_id == "SNOMEDCT_VET":
+            synonym = synonym.replace('SNOMEDCT_VET', 'SNOMED')
+        elif vocabulary_id == "MEDDRA":
+            synonym = synonym.replace('MEDDRA', 'MedDRA')
         else:
-            if vocabulary_id == "ICD-10":
-                synonym = synonym.replace('ICD-10', 'ICD10')
-            elif vocabulary_id == "ICD-9":
-                synonym = synonym.replace('ICD-9', 'ICD9CM')
-            elif vocabulary_id == "MESH":
-                synonym = synonym.replace('MESH', 'MeSH')
-            elif vocabulary_id == "RXNORM":
-                synonym = synonym.replace('RXNORM', 'RxNorm')
-            elif vocabulary_id == "SNOMEDCT":
-                synonym = synonym.replace('SNOMEDCT', 'SNOMED')
-            elif vocabulary_id == "SNOMEDCT_VET":
-                synonym = synonym.replace('SNOMEDCT_VET', 'SNOMED')
-            elif vocabulary_id == "MEDDRA":
-                synonym = synonym.replace('MEDDRA', 'MedDRA')
+            pass
+
+        return synonym
+
+    def _get_OMOP(self, synonym):
+
+        synonym = MapCurieToOMOP.change_format(synonym)
+
+        concept_id = self.concept_table_select.loc[self.concept_table_select['curie_name'] == synonym, 'concept_id']
+        return list(concept_id)
+
+    def call_oxo_API(self, key):
+        """Call OxO (the EMBL-EBI Ontology Xref Service) API to find ontology mapping.
+
+        Args:
+            key (str): the curie name e.g. "DRUGBANK:DB05024", "CUI:C0876032", "CUI:C0908863"
+
+        Returns:
+            tuple: a tuple containing curie name and its corresponding OMOP concept ids if it has mapping.
+        """
+        synonyms = [MapCurieToOMOP.change_format(synonym) for synonym in self.synonyms_dict[key]['synonyms'] if synonym.split(":")[0] != "OMIM" and synonym.split(":")[0] != "Orphanet" and synonym.split(":")[0] != "CHEMBL.COMPOUND"]  # "OMIM" and "Orphanet" will cause 500 return status which is 'Internal Server Error' and "CHEMBL.COMPOUND" is not accepted by API.
+        if len(synonyms) != 0:
+            query_ids = ",".join(synonyms)
+            dist = 1
+            res = requests.get(f"https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}")
+            if res.status_code == 200:
+                res_curies = [curie['curie'].upper() for item in res.json()['_embedded']['searchResults'] if len(item['mappingResponseList']) != 0 for curie in item['mappingResponseList']]
+                if any([True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]):
+                    bool_list = [True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]
+                    return (key, list(self.concept_table_select['concept_id'][bool_list]))
+                else:
+                    dist = 2
+                    res = requests.get(f"https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}")
+                    if res.status_code == 200:
+                        res_curies = [curie['curie'].upper() for item in res.json()['_embedded']['searchResults'] if len(item['mappingResponseList']) != 0 for curie in item['mappingResponseList']]
+                        if any([True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]):
+                            bool_list = [True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]
+                            return (key, list(self.concept_table_select['concept_id'][bool_list]))
+                        else:
+                            dist = 3
+                            res = requests.get(f"https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}")
+                            if res.status_code == 200:
+                                res_curies = [curie['curie'].upper() for item in res.json()['_embedded']['searchResults'] if len(item['mappingResponseList']) != 0 for curie in item['mappingResponseList']]
+                                if any([True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]):
+                                    bool_list = [True if curie_name in res_curies else False for curie_name in self.concept_table_select['curie_name']]
+                                    return (key, list(self.concept_table_select['concept_id'][bool_list]))
+                                else:
+                                    return (key, [])
+                            else:
+                                print(f"{key}\tError {res.status_code}: https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}", flush=True)
+                                return (key, [])
+                    else:
+                        print(f"{key}\tError {res.status_code}: https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}", flush=True)
+                        return (key, [])
             else:
-                pass
-
-            concept_id = self.concepts_table_select.loc[self.concepts_table_select['curie_name'] == synonym, 'concept_id']
-            return list(concept_id)
-
-
+                print(f"{key}\tError {res.status_code}: https://www.ebi.ac.uk/spot/oxo/api/search?format=json&ids={query_ids}&distance={dist}", flush=True)
+                return (key, [])
+        else:
+            return (key, [])
 
     def map_curie_to_OMOP(self, pre_run_dict=None):
-        """Map curies to OMOP ids based on the synonyms returned from NodeSynonymizer
+        """Map curies to OMOP ids based on the synonyms returned from NodeSynonymizer.
 
-        :param pre_run_dict(optional, str or dict): the path of result saved as pickle file returned from 'get_synonyms' method or the dict object returned from 'get_synonyms' method e.g. 'synonyms_kg1.pkl' or kg2_synonyms
-        return: a dict containing OMOP id list,synonym list, name and type for each node
+        Args:
+            pre_run_dict (str or dict, optional): the path of result saved as pickle file returned from 'get_synonyms' method or the dict object returned from 'get_synonyms' method e.g. 'synonyms_kg1.pkl' or kg2_synonyms. Defaults to None.
+
+        Returns:
+            dict: a dict containing OMOP id list,synonym list, name and type for each node.
         """
-
-        if pre_run_dict==None:
+        if pre_run_dict is None:
             if not self.get_synonyms_done:
                 print(f"Please run 'get_synonyms' method first before run this method.")
                 return {}
@@ -160,37 +229,33 @@ class MapCurieToOMOP:
 
         infolder = os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code', 'ARAX', 'KnowledgeSources', 'COHD_local', 'data'])
         try:
-            # if self.kg == "KG1":
-            #     infile_path = infolder + '/Athena_tables/CONCEPT_KG1.txt'
-            #     concepts_table = pd.read_csv(infile_path, sep='\t', index_col=None)
-            # else:
-            #     infile_path = infolder + '/Athena_tables/CONCEPT_KG2.txt'
-            #     concepts_table = pd.read_csv(infile_path, sep='\t', index_col=None)
             infile_path = infolder + '/Athena_tables/ALL_CONCEPT_filtered.txt'
-            concepts_table = pd.read_csv(infile_path, sep='\t', index_col=None)
+            concept_table = pd.read_csv(infile_path, sep='\t', index_col=None)
         except FileNotFoundError:
             print(f"Can't find {infile_path}")
             return {}
 
-        # if self.kg == "KG1":
-        #     select = ['ATC', 'MeSH', 'ICD10', 'ICD10CM', 'ICD9CM', 'RxNorm', 'SNOMED', 'MedDRA']
-        # else:
-        select = ['ATC', 'CVX', 'HCPCS', 'ICD10', 'ICD10CM', 'ICD9CM', 'MeSH', 'NDFRT', 'RxNorm', 'SNOMED', 'MedDRA']
+        # select = ['ATC', 'CVX', 'HCPCS', 'ICD10', 'ICD10CM', 'ICD9CM', 'MeSH', 'NDFRT', 'RxNorm', 'SNOMED', 'MedDRA']
+        # concept_table_select = concepts_table.loc[[concepts_table.loc[index, 'vocabulary_id'] in select for index in range(concepts_table.shape[0])], ['concept_id', 'vocabulary_id', 'concept_code']]
 
-        concepts_table_select = concepts_table.loc[[concepts_table.loc[index, 'vocabulary_id'] in select for index in range(concepts_table.shape[0])], ['concept_id', 'vocabulary_id', 'concept_code']]
-        concepts_table_select['curie_name'] = concepts_table_select[['vocabulary_id', 'concept_code']].apply(lambda x: str(x[0]) + ":" + str(x[1]), axis=1)
-        concepts_table_select.drop(columns=['vocabulary_id', 'concept_code'])
-        self.concepts_table_select = concepts_table_select.drop(columns=['vocabulary_id', 'concept_code'])
+        concept_table_select = concept_table.loc[:, ['concept_id', 'vocabulary_id', 'concept_code']]
+        concept_table_select['curie_name'] = concept_table_select[['vocabulary_id', 'concept_code']].apply(lambda x: str(x[0]).upper() + ":" + str(x[1]).upper(), axis=1)
+        concept_table_select.drop(columns=['vocabulary_id', 'concept_code'])
+        self.concept_table_select = concept_table_select.drop(columns=['vocabulary_id', 'concept_code'])
 
         for key in self.synonyms_dict:
-            print(key)
+            print(key, flush=True)
             synonym_list = self.synonyms_dict[key]['synonyms']
-            OMOP_concept_list = [elem for elem in itertools.chain.from_iterable([OMOP_list for OMOP_list in map(self._parallel_get_OMOP, synonym_list)])]
+            OMOP_concept_list = [elem for elem in itertools.chain.from_iterable([OMOP_list for OMOP_list in map(self._get_OMOP, synonym_list)])]
             self.synonyms_dict[key]["concept_ids"] = list(set(OMOP_concept_list))
+            if len(list(set(OMOP_concept_list))) == 0:
+                key, OMOP_concept_list = self.call_oxo_API(key)
+                self.synonyms_dict[key]["concept_ids"] = list(set(OMOP_concept_list))
 
 
 def main():
-    print(f"Processing KG1",flush=True)
+    """Run the main script."""
+    print(f"Processing KG1", flush=True)
     kg1_CtoM = MapCurieToOMOP(kg="KG1")
     kg1_CtoM.get_synonyms(curie_type=["disease", "phenotypic_feature", "chemical_substance"])
     res = kg1_CtoM.synonyms_dict
@@ -208,7 +273,7 @@ def main():
     # vocabulary_id = set([identifier.split(":")[0] for identifier in set(itertools.chain.from_iterable([res[curie]['synonyms'] for curie in res]))])
     # print(f"The biomedical vocabulary ids are: {list(vocabulary_id)}")
 
-    print(f"Processing KG2",flush=True)
+    print(f"Processing KG2", flush=True)
     kg2_CtoM = MapCurieToOMOP(kg="KG2")
     kg2_CtoM.get_synonyms(curie_type=["disease", "phenotypic_feature", "chemical_substance", "drug"])
     res = kg2_CtoM.synonyms_dict
@@ -226,10 +291,8 @@ def main():
     with open(outfolder + '/synonyms_kg2_with_concepts.pkl', 'wb') as file:
         pickle.dump(res, file)
 
-
-
 ####################################################################################################
+
 
 if __name__ == "__main__":
     main()
-
