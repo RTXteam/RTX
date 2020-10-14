8f5f6f809ff2b4da343af0d494c4b6b748deedd1
Chunyu Ma <machunyu4402@hotmail.com>
2020-10-14 18:05:51 -0400

    update script for building the drug-treat-disease pre-trained database #990

--- code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/BuildMapID.py
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/BuildMapID.py
@@ -24,14 +24,14 @@ def map_to_graph(row):
         res = nodesynonymizer.get_canonical_curies(drug)[drug]
         preferred_drug_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='drug' or res['preferred_type']=='chemical_substance') else None
         res = nodesynonymizer.get_canonical_curies(disease)[disease]
-        preferred_disease_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='disease' or res['preferred_type']=='phenotypic_feature') else None
+        preferred_disease_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='disease' or res['preferred_type']=='phenotypic_feature' or res['preferred_type']=='disease_or_phenotypic_feature') else None
         return [preferred_drug_curie, preferred_disease_curie]
     elif len(row) == 3:
         count, drug, disease = row
         res = nodesynonymizer.get_canonical_curies(drug)[drug]
         preferred_drug_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='drug' or res['preferred_type']=='chemical_substance') else None
         res = nodesynonymizer.get_canonical_curies(disease)[disease]
-        preferred_disease_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='disease' or res['preferred_type']=='phenotypic_feature') else None
+        preferred_disease_curie = res['preferred_curie'] if (res is not None) and (res['preferred_type']=='disease' or res['preferred_type']=='phenotypic_feature' or res['preferred_type']=='disease_or_phenotypic_feature') else None
         return [count, preferred_drug_curie, preferred_disease_curie]
 
 
