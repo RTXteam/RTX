40d24846773f1297344edccbbe65fb8df075e60a
amykglen <glena@oregonstate.edu>
2021-01-16 00:29:40 -0800

    Change swagger_server -> openapi_server in Resultify #1184

--- code/ARAX/ARAXQuery/ARAX_resultify.py
+++ code/ARAX/ARAXQuery/ARAX_resultify.py
@@ -35,17 +35,17 @@ __status__ = 'Prototype'
 # is there a better way to import swagger_server?  Following SO posting 16981921
 PACKAGE_PARENT = '../../UI/OpenAPI/python-flask-server'
 sys.path.append(os.path.normpath(os.path.join(os.getcwd(), PACKAGE_PARENT)))
-from swagger_server.models.edge import Edge
-from swagger_server.models.node import Node
-from swagger_server.models.q_edge import QEdge
-from swagger_server.models.q_node import QNode
-from swagger_server.models.query_graph import QueryGraph
-from swagger_server.models.knowledge_graph import KnowledgeGraph
-from swagger_server.models.node_binding import NodeBinding
-from swagger_server.models.edge_binding import EdgeBinding
+from openapi_server.models.edge import Edge
+from openapi_server.models.node import Node
+from openapi_server.models.q_edge import QEdge
+from openapi_server.models.q_node import QNode
+from openapi_server.models.query_graph import QueryGraph
+from openapi_server.models.knowledge_graph import KnowledgeGraph
+from openapi_server.models.node_binding import NodeBinding
+from openapi_server.models.edge_binding import EdgeBinding
 from swagger_server.models.biolink_entity import BiolinkEntity
-from swagger_server.models.result import Result
-from swagger_server.models.message import Message
+from openapi_server.models.result import Result
+from openapi_server.models.message import Message
 
 
 # define a string-parameterized BiolinkEntity class
