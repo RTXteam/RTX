a974faa07359a4bb5a2e05b0b96462cdf72eec1b
Eric Deutsch <edeutsch@systemsbiology.org>
2021-01-16 04:50:45 +0000

    re-add the exclude and option_group_id attributes

--- code/UI/OpenAPI/python-flask-server/openapi_server/models/q_edge.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/models/q_edge.py
@@ -17,7 +17,7 @@ class QEdge(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, predicate=None, relation=None, subject=None, object=None):  # noqa: E501
+    def __init__(self, predicate=None, relation=None, subject=None, object=None, exclude=None, option_group_id=None):  # noqa: E501
         """QEdge - a model defined in OpenAPI
 
         :param predicate: The predicate of this QEdge.  # noqa: E501
@@ -28,26 +28,36 @@ class QEdge(Model):
         :type subject: str
         :param object: The object of this QEdge.  # noqa: E501
         :type object: str
+        :param exclude: The exclude of this QEdge.  # noqa: E501
+        :type exclude: bool
+        :param option_group_id: The option_group_id of this QEdge.  # noqa: E501
+        :type option_group_id: str
         """
         self.openapi_types = {
-#            'predicate': OneOfstringarray,
+            #'predicate': OneOfstringarray,
             'predicate': str,
             'relation': str,
             'subject': str,
-            'object': str
+            'object': str,
+            'exclude': bool,
+            'option_group_id': str
         }
 
         self.attribute_map = {
             'predicate': 'predicate',
             'relation': 'relation',
             'subject': 'subject',
-            'object': 'object'
+            'object': 'object',
+            'exclude': 'exclude',
+            'option_group_id': 'option_group_id'
         }
 
         self._predicate = predicate
         self._relation = relation
         self._subject = subject
         self._object = object
+        self._exclude = exclude
+        self._option_group_id = option_group_id
 
     @classmethod
     def from_dict(cls, dikt) -> 'QEdge':
@@ -153,3 +163,49 @@ class QEdge(Model):
             raise ValueError("Invalid value for `object`, must not be `None`")  # noqa: E501
 
         self._object = object
+
+    @property
+    def exclude(self):
+        """Gets the exclude of this QEdge.
+
+        If set to true, then all subgraphs containing this edge are excluded from the final results.  # noqa: E501
+
+        :return: The exclude of this QEdge.
+        :rtype: bool
+        """
+        return self._exclude
+
+    @exclude.setter
+    def exclude(self, exclude):
+        """Sets the exclude of this QEdge.
+
+        If set to true, then all subgraphs containing this edge are excluded from the final results.  # noqa: E501
+
+        :param exclude: The exclude of this QEdge.
+        :type exclude: bool
+        """
+
+        self._exclude = exclude
+
+    @property
+    def option_group_id(self):
+        """Gets the option_group_id of this QEdge.
+
+        Any string acting as a label on two or more edges indicating that they belong to a group that are to be evaluated as alternative   # noqa: E501
+
+        :return: The option_group_id of this QEdge.
+        :rtype: str
+        """
+        return self._option_group_id
+
+    @option_group_id.setter
+    def option_group_id(self, option_group_id):
+        """Sets the option_group_id of this QEdge.
+
+        Any string acting as a label on two or more edges indicating that they belong to a group that are to be evaluated as alternative   # noqa: E501
+
+        :param option_group_id: The option_group_id of this QEdge.
+        :type option_group_id: str
+        """
+
+        self._option_group_id = option_group_id
