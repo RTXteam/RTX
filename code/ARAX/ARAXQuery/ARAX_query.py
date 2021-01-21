2649bde53125a147546dd1385e19b8c9ea4a7524
Eric Deutsch <edeutsch@systemsbiology.org>
2021-01-21 00:51:52 +0000

    add back in NGD computing

--- code/ARAX/ARAXQuery/ARAX_query.py
+++ code/ARAX/ARAXQuery/ARAX_query.py
@@ -681,7 +681,7 @@ def main():
             "add_qnode(category=protein, key=n1)",
             "add_qedge(subject=n0, object=n1, key=e0)",
             "expand(edge_key=e0)",
-            #"overlay(action=compute_ngd, virtual_relation_label=N1, subject_qnode_key=n0, object_qnode_key=n1)",
+            "overlay(action=compute_ngd, virtual_relation_label=N1, subject_qnode_key=n0, object_qnode_key=n1)",
             "resultify(ignore_edge_direction=true)",
             "filter_results(action=limit_number_of_results, max_results=20)",
             "return(message=true, store=true)",
