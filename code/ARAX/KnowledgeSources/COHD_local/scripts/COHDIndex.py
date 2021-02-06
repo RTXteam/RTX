4a5aa2b36ec37bde09d727c5a27e62b0bc54dce8
Chunyu Ma <machunyu4402@hotmail.com>
2021-02-06 17:03:24 -0500

    update scripts for rebuilding COHD database

--- code/ARAX/KnowledgeSources/COHD_local/scripts/COHDIndex.py
+++ code/ARAX/KnowledgeSources/COHD_local/scripts/COHDIndex.py
@@ -28,7 +28,7 @@ class COHDIndex:
     def __init__(self):
         filepath = os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code', 'ARAX', 'KnowledgeSources', 'COHD_local', 'data'])
         self.databaseLocation = filepath
-        lastest_version = "v1.0_kg2.5.0"
+        lastest_version = "v1.0_kg2.5.1"
         self.databaseName = f"COHDdatabase_{lastest_version}.db"
         self.success_con = self.connect()
         self.synonymizer = NodeSynonymizer()
@@ -47,7 +47,7 @@ class COHDIndex:
             print("INFO: Connecting to database", flush=True)
             return True
         else:
-            # required_files = ['single_concept_counts.txt', 'patient_count.txt', 'domain_pair_concept_counts.txt', 'paired_concept_counts_associations.txt', 'domain_concept_counts.txt', 'concepts.txt', 'dataset.txt', 'preferred_synonyms_kg2_5_0_with_concepts.pkl']
+            # required_files = ['single_concept_counts.txt', 'patient_count.txt', 'domain_pair_concept_counts.txt', 'paired_concept_counts_associations.txt', 'domain_concept_counts.txt', 'concepts.txt', 'dataset.txt', 'preferred_synonyms_kg2_5_1_with_concepts.pkl']
             # has_files = [f for f in os.listdir(self.databaseLocation) if os.path.isfile(os.path.join(self.databaseLocation, f))]
             # for file in required_files:
             #     if file in has_files:
@@ -61,7 +61,7 @@ class COHDIndex:
             # if os.path.exists(f"{self.databaseLocation}/{old_version_database}"):
             #     os.remove(f"{self.databaseLocation}/{old_version_database}")
             # copy the database file to local if it doesn't exist
-            os.system(f"scp rtxconfig@arax.ncats.io:/data/orangeboard/databases/KG2.5.0/{self.databaseName} {database}")
+            os.system(f"scp rtxconfig@arax.ncats.io:/data/orangeboard/databases/KG2.5.1/{self.databaseName} {database}")
 
             self.connection = sqlite3.connect(database)
             print("INFO: Connecting to database", flush=True)
@@ -83,7 +83,7 @@ class COHDIndex:
     def create_tables(self):
 
         return
-        # # if self.success_con is True:
+        # if self.success_con is True:
         #     print("INFO: Creating database " + self.databaseName, flush=True)
         #     self.connection.execute(f"DROP TABLE IF EXISTS CURIE_TO_OMOP_MAPPING")
         #     self.connection.execute(f"CREATE TABLE CURIE_TO_OMOP_MAPPING( preferred_curie VARCHAR(255), concept_id INT )")
@@ -178,7 +178,7 @@ class COHDIndex:
         #     # for key in kg1_mapping:
         #     #     if key not in kg_mapping:
         #     #         kg_mapping[key] = kg1_mapping[key]
-        #     with open(f"{self.databaseLocation}/preferred_synonyms_kg2_5_0_with_concepts.pkl", "rb") as file:
+        #     with open(f"{self.databaseLocation}/preferred_synonyms_kg2_5_1_with_concepts.pkl", "rb") as file:
         #         kg = pickle.load(file)
 
         #     insert_command = 'INSERT INTO CURIE_TO_OMOP_MAPPING(preferred_curie,concept_id) values (?,?)'
