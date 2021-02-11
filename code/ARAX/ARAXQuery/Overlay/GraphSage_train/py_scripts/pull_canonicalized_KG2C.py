f3a72ce6736e051aa2b7a6b0cdee7a279224bcc6
Chunyu Ma <machunyu4402@hotmail.com>
2021-02-10 19:46:52 -0500

    #1233 update script for rebuilding DTD model and Database based on the new NodeSynonymizer fixing the error #1252

--- code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/pull_canonicalized_KG2C.py
+++ code/ARAX/ARAXQuery/Overlay/GraphSage_train/py_scripts/pull_canonicalized_KG2C.py
@@ -19,11 +19,14 @@ rtxc.live = 'KG2C'
 driver = GraphDatabase.driver(rtxc.neo4j_bolt, auth=(rtxc.neo4j_username, rtxc.neo4j_password))
 session = driver.session()
 
-## Pull a dataframe of all of the graph edges excluding 
-# 1. the nodes which are both 'drug' and 'disease'
-# 2. the nodes with type including 'drug' and 'disease' != category
-# 3. all edges directly connecting 'drug' and 'disease'
-query = "match (n) where (((n.category<>'biolink:Disease' and n.category<>'biolink:PhenotypicFeature' and n.category<>'biolink:DiseaseOrPhenotypicFeature') and ('biolink:Disease' in n.all_categories or 'biolink:PhenotypicFeature' in n.all_categories or 'biolink:DiseaseOrPhenotypicFeature' in n.all_categories)) or ((n.category<>'biolink:Drug' and n.category<>'biolink:ChemicalSubstance') and ('biolink:Drug' in n.all_categories or 'biolink:ChemicalSubstance' in n.all_categories))) or (('biolink:Disease' in n.all_categories or 'biolink:PhenotypicFeature' in n.all_categories or 'biolink:DiseaseOrPhenotypicFeature' in n.all_categories) and ('biolink:Drug' in n.all_categories or 'biolink:ChemicalSubstance' in n.all_categories)) with COLLECT(DISTINCT n.id) as exclude_id match (m1)-[]-(m2) where m1<>m2 and not m1.id in exclude_id and not m2.id in exclude_id and not ((m1.category='biolink:Disease' or m1.category='biolink:PhenotypicFeature' or m1.category='biolink:DiseaseOrPhenotypicFeature') and (m2.category='biolink:Drug' or m2.category='biolink:ChemicalSubstance')) and not ((m1.category='biolink:Drug' or m1.category='biolink:ChemicalSubstance') and (m2.category='biolink:Disease' or m2.category='biolink:PhenotypicFeature' or m2.category='biolink:DiseaseOrPhenotypicFeature')) with distinct m1 as node1, m2 as node2 return node1.id as source, node2.id as target"
+# !Note: Before running the below code, please first check this DSL query, if there is returned value > 0, report error on github.
+# !DSL query: match (z) where (('biolink:Disease' in z.all_categories or 'biolink:PhenotypicFeature' in z.all_categories or 'biolink:DiseaseOrPhenotypicFeature' in z.all_categories) and ('biolink:Drug' in z.all_categories or 'biolink:ChemicalSubstance' in z.all_categories)) return count(distinct z.id)
+
+## Pull a dataframe of all of the graph edges excluding:
+# the edges with one end node with all_categories including 'drug' and another end node with all_categories including 'disease'
+# 'drug' here represents all nodes with cateory that is either 'biolink:Drug' or 'biolink:ChemicalSubstance'
+# 'disease' here represents all nodes with cateory that is either 'biolink:Disease'. 'biolink:PhenotypicFeature' or 'biolink:DiseaseOrPhenotypicFeature'
+query = "match (disease) where ('biolink:Disease' in disease.all_categories or 'biolink:PhenotypicFeature' in disease.all_categories or 'biolink:DiseaseOrPhenotypicFeature' in disease.all_categories) with collect(distinct disease.id) as disease_ids match (drug) where ('biolink:Drug' in drug.all_categories or 'biolink:ChemicalSubstance' in drug.all_categories) with collect(distinct drug.id) as drug_ids, disease_ids as disease_ids match (m1)-[]-(m2) where m1<>m2 and not (m1.id in drug_ids and m2.id in disease_ids) and not (m1.id in disease_ids and m2.id in drug_ids) with distinct m1 as node1, m2 as node2 return node1.id as source, node2.id as target"
 res = session.run(query)
 KG2_alledges = pd.DataFrame(res.data())
 KG2_alledges.to_csv(output_path + 'graph_edges.txt', sep='\t', index=None)
@@ -36,13 +39,13 @@ KG2_allnodes_label = KG2_allnodes_label.iloc[:, [0, 2]]
 KG2_allnodes_label.to_csv(output_path + 'graph_nodes_label_remove_name.txt', sep='\t', index=None)
 
 ## Pulls a dataframe of all of the graph drug-associated nodes
-query = f"match (n:`biolink:ChemicalSubstance`) with distinct n.id as id, n.name as name return id, name union match (n:`biolink:Drug`) with distinct n.id as id, n.name as name return id, name"
+query = f"match (n) where ('biolink:Drug' in n.all_categories) or ('biolink:ChemicalSubstance' in n.all_categories) with distinct n.id as id, n.name as name return id, name"
 res = session.run(query)
 drugs = pd.DataFrame(res.data())
 drugs.to_csv(output_path + 'drugs.txt', sep='\t', index=None)
 
 ## Pulls a dataframe of all of the graph disease and phenotype nodes
-query = "match (n:`biolink:PhenotypicFeature`) with distinct n.id as id, n.name as name return id, name union match (n:`biolink:Disease`) with distinct n.id as id, n.name as name return id, name union match (n:`biolink:DiseaseOrPhenotypicFeature`) with distinct n.id as id, n.name as name return id, name"
+query = "match (n) where ('biolink:PhenotypicFeature' in n.all_categories) or ('biolink:Disease' in n.all_categories) or ('biolink:DiseaseOrPhenotypicFeature' in n.all_categories) with distinct n.id as id, n.name as name return id, name"
 res = session.run(query)
 diseases = pd.DataFrame(res.data())
 diseases.to_csv(output_path + 'diseases.txt', sep='\t', index=None)
