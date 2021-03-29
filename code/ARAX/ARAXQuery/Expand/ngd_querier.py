f582b82e3864417255f6ae6140e681ef944d3f3d
amykglen <glena@oregonstate.edu>
2021-03-28 17:40:11 -0700

    Include node iri and description in NGD expand #1331

--- code/ARAX/ARAXQuery/Expand/ngd_querier.py
+++ code/ARAX/ARAXQuery/Expand/ngd_querier.py
@@ -136,6 +136,7 @@ class NGDQuerier:
         ngd_node_key = kg2_node_key
         ngd_node.name = kg2_node.name
         ngd_node.category = kg2_node.category
+        ngd_node.attributes = [attribute for attribute in kg2_node.attributes if attribute.name in {"iri", "description"}]
         return ngd_node_key, ngd_node
 
     @staticmethod
