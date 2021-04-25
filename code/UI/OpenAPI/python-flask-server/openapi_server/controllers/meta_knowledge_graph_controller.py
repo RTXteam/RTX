1e321f8dd1ae0721ffc148ff69d525c3eefc4f01
edeutsch <edeutsch@systemsbiology.org>
2021-04-25 03:50:16 +0000

    updates for TRAPI 1.1  #1412

---
+++ code/UI/OpenAPI/python-flask-server/openapi_server/controllers/meta_knowledge_graph_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from openapi_server.models.meta_knowledge_graph import MetaKnowledgeGraph  # noqa: E501
+from openapi_server import util
+
+
+def meta_knowledge_graph():  # noqa: E501
+    """Meta knowledge graph representation of this TRAPI web service.
+
+     # noqa: E501
+
+
+    :rtype: MetaKnowledgeGraph
+    """
+    return 'do some magic!'
