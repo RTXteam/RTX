42af3812d4a9f79705cada90614c25c9c37b6c89
Eric Deutsch <edeutsch@systemsbiology.org>
2021-04-24 19:37:57 -0700

    update to furnish /meta_knowledge_graph for TRAPI 1.1  #1412

--- code/ARAX/KnowledgeSources/knowledge_source_metadata.py
+++ code/ARAX/KnowledgeSources/knowledge_source_metadata.py
@@ -19,17 +19,12 @@ class KnowledgeSourceMetadata:
     def __init__(self):
         pass
 
-    #### Top level decision maker for applying filters
-    def get_kg_predicates(self, kg_name='KG2'):
+    #### Get a list of all supported subjects, predicates, and objects and reformat to /predicates format
+    def get_kg_predicates(self):
         method_name = inspect.stack()[0][3]
 
-        # Determine the table prefix for the knowledge graph selected
-        if kg_name.upper() == 'KG1' or kg_name.upper() == 'KG2':
-            kg_prefix = kg_name.upper()
-        else:
-            eprint(f"ERROR [{method_name}]: kg_name must be either 'KG1' or 'KG2'")
-            return None
-
+        # We always furnish KG2C results
+        kg_prefix = 'KG2C'
 
         # Verify that the source data file exists
         input_filename = os.path.dirname(os.path.abspath(__file__)) + f"/{kg_prefix}_allowed_predicate_triples.csv"
@@ -65,13 +60,36 @@ class KnowledgeSourceMetadata:
         return predicates
 
 
+    #### Get a list of all supported subjects, predicates, and objects and return in /meta_knowledge_graph format
+    def get_meta_knowledge_graph(self):
+        method_name = inspect.stack()[0][3]
+
+        # We always furnish KG2C results
+        kg_prefix = 'RTX_KG2C'
+
+
+        # Verify that the source data file exists
+        input_filename = os.path.dirname(os.path.abspath(__file__)) + f"/{kg_prefix}_Data.json"
+        if not os.path.exists(input_filename):
+            eprint(f"ERROR [{method_name}]: File '{input_filename}' not found")
+            return None
+
+        try:
+            with open(input_filename) as infile:
+                return json.load(infile)
+        except:
+            eprint(f"ERROR: Unable to read meta_knowledge_graph from file '{input_filename}'")
+            return
+
+
 ##########################################################################################
 def main():
 
     ksm = KnowledgeSourceMetadata()
-    #predicates = ksm.get_kg_predicates(kg_name='KG1')
-    predicates = ksm.get_kg_predicates(kg_name='KG2')
+    predicates = ksm.get_kg_predicates()
     print(json.dumps(predicates,sort_keys=True,indent=2))
 
+    meta_knowledge_graph = ksm.get_meta_knowledge_graph()
+    print(json.dumps(meta_knowledge_graph,sort_keys=True,indent=2))
 
 if __name__ == "__main__": main()
