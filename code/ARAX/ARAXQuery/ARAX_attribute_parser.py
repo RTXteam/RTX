4e2e4ae30ad0a063060f1201053ea9a3030ff44b
Eric Deutsch <edeutsch@systemsbiology.org>
2021-07-23 03:02:25 +0000

    fixed some bugs and increased granularity of breakdowns #1575

--- code/ARAX/ARAXQuery/ARAX_attribute_parser.py
+++ code/ARAX/ARAXQuery/ARAX_attribute_parser.py
@@ -27,7 +27,8 @@ class ARAXAttributeParser:
 
 
     def summarize_provenance_info(self):
-        provenance_information = {}
+        provenance_information = { 'n_sources': 0, 'provenance_counts': {}, 'predicate_counts': {} }
+        sources = {}
         information_type_ids = {
             'biolink:knowledge_source' : True,
             'biolink:original_knowledge_source': True,
@@ -37,29 +38,35 @@ class ARAXAttributeParser:
         }
  
         #### If message is in dict form, just work with that instead of converting it first
-        if isinstance(self.message,'dict'):
+        if isinstance(self.message,dict):
             if 'knowledge_graph' not in self.message or self.message['knowledge_graph'] is None:
                 return provenance_information
             if 'edges' not in self.message['knowledge_graph'] or self.message['knowledge_graph']['edges'] is None:
                 return provenance_information
             for edge_key, edge in self.message['knowledge_graph']['edges'].items():
                 found_provenance = False
+                predicate = edge['predicate']
+                if predicate not in provenance_information['predicate_counts']:
+                    provenance_information['predicate_counts'][predicate] = 0
+                provenance_information['predicate_counts'][predicate] += 1
                 if 'attributes' in edge and edge['attributes'] is not None:
                     for attribute in edge['attributes']:
                         if 'attribute_type_id' in attribute and attribute['attribute_type_id'] is not None and attribute['attribute_type_id'] in information_type_ids and 'value' in attribute:
                             value = attribute['value']
                             if value is None:
                                 value = '???'
-                            label = f"{attribute.attribute_type_id} = {value}"
-                            if label not in provenance_information:
-                                provenance_information[label] = 0
-                            provenance_information[label] += 1
+                            sources[f"{value}"] = True
+                            label = f"{predicate} --> {attribute['attribute_type_id']} = {value}"
+                            if label not in provenance_information['provenance_counts']:
+                                provenance_information['provenance_counts'][label] = 0
+                            provenance_information['provenance_counts'][label] += 1
                             found_provenance = True
                 if not found_provenance:
-                    label = 'edges with no provenance'
-                    if label not in provenance_information:
-                        provenance_information[label] = 0
-                    provenance_information[label] += 1
+                    label = f"{predicate} --> no provenance"
+                    if label not in provenance_information['provenance_counts']:
+                        provenance_information['provenance_counts'][label] = 0
+                    provenance_information['provenance_counts'][label] += 1
+            provenance_information['n_sources'] = len(sources)
 
         #### Else assume it is objects
         else:
