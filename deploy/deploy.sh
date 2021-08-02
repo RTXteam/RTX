3b1b017e3e7043d2412c4cd4abf229cdc3c1cc45
oacob1 <oacob1@gmail.com>
2021-08-02 19:43:35 -0400

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,8 @@ projectName="arax"
 namespace="rtx"
 # replace place_holder with values from env var
 # env var's key needs to be the same as the place_holder
-toReplace=('BUILD_VERSION' 'SECRET_FILE')
+# toReplace=('BUILD_VERSION' 'SECRET_FILE')
+toReplace=('BUILD_VERSION')
 
 # export .env values to env vars
 export $(egrep -v '^#' .env)
