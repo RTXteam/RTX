475bce1c9e1452a855f6f338ed86690132c87afc
oacob1 <oacob1@gmail.com>
2021-08-10 14:59:00 -0400

    test

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -7,16 +7,14 @@ chmod -R 755 /mnt/data/orangeboard/databases/
 
 # Update permission on /mnt/data/orangeboard/production/RTX/code/ARAX/KnowledgeSources/db_versions.json
 # since code/ARAX/ARAXQuery/ARAX_database_manager.py will be read and write to it while its permission is 644
-chmod 777 /mnt/data/orangeboard/production/RTX/code/ARAX/KnowledgeSources/db_versions.json
+# chmod 777 /mnt/data/orangeboard/production/RTX/code/ARAX/KnowledgeSources/db_versions.json
 
 # the instructions below are from the deployment wiki at https://github.com/RTXteam/RTX/wiki/ARAX-Docker-Deployment
 su rt 
 cd /mnt/data/orangeboard/production/RTX 
 python3 code/ARAX/ARAXQuery/ARAX_database_manager.py
-# change back to root user
-exit
 
-service apache2 start
-service RTX_OpenAPI_production start
+sudo service apache2 start
+sudo service RTX_OpenAPI_production start
 # this line is added to stop container completes with exit code 0
 tail -f /dev/null
