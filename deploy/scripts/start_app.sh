dec42595a129733c09959a833b79e0f6cc6dcab8
oacob1 <oacob1@gmail.com>
2021-08-16 20:51:35 -0400

    customize volume mounts dir for general usage

--- deploy/scripts/start_app.sh
+++ deploy/scripts/start_app.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # rename config file
-cp /configv2/configv2.json /mnt/data/orangeboard/production/RTX/code/config_local.json
+cp /configs/configv2.json /mnt/data/orangeboard/production/RTX/code/config_local.json
 
 # Update database directory permission which is mounted from a PVC
 chmod -R 755 /mnt/data/orangeboard/databases/
-
-# Update permission on /mnt/data/orangeboard/production/RTX/code/ARAX/KnowledgeSources/db_versions.json
-# since code/ARAX/ARAXQuery/ARAX_database_manager.py will be read and write to it while its permission is 644
-# chmod 777 /mnt/data/orangeboard/production/RTX/code/ARAX/KnowledgeSources/db_versions.json
 
 # the instructions below are from the deployment wiki at https://github.com/RTXteam/RTX/wiki/ARAX-Docker-Deployment
 su rt 
