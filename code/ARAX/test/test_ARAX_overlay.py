c7ee3dec34a54b90b1375c39b8ed79c1bd99d9b8
finnagin <flan313@gmail.com>
2021-08-31 13:56:02 -0700

    #1612, remove is_defined_by from overlay tests

--- code/ARAX/test/test_ARAX_overlay.py
+++ code/ARAX/test/test_ARAX_overlay.py
@@ -238,7 +238,7 @@ def test_FET_ex1():
         edge_attributes_dict = {attr.original_attribute_name:attr.value for attr in edge.attributes}
         assert edge.attributes[0].original_attribute_name == 'fisher_exact_test_p-value'
         assert edge.attributes[0].attribute_type_id == 'EDAM:data_1669'
-        assert edge_attributes_dict['is_defined_by'] == 'ARAX'
+        #assert edge_attributes_dict['is_defined_by'] == 'ARAX'
         assert edge_attributes_dict['provided_by'] == 'infores:arax'
         if edge.relation == 'FET1':
             assert 0 <= float(edge.attributes[0].value) < 0.001
@@ -283,7 +283,7 @@ def test_FET_ex2():
         edge_attributes_dict = {attr.original_attribute_name:attr.value for attr in edge.attributes}
         assert edge.attributes[0].original_attribute_name == 'fisher_exact_test_p-value'
         assert edge.attributes[0].attribute_type_id == 'EDAM:data_1669'
-        assert edge_attributes_dict['is_defined_by'] == 'ARAX'
+        #assert edge_attributes_dict['is_defined_by'] == 'ARAX'
         assert edge_attributes_dict['provided_by'] == 'infores:arax'
     FET_query_edges = {key:edge for key, edge in message.query_graph.edges.items() if key.find("FET") != -1}
     assert len(FET_query_edges) == 1
