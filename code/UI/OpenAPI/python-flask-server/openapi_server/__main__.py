e04799b391ff7db9f1ed449bd383c68acf58d8a7
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-10-05 11:45:24 -0700

    #1691

--- code/UI/OpenAPI/python-flask-server/openapi_server/__main__.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/__main__.py
@@ -5,14 +5,6 @@ import flask
 import logging
 import signal
 
-## :DEBUG: vvvvvvvvvvvvvvvvvv
-#logging.basicConfig(filename="/tmp/connexion_fork-test.log",
-#                    filemode="a",
-#                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-#                                                datefmt='%H:%M:%S',
-#                                                level=logging.DEBUG)
-# :DEBUG: ^^^^^^^^^^^^^^^^^^
-
 from flask_cors import CORS
 
 from openapi_server import encoder
@@ -22,28 +14,16 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../ARAX/ARAXQuery")
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../reasoningtool/QuestionAnswering")
 
-# :DEBUG: vvvvvvvvvvvvvvvvvv
-# # Capture and log any unexpected exceptions raise by handler code:
-#def error_handler(exception):
-#    flask.current_app.logger.exception(exception)
-# :DEBUG: ^^^^^^^^^^^^^^^^^^
-
 def receive_sigchld(signal_number, frame):
     if signal_number == signal.SIGCHLD:
         os.waitpid(-1, os.WNOHANG)
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
-# :DEBUG: vvvvvvvvvvvvvvvvvv
-#    app.app.logger.debug("Starting up connexion server")
-# :DEBUG: ^^^^^^^^^^^^^^^^^^
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'ARAX Translator Reasoner'},
                 pythonic_params=True)
-# :DEBUG: vvvvvvvvvvvvvvvvvv
-#    app.add_error_handler(Exception, error_handler)
-# :DEBUG: ^^^^^^^^^^^^^^^^^^
     CORS(app.app)
     signal.signal(signal.SIGCHLD, receive_sigchld)
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
