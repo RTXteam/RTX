5b54d56d262ba827c1f74fc1d87f5000ce5972aa
finnagin <flan313@gmail.com>
2021-10-05 02:29:33 -0700

    #1664, added sort by node attribute

--- code/ARAX/ARAXQuery/operation_to_ARAXi.py
+++ code/ARAX/ARAXQuery/operation_to_ARAXi.py
@@ -207,10 +207,22 @@ class WorkflowToARAXi:
         value = parameters.get('remove_value',None)
         # FW: need to update this to handle qedge_keys and qnode_keys
         araxi_string = f"filter_results(action=sort_by_edge_attribute,edge_attribute={parameters['edge_attribute']},direction={ascending_or_descending}"
-        # if "qnode_keys" in parameters:
-        #     araxi_string += f",remove_connected_nodes=t,qnode_keys={parameters['qnode_keys']}"
         # if "qedge_keys" in parameters:
         #     araxi_string += f",qedge_keys={parameters['qedge_keys']}"
+        araxi_string += ")"
+        ARAXi.append(araxi_string)
+        return ARAXi
+
+    @staticmethod
+    def __translate_sort_results_node_attribute(parameters, query_graph, response):
+        if ("node_attribute" not in parameters) or ("ascending_or_descending" not in parameters):
+            response.error("The operation sort_by_node_attribute must have the parameters node_attribute and ascending_or_descending", error_code="KeyError")
+        ARAXi = []
+        value = parameters.get('remove_value',None)
+        # FW: need to update this to handle qedge_keys and qnode_keys
+        araxi_string = f"filter_results(action=sort_by_node_attribute,node_attribute={parameters['node_attribute']},direction={ascending_or_descending}"
+        # if "qnode_keys" in parameters:
+        #     araxi_string += f",remove_connected_nodes=t,qnode_keys={parameters['qnode_keys']}"
         araxi_string += ")"
         ARAXi.append(araxi_string)
         return ARAXi
