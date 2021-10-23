760342cf11074561d75d904caaa97edd6c10c85f
Eric Deutsch <edeutsch@systemsbiology.org>
2021-10-23 04:51:08 +0000

    add support for UI changes

--- code/UI/OpenAPI/python-flask-server/openapi_server/controllers/status_controller.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/controllers/status_controller.py
@@ -5,7 +5,7 @@ from openapi_server import util
 from ARAX_query_tracker import ARAXQueryTracker
 
 
-def get_status(last_n_hours=None, id_=None):  # noqa: E501
+def get_status(last_n_hours=None, id_=None, terminate_pid=None, authorization=None):  # noqa: E501
     """Obtain status information about the endpoint
 
      # noqa: E501
@@ -14,10 +14,17 @@ def get_status(last_n_hours=None, id_=None):  # noqa: E501
     :type last_n_hours: int
     :param id: Identifier of the log entry
     :type id: int
+    :param terminate_pid: PID of an ongoing query to terminate
+    :type terminate_pid: int
+    :param authorization: Authorization string required for certain calls to status
+    :type authorization: str
 
     :rtype: object
     """
 
     query_tracker = ARAXQueryTracker()
-    status = query_tracker.get_status(last_n_hours=last_n_hours, id_=id_)
+    if terminate_pid is not None:
+        status = query_tracker.terminate_job(terminate_pid=None, authorization=None)
+    else:
+        status = query_tracker.get_status(last_n_hours=last_n_hours, id_=id_)
     return status
