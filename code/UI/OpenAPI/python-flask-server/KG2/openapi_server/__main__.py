619504003db666f55fa2af1264e33040dc7246fa
saramsey <ramseyst@oregonstate.edu>
2021-11-13 01:37:57 +0000

    #1683

--- code/UI/OpenAPI/python-flask-server/KG2/openapi_server/__main__.py
+++ code/UI/OpenAPI/python-flask-server/KG2/openapi_server/__main__.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python3
 
-import connexion
-import flask
+import connexion, flask, flask_cors
 import logging
-import signal
 import json
+import openapi_server.encoder
+import os, sys, signal
 
-from flask_cors import CORS
-
-from openapi_server import encoder
-
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../../ARAX/ARAXQuery")
+logging.basicConfig(level=logging.INFO)  # can change this to logging.DEBUG for debuggging
 
 def receive_sigchld(signal_number, frame):
     if signal_number == signal.SIGCHLD:
         try:
             os.waitpid(-1, os.WNOHANG)
         except ChildProcessError as e:
-            print(repr(e), file=sys.stderr)
+            logging.error(repr(e))
 
 def receive_sigpipe(signal_number, frame):
     if signal_number == signal.SIGPIPE:
-        print("pipe error", file=sys.stderr)
-        sys.stderr.flush()
+        logging.error("pipe error")
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
+    app.app.json_encoder = openapi_server.encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'RTX KG2 Translator KP'},
                 pythonic_params=True)
-    CORS(app.app)
+    flask_cors.CORS(app.app)
     signal.signal(signal.SIGCHLD, receive_sigchld)
     signal.signal(signal.SIGPIPE, receive_sigpipe)
 
