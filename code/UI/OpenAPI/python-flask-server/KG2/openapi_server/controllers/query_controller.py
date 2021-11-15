12df14bf698ec42bc35bcbd69f7d329655011510
Eric Deutsch <edeutsch@systemsbiology.org>
2021-11-15 22:44:44 +0000

    updated

--- code/UI/OpenAPI/python-flask-server/KG2/openapi_server/controllers/query_controller.py
+++ code/UI/OpenAPI/python-flask-server/KG2/openapi_server/controllers/query_controller.py
@@ -84,9 +84,14 @@ def query(request_body):  # noqa: E501
 
     query = connexion.request.get_json()  # :QUESTION: why don't we use `request_body`?
 
+    mime_type = 'application/json'
+
+    http_status = 200
     if query.get('stream_progress', False):  # if stream_progress is specified and if it is True:
 
         fork_mode = True # :DEBUG: can turn this to False to disable fork-mode
+        http_status = None
+        mime_type = 'text/event-stream'
 
         if not fork_mode:
             json_generator = _run_query_and_return_json_generator_stream(query)
@@ -99,8 +104,13 @@ def query(request_body):  # noqa: E501
         json_generator = run_query_dict_in_child_process(query,
                                                          _run_query_and_return_json_generator_nonstream)
 
-    return flask.Response(json_generator, mimetype='application/json')
- 
+    if http_status is not None and hasattr(resp_obj, 'http_status'):
+        http_status = resp_obj.http_status
+    else:
+        http_status = None
+
+    return (flask.Response(json_generator, mimetype=mime_type), http_status)
+
 
 # :TESTING: vvvvvvvvvvvvvvvvvv
 # if __name__ == "__main__":
