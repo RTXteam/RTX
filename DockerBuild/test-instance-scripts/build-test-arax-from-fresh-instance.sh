27120f6eaa105f445ad988e53617e426d7c57d37
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-11-18 12:26:39 -0800

    #1741

--- DockerBuild/test-instance-scripts/build-test-arax-from-fresh-instance.sh
+++ DockerBuild/test-instance-scripts/build-test-arax-from-fresh-instance.sh
@@ -5,26 +5,43 @@ set -o nounset -o pipefail -o errexit
 arax_base=/mnt/data/orangeboard
 
 sudo apt-get update
+
+# --------------------------------------------------------------------------------------------------
+# the following are optional; they are just for convenience in testing and debugging in the instance:
 sudo apt-get install -y emacs  # convenience for Steve
 sudo apt-get install -y netcat # useful for debugging
+# --------------------------------------------------------------------------------------------------
+
 sudo apt-get install -y docker.io
-export VENV_DIR=/home/ubuntu/venv
+
+# install python3.7 (with pip) into the host OS, using the Ubuntu packages
+export VENV_DIR=/home/ubuntu/venv   # have to set this environment variable for setup-python37-in-ubuntu18.shinc to work properly
 source <(curl -s https://raw.githubusercontent.com/RTXteam/RTX-KG2/master/setup-python37-in-ubuntu18.shinc)
+
+# clone the ARAX software repo from GitHub (master branch)
+rm -r -f RTX
 git clone https://github.com/RTXteam/RTX.git
+
+# create the directory for the databases, if it doesn't exist already (don't delete it if it already exists!)
 sudo mkdir -p ${arax_base}/databases
 sudo chown ubuntu.ubuntu ${arax_base}/databases
-scp araxconfig@araxconfig.rtx.ai:configv2.json ~
-cat ~/configv2.json | \
+
+# copy the config file into the RTX/code directory
+scp araxconfig@araxconfig.rtx.ai:configv2.json RTX/code
+
+# create config_local.json that points to the local RTX-KG2 API
+cat RTX/code/configv2.json | \
     sed 's|https://arax.ncats.io/beta/api/rtxkg2/v1.2|http://localhost:5008/api/rtxkg2/v1.2|g' > \
 	RTX/code/config_local.json
 
 # download the database files (this step takes a long time)
 venv/bin/python3 RTX/code/ARAX/ARAXQuery/ARAX_database_manager.py --mnt --skip-if-exists
 
+# copy the dockerfile to the CWD so we can modify it in-place
 cp RTX/DockerBuild/Merged-Dockerfile .
 
 # --------------------------------------------------------------------------------------------------
-# the following are just for convenience in testing and debugging inside the container:
+# the following are optional; they are just for convenience in testing and debugging inside the container:
 echo <<EOF >> ./Merged-Dockerfile
 RUN apt-get install -y netcat 
 RUN apt-get install -y emacs
@@ -54,7 +71,7 @@ sudo docker start arax
 # done
 
 # create the required symbolic links for ARAX/KG2 database files, inside the container
-sudo docker exec arax bash -c "su rt; cd /mnt/data/orangeboard/production/RTX; python3 code/ARAX/ARAXQuery/ARAX_database_manager.py"
+sudo docker exec arax bash -c "su rt; cd ${arax_base}/production/RTX; python3 code/ARAX/ARAXQuery/ARAX_database_manager.py"
 
 # start all the services
 sudo docker exec arax service apache2 start
