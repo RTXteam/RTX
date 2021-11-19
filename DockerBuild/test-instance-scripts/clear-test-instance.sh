5a20a16e9ee3f49eee68432fe32d98e286685525
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-11-18 21:05:01 -0800

    #1741

--- DockerBuild/test-instance-scripts/clear-test-instance.sh
+++ DockerBuild/test-instance-scripts/clear-test-instance.sh
@@ -8,11 +8,12 @@ read -p "Are you sure you are running this command in the ARAX test instance and
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    cd ~
     sudo docker exec arax service RTX_OpenAPI_production stop
     sudo docker exec arax service RTX_OpenAPI_kg2 stop
     sudo docker exec arax service apache2 stop
     sudo docker stop arax
     sudo docker rm arax
     sudo docker image rm arax:1.0
-    sudo rm -r -f RTX venv Merged-Dockerfile
+    sudo rm -r -f RTX venv Merged-Dockerfile build-test-arax-from-fresh-instance.sh
 fi
