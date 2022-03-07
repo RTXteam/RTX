2ae87e7cf3cca88b504459073444c0ff863e1022
finnagin <flan313@gmail.com>
2022-03-06 17:45:42 -0800

    updated test

--- code/ARAX/test/test_ARAX_translate.py
+++ code/ARAX/test/test_ARAX_translate.py
@@ -539,14 +539,18 @@ def test_connect_knodes_2_nodes():
     [response, message] = _do_arax_query(query)
     assert response.status == 'OK'
     assert len(message.results) == 30
-    connected_bindings = set()
+    connected_bindings_ngd = set()
+    connected_bindings_fisher = set()
     for result in message.results:
         assert result.score is not None
         for eb_key, edge_bindings in result.edge_bindings.items():
             for edge_binding in edge_bindings:
-                if edge_binding.id.startswith("connect_knodes"):
-                    connected_bindings.add(edge_binding.id)
-    assert len(connected_bindings) > 0
+                if edge_binding.id.startswith("connect_knodes_fisher"):
+                    connected_bindings_fisher.add(edge_binding.id)
+                elif edge_binding.id.startswith("connect_knodes_ngd"):
+                    connected_bindings_ngd.add(edge_binding.id)
+    assert len(connected_bindings_ngd) > 0
+    assert len(connected_bindings_fisher) > 0
 
 @pytest.mark.slow
 def test_connect_knodes_3_nodes():
