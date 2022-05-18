4882d29be9a0fd886b8d06288d99057bc1512ef4
Eric Deutsch <edeutsch@systemsbiology.org>
2022-05-18 03:34:50 +0000

    enable better logging and move the caching to a separate database because the production database is now read-only

--- code/autocomplete/rtxcomplete.py
+++ code/autocomplete/rtxcomplete.py
@@ -3,6 +3,7 @@ import re
 import timeit
 import sys
 import os
+def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
 
 pathlist = os.path.realpath(__file__).split(os.path.sep)
 RTXindex = pathlist.index("RTX")
@@ -15,21 +16,33 @@ autocomplete_filepath = os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code', 'a
 
 conn = None
 cursor = None
+cache_conn = None
+cache_cursor = None
 
 
 def load():
     global conn
     global cursor
+    global cache_conn
+    global cache_cursor
     database_name = f"{autocomplete_filepath}{os.path.sep}{RTXConfig.autocomplete_path.split('/')[-1]}"
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
-    #print(f"INFO: Connected to {database_name}",file=sys.stderr)
+    print(f"INFO: Connected to {database_name}",file=sys.stderr)
+
+    cache_database_name = os.path.dirname(os.path.abspath(__file__)) + '/rtxcomplete_cache.sqlite'
+    cache_conn = sqlite3.connect(cache_database_name)
+    cache_cursor = cache_conn.cursor()
+    print(f"INFO: Connected to {cache_database_name}",file=sys.stderr)
+    cache_cursor.execute("CREATE TABLE IF NOT EXISTS cached_fragments(fragment VARCHAR(1024))")
+    cache_cursor.execute("CREATE TABLE IF NOT EXISTS cached_fragment_terms(fragment_id VARCHAR(1024), term VARCHAR(1024))")
+
     return True
 
 
 def get_nodes_like(word,requested_limit):
 
-    debug = False
+    debug = True
 
     t0 = timeit.default_timer()
     requested_limit = int(requested_limit)
@@ -48,7 +61,7 @@ def get_nodes_like(word,requested_limit):
 
     #### Get a list of matching node names that begin with these letters
     if debug:
-        print(f"INFO: Query 1")
+        eprint(f"INFO: Query 1")
     #cursor.execute("SELECT term FROM term WHERE term LIKE \"%s%%\" ORDER BY length(term),term LIMIT %s" % (word,1000))
     cursor.execute(f"SELECT term FROM terms WHERE term > \"{floor}\" AND term < \"{ceiling}\" AND term LIKE \"{word}%%\" ORDER BY length(term),term LIMIT {requested_limit}")
     rows = cursor.fetchall()
@@ -57,7 +70,7 @@ def get_nodes_like(word,requested_limit):
         term = row[0]
         if term.upper() not in values_dict:
             if debug:
-                print(f"    - {term}")
+                eprint(f"    - {term}")
             properties = { "curie": '??', "name": term, "type": '??' }
             values.append(properties)
             values_dict[term.upper()] = 1
@@ -66,19 +79,19 @@ def get_nodes_like(word,requested_limit):
                 break
     t1 = timeit.default_timer()
     if debug:
-        print(f"INFO: Query 1 in {t1-t0} sec")
+        eprint(f"INFO: Query 1 in {t1-t0} sec")
 
     #### If we haven't reached the limit yet, add a list of matching terms that contain this string
     if n_values < requested_limit:
         if debug:
-            print(f"INFO: Query 2")
+            eprint(f"INFO: Query 2")
 
         #### See if there is a cached entry already
         word_part = word
         found_fragment = None
         while len(word_part) > 2:
-            cursor.execute(f"SELECT rowid, fragment FROM cached_fragments WHERE fragment == \"{word_part}\"")
-            rows = cursor.fetchall()
+            cache_cursor.execute(f"SELECT rowid, fragment FROM cached_fragments WHERE fragment == \"{word_part}\"")
+            rows = cache_cursor.fetchall()
             if len(rows) > 0:
                 fragment_id = rows[0][0]
                 found_fragment = rows[0][1]
@@ -87,10 +100,10 @@ def get_nodes_like(word,requested_limit):
 
         if found_fragment:
             if debug:
-                print(f"Found matching fragment {found_fragment} as fragment_id {fragment_id}")
+                eprint(f"Found matching fragment {found_fragment} as fragment_id {fragment_id}")
 
-            cursor.execute(f"SELECT term FROM cached_fragment_terms WHERE fragment_id = {fragment_id} AND term LIKE \"%%{word}%%\"")
-            rows = cursor.fetchall()
+            cache_cursor.execute(f"SELECT term FROM cached_fragment_terms WHERE fragment_id = {fragment_id} AND term LIKE \"%%{word}%%\"")
+            rows = cache_cursor.fetchall()
 
             for row in rows:
                 term = row[0]
@@ -108,13 +121,13 @@ def get_nodes_like(word,requested_limit):
 
             #### Cache this fragment in the database
             try:
-                cursor.execute("INSERT INTO cached_fragments(fragment) VALUES(?)", (word,))
-                fragment_id = cursor.lastrowid
+                cache_cursor.execute("INSERT INTO cached_fragments(fragment) VALUES(?)", (word,))
+                fragment_id = cache_cursor.lastrowid
             except:
-                print(f"ERROR: Unable to INSERT into cached_fragments(fragment)",file=sys.stderr)
+                eprint(f"ERROR: Unable to INSERT into cached_fragments(fragment)",file=sys.stderr)
                 fragment_id = 0
             if debug:
-                print(f"fragment_id = {fragment_id}")
+                eprint(f"fragment_id = {fragment_id}")
 
             #### Execute an expensive LIKE query
             cursor.execute("SELECT term FROM terms WHERE term LIKE \"%%%s%%\" ORDER BY length(term),term LIMIT %s" % (word,10000))
@@ -126,18 +139,18 @@ def get_nodes_like(word,requested_limit):
 
                     if n_values < requested_limit:
                         if debug:
-                            print(f"    - {term}")
+                            eprint(f"    - {term}")
                         properties = { "curie": '??', "name": term, "type": '??' }
                         values.append(properties)
                         n_values += 1
 
                     values_dict[term.upper()] = 1
-                    cursor.execute("INSERT INTO cached_fragment_terms(fragment_id, term) VALUES(?,?)", (fragment_id, term,))
-            conn.commit()
+                    cache_cursor.execute("INSERT INTO cached_fragment_terms(fragment_id, term) VALUES(?,?)", (fragment_id, term,))
+            cache_conn.commit()
 
         t2 = timeit.default_timer()
         if debug:
-            print(f"INFO: Query 2 in {t2-t1} sec")
+            eprint(f"INFO: Query 2 in {t2-t1} sec")
 
 
     return(values)
