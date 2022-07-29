ce545011e202e14e3b2da3b5928a981077768944
dkoslicki <dmkoslicki@gmail.com>
2022-07-29 15:23:30 -0400

    add test for #1881

--- code/ARAX/test/test_ARAX_connect.py
+++ code/ARAX/test/test_ARAX_connect.py
@@ -133,7 +133,23 @@ def test_connect_nodes_3_disconnected_nodes():
             "add_qnode(name=MESH:D004781,key=n01)",
             "connect(action=connect_nodes)",
             "resultify(ignore_edge_direction=true, debug=true)",
-            "return(message=true, store=fale)"
+            "return(message=true, store=false)"
+        ]}}
+    [response, message] = _do_arax_query(query)
+    assert response.status == 'OK'
+    assert len(message.query_graph.edges) >= 3
+    assert len(message.results) > 0
+
+
+def test_1881():
+    query = {"operations": {"actions": [
+            "create_message",
+            "add_qnode(name=glucose, key=n0)",
+            "add_qnode(name=diabetes, key=n1)",
+            "connect(action=connect_nodes, max_path_length=3, shortest_path=false)",
+            "resultify()",
+            "filter_results(action=limit_number_of_results, max_results=30)",
+            "return(message=true, store=true)"
         ]}}
     [response, message] = _do_arax_query(query)
     assert response.status == 'OK'
