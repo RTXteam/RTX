46bc24363c56da49c065fdd60075ce473c3904ec
amykglen <glena@oregonstate.edu>
2022-08-16 15:00:36 -0700

    Only fill out KG2 URL in RTXConfig if it's an override #1466

--- code/RTXConfiguration.py
+++ code/RTXConfiguration.py
@@ -136,25 +136,12 @@ class RTXConfiguration:
         else:  # Includes staging, development
             self.plover_url = self.config_dbs["plover"]["dev"]
 
-        # TEMPORARILY set KG2 url here until pulled from SmartAPI; TODO: remove this when #1466 is done
+        # Set KG2 url if an override was provided
         kg2_url_override_value = self._read_override_file(f"{file_dir}/kg2_url_override.txt")
         if kg2_url_override_value:
             self.rtx_kg2_url = kg2_url_override_value
-        elif self.is_itrb_instance:
-            if self.maturity in {"production", "prod"}:
-                self.rtx_kg2_url = "https://kg2.transltr.io/api/rtxkg2/v1.2"
-            elif self.maturity in {"testing", "test"}:
-                self.rtx_kg2_url = "https://kg2.test.transltr.io/api/rtxkg2/v1.2"
-            else:
-                self.rtx_kg2_url = "https://kg2.ci.transltr.io/api/rtxkg2/v1.2"
         else:
-            if "NewFmt" in self.instance_name or self.current_branch_name == "NewFmt":
-                self.rtx_kg2_url = "https://arax.ncats.io/api/rtxkg2/v1.3"
-            elif self.maturity in {"production", "prod"}:
-                self.rtx_kg2_url = "https://arax.ncats.io/api/rtxkg2/v1.2"
-            else:
-                self.rtx_kg2_url = "https://arax.ncats.io/beta/api/rtxkg2/v1.2"
-        # TODO: add special exception for CICD (needs to point to localhost KG2)
+            self.rtx_kg2_url = None
 
         # Default to KG2c neo4j
         self.neo4j_kg2 = "KG2c"
