25e535948c8317581c570d3cd54ea95ac7998b0d
Pouyan Ahmadi <pahmadi@gmu.edu>
2022-08-17 10:08:44 -0400

    feat: sync arax with kg2

--- deploy/kg2/scripts/start_app.sh
+++ deploy/kg2/scripts/start_app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # rename config file
-cp /configs/configv2.json /mnt/data/orangeboard/kg2/RTX/code/config_local.json
+cp /configs/config_secrets.json /mnt/data/orangeboard/kg2/RTX/code/config_secrets.json
 
 # Update database directory permission which is mounted from a PVC
-chmod -R 755 /mnt/data/orangeboard/databases/
+chmod -R 777 /mnt/data/orangeboard/databases/
+# change owner and groups for database dir
+chown -R rt:rt /mnt/data/orangeboard/databases/
 
 # the instructions below are from the deployment wiki at https://github.com/RTXteam/RTX/wiki/RTX-KG2-API-Docker-Deployment
 su rt 
@@ -12,6 +14,6 @@ python3 code/ARAX/ARAXQuery/ARAX_database_manager.py
 
 sudo service apache2 start
 sudo service RTX_OpenAPI_kg2 start
-# sudo service RTX_Complete start
+sudo service RTX_Complete start
 # this line is added to stop container completes with exit code 0
-tail -f /dev/null
+tail -f /var/log/apache2/access.log
