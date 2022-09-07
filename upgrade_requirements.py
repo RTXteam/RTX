d680145c006c50ca721d5cab5d16b4689031b5d2
amykglen <glena@oregonstate.edu>
2022-09-07 15:32:13 -0700

    Fix package updater

--- upgrade_requirements.py
+++ upgrade_requirements.py
@@ -4,21 +4,26 @@ def get_package_name(package_with_version: str) -> str:
     return package_with_version.split("=")[0].replace(">", "").replace("<", "")
 
 
-old_requirements = tuple(open("requirements.txt", 'r'))
-new_freeze = tuple(open("freeze-new.txt", 'r'))
+with open(f"requirements.txt") as old_requirements_file:
+    old_requirements = tuple(old_requirements_file)
+with open(f"freeze-new.txt") as new_freeze_file:
+    new_requirements = tuple(new_freeze_file)
 packages_dict = dict()
 
-for old_requirement in old_requirements:
+index_line = old_requirements[0]
+for old_requirement in old_requirements[1:]:  # Skip the --index line
+    old_requirement.replace("\n", "")
     package = get_package_name(old_requirement)
-    packages_dict[package] = None
+    old_version_str = old_requirement.replace(package, "")
+    packages_dict[package] = old_version_str
 
-for new_requirement in new_freeze:
+for new_requirement in new_requirements:
     package = get_package_name(new_requirement)
     if package in packages_dict:
         version_str = new_requirement.replace(package, "")
         packages_dict[package] = version_str
-        print(f"New version for {package} is {version_str}")
 
 with open(f"requirements.txt", "w+") as requirements_file:
+    requirements_file.write(index_line)
     for package, version_str in packages_dict.items():
         requirements_file.write(f"{package}{version_str}")
