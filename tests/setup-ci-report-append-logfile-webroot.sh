e54980b6e26b5fb9733b202161536fbe78f8c2a8
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2022-10-05 15:16:30 -0700

    updated

--- tests/setup-ci-report-append-logfile-webroot.sh
+++ tests/setup-ci-report-append-logfile-webroot.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y nginx certbot python3-certbot-nginx
 sudo sed -i 's/server_name _;/server_name cicd.rtx.ai;/g' /etc/nginx/sites-enabled/default
 sudo servic nginx reload
 sudo certbot --no-eff-email --agree-tos --redirect --nginx -m ramseyst@oregonstate.edu -d cicd.rtx.ai
+crontab -l > cron_bkp
+echo "0 * * * * /home/ubuntu/RTX/tests/ci-report-append-logfile-webroot.sh" >> cron_bkp
+crontab cron_bkp
