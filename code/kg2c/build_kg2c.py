d1e7df22d61c71bf05ca2c37e26708aa1b22bd4a
sundareswarpullela <pullelas@oregonstate.edu>
2023-01-06 09:39:48 -0800

    Updating print statement to arax-databases.rtx.ai #1944

--- code/kg2c/build_kg2c.py
+++ code/kg2c/build_kg2c.py
@@ -130,7 +130,7 @@ def main():
             if upload_to_s3:
                 _upload_output_files_to_s3()
             if upload_to_arax_databases_rtx_ai:
-                logging.info(f"Uploading KG2c artifacts to arax.ncats.io:{upload_directory}")
+                logging.info(f"Uploading KG2c artifacts to arax-databases.rtx.ai:{upload_directory}")
                 subprocess.check_call(["bash", "-x", f"{KG2C_DIR}/upload-kg2c-artifacts.sh", RTXConfiguration().db_host, upload_directory])
 
         logging.info(f"DONE WITH {'TEST ' if args.test else ''}KG2c BUILD! Took {round(((time.time() - start) / 60) / 60, 1)} hours")
