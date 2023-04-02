919408742ae900cee94a84ef38f8dc931b6a863c
Eric Deutsch <ericdeutsch@gmail.com>
2023-04-01 23:07:32 -0700

    Add support for /asyncquery_status in master since it is backwards compatible #1991

--- code/UI/OpenAPI/python-flask-server/openapi_server/controllers/asyncquery_status_controller.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/controllers/asyncquery_status_controller.py
@@ -4,6 +4,9 @@ import six
 from openapi_server.models.async_query_status_response import AsyncQueryStatusResponse  # noqa: E501
 from openapi_server import util
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../../../../ARAX/ARAXQuery")
+from ARAX_query_tracker import ARAXQueryTracker
+
 
 def asyncquery_status(job_id):  # noqa: E501
     """Retrieve the current status of a previously submitted asyncquery given its job_id
@@ -15,4 +18,13 @@ def asyncquery_status(job_id):  # noqa: E501
 
     :rtype: AsyncQueryStatusResponse
     """
-    return 'do some magic!'
+
+    query_tracker = ARAXQueryTracker()
+
+    response = query_tracker.get_job_status()
+
+    if response.status == 'UnknownJobId':
+        return( { "status": 404, "title": "Job id not found", "detail": response, "type": "about:blank" }, 404 )
+
+    return response
+
