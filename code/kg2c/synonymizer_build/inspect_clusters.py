13cd2227cc3b5fe87765d7891e1118e5c1e626ac
amykglen <glena@oregonstate.edu>
2023-05-03 10:44:42 -0700

    Better format cluster markdown output #2003

--- code/kg2c/synonymizer_build/inspect_clusters.py
+++ code/kg2c/synonymizer_build/inspect_clusters.py
@@ -118,6 +118,16 @@ def main():
     cluster_graph = get_graph(member_node_ids, intra_cluster_edge_ids, cursor)
     cluster_rep = next(node for node in cluster_graph["nodes"] if node["id"] == cluster_id)
 
+    # Print out the cluster edges in tabular format
+    column_names = ["subject", "predicate", "object", "upstream_resource_id", "primary_knowledge_source"]
+    cluster_edge_rows = [[edge["subject"], edge["predicate"], edge["object"],
+                          edge["upstream_resource_id"],
+                          edge["primary_knowledge_source"] if edge["primary_knowledge_source"] else ""]
+                         for edge in cluster_graph["edges"]]
+    cluster_edges_df = pd.DataFrame(cluster_edge_rows, columns=column_names).sort_values(by="upstream_resource_id")
+    print(f"\nCluster for {args.node_id} ({cluster_id}) has {cluster_edges_df.shape[0]} edges:")
+    print(f"\n{cluster_edges_df.to_markdown(index=False)}\n")
+
     # Print out the cluster nodes in tabular format
     column_names = ["id", "category", "name", "in_SRI", "in_KG2pre", "is_cluster_rep"]
     cluster_node_rows = [[node["id"], node["category"], node["name"],
@@ -126,26 +136,19 @@ def main():
                           convert_to_check_mark(node["id"] == cluster_id)]
                          for node in cluster_graph["nodes"]]
     cluster_nodes_df = pd.DataFrame(cluster_node_rows, columns=column_names).sort_values(by="id")
-    print(f"\n{cluster_nodes_df.to_markdown()}\n")
-
-    # Print out the cluster edges in tabular format
-    column_names = ["subject", "predicate", "object", "upstream_resource_id", "primary_knowledge_source"]
-    cluster_edge_rows = [[edge["subject"], edge["predicate"], edge["object"],
-                          edge["upstream_resource_id"],
-                          edge["primary_knowledge_source"] if edge["primary_knowledge_source"] else ""]
-                         for edge in cluster_graph["edges"]]
-    cluster_edges_df = pd.DataFrame(cluster_edge_rows, columns=column_names).sort_values(by="upstream_resource_id")
-    print(f"\n{cluster_edges_df.to_markdown()}\n")
+    print(f"\nCluster for {args.node_id} ({cluster_id}) has {cluster_nodes_df.shape[0]} nodes:")
+    print(f"\n{cluster_nodes_df.to_markdown(index=False)}\n")
 
     # Compute the density of this cluster
     density = compute_cluster_density(cluster_graph)
-    logging.info(f"Density of cluster {cluster_id} is: {density}")
+    print(f"With {len(cluster_graph['nodes'])} nodes and {len(cluster_graph['edges'])} edges, "
+          f"density of cluster is: {density}\n")
 
     # Save the cluster graph in a JSON file
     cluster_file_path = f"{cluster_graphs_path}/{get_node_name(cluster_rep)}_{cluster_id}.json"
     with open(cluster_file_path, "w+") as cluster_file:
         json.dump(cluster_graph, cluster_file, indent=2)
-    logging.info(f"Cluster saved in Biolink format to: {cluster_file_path}")
+    print(f"Cluster saved in Biolink format to: {cluster_file_path}\n")
 
     cursor.close()
     db_connection.close()
