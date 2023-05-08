47d28bbc127b90459ede674cfaab417ab2d3c287
amykglen <glena@oregonstate.edu>
2023-05-08 15:12:09 -0700

    Specify full path of sub-scripts #2003

--- code/kg2c/synonymizer_build/build_synonymizer.py
+++ code/kg2c/synonymizer_build/build_synonymizer.py
@@ -1,5 +1,8 @@
 import argparse
+import os
 import subprocess
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
@@ -11,11 +14,11 @@ def main():
     arg_parser.add_argument('--useconfigname', dest='use_config_name', action='store_true')
     args = arg_parser.parse_args()
 
-    step_1_command = ["python", "1_build_match_graph_kg2pre.py", args.kg2pre_version] + (["--downloadfresh"] if args.download_kg2pre else [])
-    step_2_command = ["python", "2_build_match_graph_sri.py"] + (["--downloadfresh"] if args.download_sri else [])
-    step_3_command = ["python", "3_merge_match_graphs.py"]
-    step_4_command = ["python", "4_cluster_match_graph.py"]
-    step_5_command = ["python", "5_create_synonymizer_sqlite.py"] + (["--useconfigname"] if args.use_config_name else [])
+    step_1_command = ["python", f"{SCRIPT_DIR}/1_build_match_graph_kg2pre.py", args.kg2pre_version] + (["--downloadfresh"] if args.download_kg2pre else [])
+    step_2_command = ["python", f"{SCRIPT_DIR}/2_build_match_graph_sri.py"] + (["--downloadfresh"] if args.download_sri else [])
+    step_3_command = ["python", f"{SCRIPT_DIR}/3_merge_match_graphs.py"]
+    step_4_command = ["python", f"{SCRIPT_DIR}/4_cluster_match_graph.py"]
+    step_5_command = ["python", f"{SCRIPT_DIR}/5_create_synonymizer_sqlite.py"] + (["--useconfigname"] if args.use_config_name else [])
     all_steps = [step_1_command, step_2_command, step_3_command, step_4_command, step_5_command]
 
     # Run the requested steps (default is all steps)
