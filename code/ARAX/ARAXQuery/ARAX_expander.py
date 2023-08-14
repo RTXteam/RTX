fcdfad00cb3e37797df8fd34918760f43838fd13
Sundareswar Pullela <pullelas@oregonstate.edu>
2023-08-14 22:03:54 +0530

    #2073

--- code/ARAX/ARAXQuery/ARAX_expander.py
+++ code/ARAX/ARAXQuery/ARAX_expander.py
@@ -439,6 +439,7 @@ class ARAXExpander:
         if qedge_keys_to_expand:
             query_sub_graph = self._extract_query_subgraph(qedge_keys_to_expand, query_graph, log)
             if log.status != 'OK':
+                response = self._filter_response_domain_range_exclusion(response=response, log=log)
                 return response
             log.debug(f"Query graph for this Expand() call is: {query_sub_graph.to_dict()}")
 
@@ -578,6 +579,7 @@ class ARAXExpander:
                                 kp_log.status = 'OK'  # We don't want to halt just because one KP reported an error #1500
                             log.merge(kp_log)
                             if response.status != 'OK':
+                                response = self._filter_response_domain_range_exclusion(response=response, log=log)
                                 return response
                 else:
                     log.error(f"Expand could not find any KPs to answer {qedge_key} with.", error_code="NoResults")
@@ -595,6 +597,7 @@ class ARAXExpander:
                     else:
                         self._merge_answer_into_message_kg(answer_kg, overarching_kg, message.query_graph, query_graph, mode, response)
                     if response.status != 'OK':
+                        response = self._filter_response_domain_range_exclusion(response=response, log=log)
                         return response
                 log.debug(f"After merging KPs' answers, total KG counts are: {eu.get_printable_counts_by_qg_id(overarching_kg)}")
 
@@ -653,6 +656,7 @@ class ARAXExpander:
                     # Remove any paths that are now dead-ends
                     overarching_kg = self._remove_dead_end_paths(query_graph, overarching_kg, response)
                     if response.status != 'OK':
+                        response = self._filter_response_domain_range_exclusion(response=response, log=log)
                         return response
 
                 # Declare that we are done expanding this qedge
@@ -663,6 +667,7 @@ class ARAXExpander:
                 if not eu.qg_is_fulfilled(one_hop_qg, overarching_kg) and not qedge.exclude and not qedge.option_group_id:
                     log.warning(f"No paths were found in any KPs satisfying qedge {qedge_key}. KPs used were: "
                                 f"{kps_to_query}")
+                    response = self._filter_response_domain_range_exclusion(response=response, log=log)
                     return response
 
         # Expand any specified nodes
@@ -675,6 +680,7 @@ class ARAXExpander:
                     return response
                 self._merge_answer_into_message_kg(answer_kg, overarching_kg, message.query_graph, query_graph, mode, log)
                 if log.status != 'OK':
+                    response = self._filter_response_domain_range_exclusion(response=response, log=log)
                     return response
 
         # Get rid of any lingering expand-added subclass self-qedges that are no longer relevant (edges pruned)
@@ -705,7 +711,7 @@ class ARAXExpander:
         kg = message.knowledge_graph
         log.info(f"After Expand, the KG has {len(kg.nodes)} nodes and {len(kg.edges)} edges "
                  f"({eu.get_printable_counts_by_qg_id(overarching_kg)})")
-
+        response = self._filter_response_domain_range_exclusion(response=response, log=log)
         return response
 
     async def _expand_edge_async(self, edge_qg: QueryGraph, kp_to_use: str, input_parameters: Dict[str, any],
