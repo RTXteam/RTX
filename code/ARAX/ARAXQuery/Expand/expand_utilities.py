d3acd95c536d3c3f3bc74ac446f6c2b0b228069b
Sundareswar Pullela <pullelas@oregonstate.edu>
2023-08-16 21:54:41 +0530

    #2073

--- code/ARAX/ARAXQuery/Expand/expand_utilities.py
+++ code/ARAX/ARAXQuery/Expand/expand_utilities.py
@@ -600,16 +600,17 @@ def remove_semmeddb_edges_and_nodes_with_low_publications(kg: KnowledgeGraph, lo
         else:
             log.info(f"{edges_removed_counter} Semmeddb Edges with low publications successfully removed")
 
-def filter_response_domain_range_exclusion(plover_answer, log: ARAXResponse):
+def filter_response_domain_range_exclusion(plover_answer, qg, log: ARAXResponse):
         log.debug("Applying domain range exclusion to plover response")
         log.debug(plover_answer)
         filtered_count = 0
-        
-        edge_keys_to_filter = {edge_id for edge_id, edge in plover_answer.edges.items() if edge.domain_range_exclusion == "True"}
-        for edge in plover_answer.edges:
-            if edge in edge_keys_to_filter:
+        qg_edge_keys = qg.edges.keys()
+        for qg_edge_key in qg_edge_keys:
+            edge_keys_to_filter = {edge_id for edge_id, edge in plover_answer['edges'][qg_edge_key].items() if edge.domain_range_exclusion == "True"}
+            for edge_key in edge_keys_to_filter:
+                del plover_answer['edges'][qg_edge_key][edge_key]
                 filtered_count += 1
-                del plover_answer.edges[edge]
+        
         log.info(f"Filtered out {filtered_count} edges from response due to domain range exclusion")
 
         return plover_answer
