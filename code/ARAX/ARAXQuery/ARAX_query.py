4da003b34d43287c1e01cabd6427803b3313b5f6
Eric Deutsch <edeutsch@systemsbiology.org>
2024-05-28 18:22:12 +0000

    add member_ids to allowed QNode properties

--- code/ARAX/ARAXQuery/ARAX_query.py
+++ code/ARAX/ARAXQuery/ARAX_query.py
@@ -497,7 +497,7 @@ class ARAXQuery:
         response.info(f"Validating the input query graph")
 
         # Define allowed qnode and qedge attributes to check later
-        allowed_qnode_attributes = { 'ids': 1, 'categories':1, 'is_set': 1, 'set_interpretation': 1, 'set_id': 1, 'option_group_id': 1, 'name': 1, 'constraints': 1 }
+        allowed_qnode_attributes = { 'ids': 1, 'categories':1, 'is_set': 1, 'set_interpretation': 1, 'set_id': 1, 'member_ids': 1, 'option_group_id': 1, 'name': 1, 'constraints': 1 }
         allowed_qedge_attributes = { 'predicates': 1, 'subject': 1, 'object': 1, 'option_group_id': 1, 'exclude': 1, 'relation': 1, 'attribute_constraints': 1, 'qualifier_constraints': 1, 'knowledge_type': 1 }
 
         #### Loop through nodes checking the attributes
