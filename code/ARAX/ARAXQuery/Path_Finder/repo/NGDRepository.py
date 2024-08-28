b3d848ee538edd07f69073ac1afa711e1df3fc58
Mohsen Taheri <mohsen.taheri.1989@gmail.com>
2024-08-28 13:05:16 -0400

    Mistype NGDRepository

--- code/ARAX/ARAXQuery/Path_Finder/repo/NGDRepository.py
+++ code/ARAX/ARAXQuery/Path_Finder/repo/NGDRepository.py
@@ -7,16 +7,16 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 from repo.utility import get_curie_ngd_path
 
 
-class NodeDegreeRepo:
+class NGDRepository:
 
     def __init__(self, db_path=get_curie_ngd_path()):
         self.db_path = db_path
 
-    def get_curie_ngd(self, node):
+    def get_curie_ngd(self, curie):
         sqlite_connection_read = sqlite3.connect(self.db_path)
         cursor = sqlite_connection_read.cursor()
         query = "SELECT ngd FROM curie_ngd WHERE curie = ?"
-        cursor.execute(query, (node.id,))
+        cursor.execute(query, (curie,))
         row = cursor.fetchone()
         cursor.close()
         sqlite_connection_read.close()
