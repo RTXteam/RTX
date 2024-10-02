cbb6ecaedb83c5f2554057f0fb5b5ebdf4ab6e5e
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2024-10-02 09:35:34 -0700

    Fix failing test issues #2380

--- code/ARAX/test/test_ARAX_json_queries.py
+++ code/ARAX/test/test_ARAX_json_queries.py
@@ -180,7 +180,7 @@ def test_workflow1():
     }
     nodes_by_qg_id, edges_by_qg_id, response = _run_query_and_do_standard_testing(json_query=query)
     essences = [x.to_dict()['essence'].upper() for x in response.envelope.message.results]
-    assert 'CYCLOOXYGENASE' in essences
+    assert 'VANILLOID RECEPTOR' in essences
 
 @pytest.mark.slow
 def test_workflow2():
