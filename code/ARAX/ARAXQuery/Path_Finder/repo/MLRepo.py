8a765da1d291d58fac1dd616db56d3772437c7f1
mohsenht <mohsen.taheri.1989@gmail.com>
2025-04-02 10:43:35 -0400

    #2435 hard-coded categories in MLRepo

--- code/ARAX/ARAXQuery/Path_Finder/repo/MLRepo.py
+++ code/ARAX/ARAXQuery/Path_Finder/repo/MLRepo.py
@@ -30,62 +30,20 @@ class MLRepo(Repository):
         self.degree_repo = degree_repo
         self.ngd_repo = ngd_repo
         self.node_synonymizer = node_synonymizer
-        self.sorted_category_list = ['Activity', 'Agent', 'AnatomicalEntity', 'Behavior', 'BehavioralFeature',
-                                     'BiologicalEntity', 'BiologicalProcess', 'Cell', 'CellLine', 'CellularComponent',
-                                     'ChemicalEntity', 'ChemicalMixture', 'ClinicalAttribute', 'Cohort',
-                                     'ComplexMolecularMixture', 'Device', 'Disease', 'DiseaseOrPhenotypicFeature',
-                                     'Drug', 'EnvironmentalProcess', 'Event', 'Exon', 'Food', 'Gene', 'GeneFamily',
-                                     'GenomicEntity', 'GeographicLocation', 'GrossAnatomicalStructure', 'Human',
-                                     'IndividualOrganism', 'InformationContentEntity', 'LifeStage', 'MaterialSample',
-                                     'MicroRNA', 'MolecularActivity', 'MolecularEntity', 'MolecularMixture',
-                                     'NamedThing', 'NoncodingRNAProduct', 'NucleicAcidEntity', 'OrganismAttribute',
-                                     'OrganismTaxon', 'PathologicalProcess', 'Pathway', 'Phenomenon',
-                                     'PhenotypicFeature', 'PhysicalEntity', 'PhysiologicalProcess', 'Polypeptide',
-                                     'PopulationOfIndividualOrganisms', 'Procedure', 'Protein', 'Publication',
-                                     'RNAProduct', 'RetrievalSource', 'SmallMolecule', 'Transcript', 'Treatment']
-        self.edge_category_to_idx = {'biolink:related_to': 0, 'biolink:close_match': 1, 'biolink:subclass_of': 2,
-                                     'biolink:interacts_with': 3,
-                                     'biolink:treats_or_applied_or_studied_to_treat': 4, 'biolink:affects': 5,
-                                     'biolink:has_input': 6,
-                                     'biolink:causes': 7, 'biolink:disrupts': 8,
-                                     'biolink:preventative_for_condition': 9,
-                                     'biolink:predisposes_to_condition': 10, 'biolink:produces': 11,
-                                     'biolink:coexists_with': 12,
-                                     'biolink:precedes': 13, 'biolink:exacerbates_condition': 14,
-                                     'biolink:manifestation_of': 15,
-                                     'biolink:located_in': 16, 'biolink:diagnoses': 17, 'biolink:occurs_in': 18,
-                                     'biolink:has_participant': 19,
-                                     'biolink:in_clinical_trials_for': 20, 'biolink:treats': 21,
-                                     'biolink:contraindicated_in': 22,
-                                     'biolink:physically_interacts_with': 23, 'biolink:has_metabolite': 24,
-                                     'biolink:has_member': 25,
-                                     'biolink:has_part': 26, 'biolink:transcribed_from': 27, 'biolink:regulates': 28,
-                                     'biolink:gene_associated_with_condition': 29, 'biolink:translates_to': 30,
-                                     'biolink:enables': 31,
-                                     'biolink:actively_involved_in': 32, 'biolink:capable_of': 33,
-                                     'biolink:colocalizes_with': 34,
-                                     'biolink:in_taxon': 35, 'biolink:expressed_in': 36, 'biolink:gene_product_of': 37,
-                                     'biolink:directly_physically_interacts_with': 38, 'biolink:contributes_to': 39,
-                                     'biolink:applied_to_treat': 40,
-                                     'biolink:biomarker_for': 41, 'biolink:overlaps': 42, 'biolink:has_phenotype': 43,
-                                     'biolink:same_as': 44,
-                                     'biolink:has_output': 45, 'biolink:chemically_similar_to': 46,
-                                     'biolink:derives_from': 47,
-                                     'biolink:is_sequence_variant_of': 48, 'biolink:associated_with': 49,
-                                     'biolink:correlated_with': 50,
-                                     'biolink:disease_has_location': 51, 'biolink:exact_match': 52,
-                                     'biolink:temporally_related_to': 53,
-                                     'biolink:composed_primarily_of': 54, 'biolink:has_plasma_membrane_part': 55,
-                                     'biolink:has_not_completed': 56,
-                                     'biolink:develops_from': 57, 'biolink:has_increased_amount': 58,
-                                     'biolink:lacks_part': 59,
-                                     'biolink:has_decreased_amount': 60, 'biolink:opposite_of': 61,
-                                     'biolink:beneficial_in_models_for': 62,
-                                     'biolink:disease_has_basis_in': 63,
-                                     'biolink:indirectly_physically_interacts_with': 64}
+
+        with open((os.path.dirname(os.path.abspath(__file__)) + '/sorted_category_list.pkl'), "rb") as f:
+            self.sorted_category_list = pickle.load(f)
+
+        with open((os.path.dirname(os.path.abspath(__file__)) + '/edge_category_to_idx.pkl'), "rb") as f:
+            self.edge_category_to_idx = pickle.load(f)
         self.category_to_idx = {cat_name: idx for idx, cat_name in enumerate(self.sorted_category_list)}
+
         with open((os.path.dirname(os.path.abspath(__file__)) + '/ancestors_by_indices.pkl'), "rb") as f:
             self.ancestors_by_id = pickle.load(f)
+
+        self.bst_loaded = xgb.Booster()
+        self.bst_loaded.load_model(os.path.dirname(os.path.abspath(__file__)) + '/pathfinder_xgboost_model')
+
 
     def get_neighbors(self, node, limit=-1):
         if limit <= 0:
@@ -110,10 +68,7 @@ class MLRepo(Repository):
 
         dtest = xgb.DMatrix(X)
 
-        bst_loaded = xgb.Booster()
-        bst_loaded.load_model(os.path.dirname(os.path.abspath(__file__)) + '/pathfinder_xgboost_model')
-
-        scores = bst_loaded.predict(dtest)
+        scores = self.bst_loaded.predict(dtest)
 
         probabilities = sigmoid(scores)
 
