51d618abd7afdbc1bd03cd352e7b4ab3aa24e305
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-07-16 15:47:19 -0700

    #2537

--- code/UI/OpenAPI/python-flask-server/openapi_server/test/__init__.py
+++ code/UI/OpenAPI/python-flask-server/openapi_server/test/__init__.py
@@ -3,14 +3,14 @@ import logging
 import connexion
 from flask_testing import TestCase
 
-from openapi_server.encoder import JSONEncoder
-
+from openapi_server.provider import CustomJSONProvider
 
 class BaseTestCase(TestCase):
 
     def create_app(self):
         logging.getLogger('connexion.operation').setLevel('ERROR')
         app = connexion.App(__name__, specification_dir='../openapi/')
-        app.app.json_encoder = JSONEncoder
+        app.json_provider_class = CustomJSONProvider
+        app.json = app.json_provider_class(app)        
         app.add_api('openapi.yaml', pythonic_params=True)
         return app.app
