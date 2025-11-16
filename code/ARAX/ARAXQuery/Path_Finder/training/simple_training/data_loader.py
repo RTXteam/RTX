9a7433878500c7cb703320325727e5feae7a1f4f
mohsenht <mohsen.taheri.1989@gmail.com>
2025-11-15 20:18:28 -0500

    Add KEGG data to training multiple strategies: post training fine tuning complete training

--- code/ARAX/ARAXQuery/Path_Finder/training/simple_training/data_loader.py
+++ code/ARAX/ARAXQuery/Path_Finder/training/simple_training/data_loader.py
@@ -4,14 +4,14 @@ import pickle
 import numpy as np
 
 
-def load_data():
+def load_data(data_source):
     logging.info(f"Features vector is loading")
-    x = np.load("X_data.npy")
+    x = np.load(f"{data_source}X_data.npy")
     logging.info(f"Features vector is loaded")
     logging.info(f"Labels are loading")
-    y = np.load("y_data.npy")
+    y = np.load(f"{data_source}y_data.npy")
     logging.info(f"Labels are loaded")
-    with open("group.pkl", "rb") as f:
+    with open(f"{data_source}group.pkl", "rb") as f:
         group = pickle.load(f)
 
     return x, y, group
