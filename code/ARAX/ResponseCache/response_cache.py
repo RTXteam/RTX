9989dc2c69fa226b02211ddf5d388fa7348748cd
Eric Deutsch <ericdeutsch@gmail.com>
2025-12-23 21:39:30 +0000

    Switch hard-coded TRAPI validation version from 1.5.0 to 1.6.0 #2603

--- code/ARAX/ResponseCache/response_cache.py
+++ code/ARAX/ResponseCache/response_cache.py
@@ -48,7 +48,7 @@ from ARAX_attribute_parser import ARAXAttributeParser
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../../UI/OpenAPI/python-flask-server/")
 from openapi_server.models.response import Response as Envelope
 
-trapi_version = '1.5.0'
+trapi_version = '1.6.0'
 biolink_version = '4.2.1'
 
 try:
