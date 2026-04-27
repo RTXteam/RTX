86b4731879b8769560a5c9abab78eb67d7274b5b
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2026-04-27 15:56:02 -0700

    #2730 updated generate-db-symlinks.sh for testing this branch

--- code/generate-db-symlinks.sh
+++ code/generate-db-symlinks.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 
 # Stephen Ramsey, Oregon State University
 
-LATEST_KG2_VER="KG2.10.2"
+LATEST_TIER0_VER="tier0-20260408"
 DB_DIR="/Users/sramsey/Work/big-files/arax-dbs"
 
 if [[ ! -d "RTX" ]]; then
@@ -41,15 +41,21 @@ link() {
   ln -sfn "$src" "$dest"
 }
 
-link "${DB_DIR}/KG2.8.0/COHDdatabase_v1.0_KG2.8.0.db" "RTX/code/ARAX/KnowledgeSources/COHD_local/data/COHDdatabase_v1.0_KG2.8.0.db"
-link "${DB_DIR}/KG2.10.0/xcrg_decrease_model_v1.0.KG2.10.0_new_version.pt" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/xcrg_decrease_model_v1.0.KG2.10.0_new_version.pt"
-link "${DB_DIR}/KG2.10.0/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt"
-link "${DB_DIR}/${LATEST_KG2_VER}/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_KG2_VER}.npz" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_KG2_VER}.npz"
-link "${DB_DIR}/${LATEST_KG2_VER}/ExplainableDTD_v1.0_KG2.10.0_refreshedTo_${LATEST_KG2_VER}.db" "RTX/code/ARAX/KnowledgeSources/Prediction/ExplainableDTD_v1.0_KG2.10.0_refreshedTo_${LATEST_KG2_VER}.db"
-link "${DB_DIR}/${LATEST_KG2_VER}/curie_ngd_v1.0_${LATEST_KG2_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_ngd_v1.0_${LATEST_KG2_VER}.sqlite"
-link "${DB_DIR}/${LATEST_KG2_VER}/curie_to_pmids_v1.0_${LATEST_KG2_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_to_pmids_v1.0_${LATEST_KG2_VER}.sqlite"
-link "${DB_DIR}/${LATEST_KG2_VER}/fda_approved_drugs_v1.0_${LATEST_KG2_VER}c.pickle" "RTX/code/ARAX/KnowledgeSources/fda_approved_drugs_v1.0_${LATEST_KG2_VER}c.pickle"
-link "${DB_DIR}/${LATEST_KG2_VER}/kg2c_v1.0_${LATEST_KG2_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/KG2c/kg2c_v1.0_${LATEST_KG2_VER}.sqlite"
-link "${DB_DIR}/${LATEST_KG2_VER}/node_synonymizer_v1.0_${LATEST_KG2_VER}.sqlite" "RTX/code/ARAX/NodeSynonymizer/node_synonymizer_v1.0_${LATEST_KG2_VER}.sqlite"
-link "${DB_DIR}/${LATEST_KG2_VER}/autocomplete_v1.0_${LATEST_KG2_VER}.sqlite" "RTX/code/autocomplete/autocomplete_v1.0_${LATEST_KG2_VER}.sqlite"
+link "${DB_DIR}/${LATEST_TIER0_VER}/COHDdatabase_v1.0_${LATEST_TIER0_VER}.db" "RTX/code/ARAX/KnowledgeSources/COHD_local/data/COHDdatabase_v1.0_${LATEST_TIER0_VER}.db"
+
+# commented out pending readiness of new xCRG code in: https://github.com/Translator-CATRAX/shepherd/issues
+#link "${DB_DIR}/KG2.10.0/xcrg_decrease_model_v1.0.KG2.10.0_new_version.pt" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/xcrg_decrease_model_v1.0.KG2.10.0_new_version.pt"
+#link "${DB_DIR}/KG2.10.0/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt"
+#link "${DB_DIR}/${LATEST_TIER0_VER}/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_TIER0_VER}.npz" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_TIER0_VER}.npz"
+
+link "${DB_DIR}/${LATEST_TIER0_VER}/ExplainableDTD_${LATEST_TIER0_VER}.db" "RTX/code/ARAX/KnowledgeSources/Prediction/ExplainableDTD_${LATEST_TIER0_VER}.db"
+
+# commented out pending resolution of ARAX issue 2732
+#link "${DB_DIR}/${LATEST_TIER0_VER}/curie_ngd_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_ngd_v1.0_${LATEST_TIER0_VER}.sqlite"
+
+link "${DB_DIR}/${LATEST_TIER0_VER}/curie_to_pmids_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_to_pmids_v1.0_${LATEST_TIER0_VER}.sqlite"
+link "${DB_DIR}/${LATEST_TIER0_VER}/fda_approved_drugs_v1.0.pickle" "RTX/code/ARAX/KnowledgeSources/fda_approved_drugs_v1.0.pickle"
+link "${DB_DIR}/${LATEST_TIER0_VER}/tier0-info-for-overlay_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/KG2c/tier0-info-for-overlay_v1.0_${LATEST_TIER0_VER}.sqlite"
+link "${DB_DIR}/${LATEST_TIER0_VER}/node_synonymizer_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/NodeSynonymizer/node_synonymizer_v1.0_${LATEST_TIER0_VER}.sqlite"
+link "${DB_DIR}/${LATEST_TIER0_VER}/autocomplete_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/autocomplete/autocomplete_v1.0_${LATEST_TIER0_VER}.sqlite"
 
