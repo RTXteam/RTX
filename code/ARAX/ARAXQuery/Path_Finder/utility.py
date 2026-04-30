14843007995b8d788060d93e912ec336ec626902
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2026-04-30 10:00:07 -0700

    #2730 patch to make it get_gandalf_mmap_path work inside ARAX docker container

--- code/ARAX/ARAXQuery/Path_Finder/utility.py
+++ code/ARAX/ARAXQuery/Path_Finder/utility.py
@@ -18,9 +18,23 @@ def get_curie_ngd_path():
     return f"sqlite:{filepath}{os.path.sep}{sqlite_name}"
 
 def get_gandalf_mmap_path():
-    pathlist = os.path.realpath(__file__).split(os.path.sep)
-    RTXindex = pathlist.index("RTX")
-    fileName = RTXConfiguration().gandalf_mmap_path.split("/")[-1]
-    gandalf_dir = fileName.split("_tier0")[0]
-    path = os.path.sep.join([*pathlist[:(RTXindex + 1)], 'code', 'ARAX', 'KnowledgeSources', 'Gandalf', gandalf_dir])
-    return f"gandalf:{path}"
+    gandalf_mmap_path = RTXConfiguration().gandalf_mmap_path
+    gandalf_db_bundle = os.path.basename(gandalf_mmap_path)
+    gandalf_dir = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'KnowledgeSources', 'Gandalf'))
+    gandalf_db_bundle_local = os.path.join(gandalf_dir, gandalf_db_bundle)
+    if os.path.islink(gandalf_db_bundle_local):
+        gandalf_db_bundle_to_use = os.path.realpath(gandalf_db_bundle_local)
+    else:
+        gandalf_db_bundle_to_use = gandalf_db_bundle_local
+    gandalf_mmap_dir = os.path.join(os.path.dirname(gandalf_db_bundle_to_use), 'gandalf_mmap')
+    return f"gandalf:{gandalf_mmap_dir}"
+
+
+def main():
+    print(get_kg2c_db_path())
+    print(get_curie_ngd_path())
+    print(get_gandalf_mmap_path())
+
+
+if __name__ == "__main__":
+    main()
