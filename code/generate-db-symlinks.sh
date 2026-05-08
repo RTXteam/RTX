d392ea5a1e7a1ce03997814a69510280a320c677
Frankie Hodges <hodgesf@oregonstate.edu>
2026-05-08 15:00:31 -0700

    update symlink script

--- code/generate-db-symlinks.sh
+++ code/generate-db-symlinks.sh
@@ -49,7 +49,7 @@ link "${DB_DIR}/KG2.8.0/COHDdatabase_v1.0_KG2.8.0.db" "RTX/code/ARAX/KnowledgeSo
 #link "${DB_DIR}/KG2.10.0/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/xcrg_increase_model_v1.0.KG2.10.0_new_version.pt"
 #link "${DB_DIR}/${LATEST_TIER0_VER}/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_TIER0_VER}.npz" "RTX/code/ARAX/ARAXQuery/Infer/data/xCRG_data/chemical_gene_embeddings_v1.0.KG2.10.0_refreshedTo_${LATEST_TIER0_VER}.npz"
 
-link "${DB_DIR}/${LATEST_TIER0_VER}/ExplainableDTD_${LATEST_TIER0_VER}.db" "RTX/code/ARAX/KnowledgeSources/Prediction/ExplainableDTD_${LATEST_TIER0_VER}.db"
+link "${DB_DIR}/${LATEST_TIER0_VER}/ExplainableDTD_${LATEST_TIER0_VER}-all_with_paths.db" "RTX/code/ARAX/KnowledgeSources/Prediction/ExplainableDTD_${LATEST_TIER0_VER}-all_with_paths.db"
 link "${DB_DIR}/${LATEST_TIER0_VER}/curie_ngd_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_ngd_v1.0_${LATEST_TIER0_VER}.sqlite"
 link "${DB_DIR}/${LATEST_TIER0_VER}/curie_to_pmids_v1.0_${LATEST_TIER0_VER}.sqlite" "RTX/code/ARAX/KnowledgeSources/NormalizedGoogleDistance/curie_to_pmids_v1.0_${LATEST_TIER0_VER}.sqlite"
 link "${DB_DIR}/${LATEST_TIER0_VER}/fda_approved_drugs_v1.0.pickle" "RTX/code/ARAX/KnowledgeSources/fda_approved_drugs_v1.0.pickle"
