cb39696b706eaee5fb4a7a08335b3e0f52a78611
mohsenht <mohsen.taheri.1989@gmail.com>
2026-05-19 17:32:56 -0400

    Updated pathfinder-catrax with new model for Tier0 KG

--- code/ARAX/ARAXQuery/ARAX_connect.py
+++ code/ARAX/ARAXQuery/ARAX_connect.py
@@ -383,8 +383,8 @@ class ARAXConnect:
                 hops_numbers=self.parameters['max_path_length'],
                 max_hops_to_explore=self.parameters['max_path_length'],
                 limit=max_pathfinder_paths,
-                prune_top_k=200,
-                degree_threshold=20000,
+                prune_top_k=75,
+                degree_threshold=50000,
                 category_constraints=descendants,
             )
         except Exception as e:
